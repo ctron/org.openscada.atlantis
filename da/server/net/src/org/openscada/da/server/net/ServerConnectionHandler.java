860e899495dd4a461f316c3009d71e22e43cdf1b
Jens Reimann <jens.reimann@th4-systems.com>
2006-05-16 12:35:15 +0000

    added write operation
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis@235 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- da/server/net/src/org/openscada/da/server/net/ServerConnectionHandler.java
+++ da/server/net/src/org/openscada/da/server/net/ServerConnectionHandler.java
@@ -11,6 +11,8 @@ import org.openscada.da.core.InvalidSessionException;
 import org.openscada.da.core.ItemChangeListener;
 import org.openscada.da.core.ItemListListener;
 import org.openscada.da.core.Session;
+import org.openscada.da.core.WriteOperationListener;
+import org.openscada.da.core.common.impl.WriteOperation;
 import org.openscada.da.core.data.Variant;
 import org.openscada.net.base.ConnectionHandlerBase;
 import org.openscada.net.base.MessageListener;
@@ -20,106 +22,113 @@ import org.openscada.net.da.handler.EnumEvent;
 import org.openscada.net.da.handler.Messages;
 import org.openscada.net.io.Connection;
 import org.openscada.net.utils.MessageCreator;
+import org.openscada.utils.lang.Holder;
 
 public class ServerConnectionHandler extends ConnectionHandlerBase implements ItemChangeListener, ItemListListener {
-
+    
     private static Logger _log = Logger.getLogger ( ServerConnectionHandler.class );
     
-	private Hive _hive = null;
-	private Session _session = null;
-	
-	public ServerConnectionHandler(Hive hive)
+    private Hive _hive = null;
+    private Session _session = null;
+    
+    public ServerConnectionHandler(Hive hive)
     {
-		super();
-		
-		_hive = hive;
-		
-		getMessageProcessor().setHandler(Messages.CC_CREATE_SESSION, new MessageListener(){
-
-			public void messageReceived(Connection connection, Message message) {
-				createSession ( message );
-			}});
-		
-		getMessageProcessor().setHandler(Messages.CC_CLOSE_SESSION, new MessageListener(){
-
-			public void messageReceived(Connection connection, Message message) {
-				closeSession ();
-			}});
+        super();
+        
+        _hive = hive;
+        
+        getMessageProcessor().setHandler(Messages.CC_CREATE_SESSION, new MessageListener(){
+            
+            public void messageReceived(Connection connection, Message message) {
+                createSession ( message );
+            }});
+        
+        getMessageProcessor().setHandler(Messages.CC_CLOSE_SESSION, new MessageListener(){
+            
+            public void messageReceived(Connection connection, Message message) {
+                closeSession ();
+            }});
         
         getMessageProcessor().setHandler(Messages.CC_SUBSCRIBE_ITEM, new MessageListener(){
-
+            
             public void messageReceived(Connection connection, Message message) {
                 subscribe ( message );
             }});
         
         getMessageProcessor().setHandler(Messages.CC_UNSUBSCRIBE_ITEM, new MessageListener(){
-
+            
             public void messageReceived(Connection connection, Message message) {
                 unsubscribe ( message );
             }});
         
         getMessageProcessor().setHandler(Messages.CC_ENUM_SUBSCRIBE, new MessageListener(){
-
+            
             public void messageReceived ( Connection connection, Message message )
             {
                 enumSubscribe ( message );
             }});
         
         getMessageProcessor().setHandler(Messages.CC_ENUM_UNSUBSCRIBE, new MessageListener(){
-
+            
             public void messageReceived ( Connection connection, Message message )
             {
                 enumUnsubscribe ( message );
             }});
-		
-	}
-	
-	private void createSession ( Message message )
-	{
-		// if session exists this is an error
-		if ( _session != null )
-		{
-			getConnection().sendMessage(MessageCreator.createFailedMessage(message,"Session already exists"));
-			return;
-		}
-		
-		Properties props = new Properties();
-		for ( Map.Entry<String,Value> entry : message.getValues().entrySet() )
-		{
-			props.put ( entry.getKey(), entry.getValue().toString() );
-		}
-		
-		_session = _hive.createSession(props);
+        
+        getMessageProcessor().setHandler(Messages.CC_WRITE_OPERATION, new MessageListener(){
+            
+            public void messageReceived ( Connection connection, Message message )
+            {
+                performWrite ( message );
+            }});
+    }
+    
+    private void createSession ( Message message )
+    {
+        // if session exists this is an error
+        if ( _session != null )
+        {
+            getConnection().sendMessage(MessageCreator.createFailedMessage(message,"Session already exists"));
+            return;
+        }
+        
+        Properties props = new Properties();
+        for ( Map.Entry<String,Value> entry : message.getValues().entrySet() )
+        {
+            props.put ( entry.getKey(), entry.getValue().toString() );
+        }
+        
+        _session = _hive.createSession(props);
         _session.setListener((ItemListListener)this);
         _session.setListener((ItemChangeListener)this);
-		
-		if ( _session == null )
-		{
-			getConnection().sendMessage(MessageCreator.createFailedMessage(message,"unable to create session"));
-			return;
-		}
-		getConnection().sendMessage(MessageCreator.createACK(message));
-	}
-	
-	private void disposeSession ()
-	{
-		// if session does not exists, silently ignore it
-		if ( _session != null )
-		{
-			try {
-				_hive.closeSession(_session);
-			} catch (InvalidSessionException e) {
-				e.printStackTrace();
-			}
-		}	
-	}
-	
-	private void closeSession ()
-	{
-		disposeSession ();
-		// also shut down communcation connection
-		getConnection().close();
-	}
+        
+        if ( _session == null )
+        {
+            getConnection().sendMessage(MessageCreator.createFailedMessage(message,"unable to create session"));
+            return;
+        }
+        getConnection().sendMessage(MessageCreator.createACK(message));
+    }
+    
+    private void disposeSession ()
+    {
+        // if session does not exists, silently ignore it
+        if ( _session != null )
+        {
+            try {
+                _hive.closeSession(_session);
+            } catch (InvalidSessionException e) {
+                e.printStackTrace();
+            }
+        }	
+    }
+    
+    private void closeSession ()
+    {
+        disposeSession ();
+        // also shut down communcation connection
+        getConnection().close();
+    }
     
     private void subscribe ( Message message )
     {
@@ -211,33 +220,64 @@ public class ServerConnectionHandler extends ConnectionHandlerBase implements It
         }
         
     }
-	
-	private void cleanUp ()
-	{
-        disposeSession();
-	}
-	
-	@Override
-	public void closed()
+    
+    private void cleanUp ()
     {
-		cleanUp ();
-		super.closed();
-	}
-
+        disposeSession();
+    }
+    
+    @Override
+    public void closed()
+    {
+        cleanUp ();
+        super.closed();
+    }
+    
     public void valueChanged ( String name, Variant value, boolean initial )
     {
         getConnection().sendMessage(Messages.notifyValue(name, value, initial));
     }
-
+    
     public void attributesChanged ( String name, Map<String, Variant> attributes, boolean initial )
     {
         getConnection().sendMessage(Messages.notifyAttributes(name, attributes, initial));
     }
-
+    
     public void changed ( Collection<String> added, Collection<String> removed, boolean initial )
     {
         _log.debug("Got enum change event from hive");
         getConnection().sendMessage(EnumEvent.create(added, removed, initial));
     }
-	
+    
+    private void performWrite ( final Message message )
+    {
+        Holder<String> itemName = new Holder<String>();
+        Holder<Variant> value = new Holder<Variant>();
+        
+        org.openscada.net.da.handler.WriteOperation.parse ( message, itemName, value );
+        
+        _log.debug("Writing to '" + itemName.value + "'" );
+        
+        try {
+            _hive.startWrite ( _session, itemName.value, value.value, new WriteOperationListener(){
+                
+                public void success ()
+                {
+                    getConnection().sendMessage ( MessageCreator.createACK(message) ) ;
+                }
+                
+                public void failure ( String errorMessage )
+                {
+                    getConnection().sendMessage ( MessageCreator.createFailedMessage ( message, errorMessage ) ) ;
+                }} );
+        }
+        catch ( InvalidItemException e )
+        {
+            getConnection().sendMessage ( MessageCreator.createFailedMessage ( message, "Invalid item" ) );
+        }
+        catch ( InvalidSessionException e )
+        {
+            getConnection().sendMessage ( MessageCreator.createFailedMessage ( message, "Invalid session" ) );
+        }
+    }
 }
