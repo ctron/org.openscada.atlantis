5370b677bb98f5c01a7cb5aeb7736175dd96e522
Jens Reimann <jens.reimann@th4-systems.com>
2006-05-16 12:34:42 +0000

    added write operation
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis@234 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- da/net/base/src/org/openscada/net/da/handler/WriteOperation.java
+++ da/net/base/src/org/openscada/net/da/handler/WriteOperation.java
@@ -3,13 +3,14 @@ package org.openscada.net.da.handler;
 import org.openscada.da.core.data.Variant;
 import org.openscada.net.base.data.Message;
 import org.openscada.net.base.data.StringValue;
+import org.openscada.utils.lang.Holder;
 
 public class WriteOperation
 {
 
     public static Message create ( String itemName, Variant value )
     {
-        Message message = new Message ( Messages.CC_READ_OPERATION );
+        Message message = new Message ( Messages.CC_WRITE_OPERATION );
         
         message.getValues().put ( "item-name", new StringValue(itemName) );
         message.getValues().put ( "value", Messages.variantToValue(value) );
@@ -17,11 +18,11 @@ public class WriteOperation
         return message;
     }
 
-    public static void parse ( Message message, String itemName, Variant value )
+    public static void parse ( Message message, Holder<String> itemName, Holder<Variant> value )
     {
         // FIXME: handle missing item name
-        itemName = message.getValues().get ( "item-name" ).toString();
+        itemName.value = message.getValues().get ( "item-name" ).toString();
         
-        value = Messages.valueToVariant ( message.getValues().get("value"), new Variant() );
+        value.value = Messages.valueToVariant ( message.getValues().get("value"), new Variant() );
     }
 }
