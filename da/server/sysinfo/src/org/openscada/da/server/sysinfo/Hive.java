dfab2cd0fe120b26f874bf1323456027917faed0
Jens Reimann <jens.reimann@th4-systems.com>
2006-05-17 15:42:00 +0000

    added some testing stuff ... we really need a test server i guess
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis@266 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- da/server/sysinfo/src/org/openscada/da/server/sysinfo/Hive.java
+++ da/server/sysinfo/src/org/openscada/da/server/sysinfo/Hive.java
@@ -7,6 +7,8 @@ import org.openscada.da.core.common.DataItemCommand;
 import org.openscada.da.core.common.MemoryDataItem;
 import org.openscada.da.core.common.impl.HiveCommon;
 import org.openscada.da.core.data.Variant;
+import org.openscada.da.server.sysinfo.items.MemoryCellItem;
+import org.openscada.da.server.sysinfo.items.WriteDelayItem;
 import org.openscada.utils.timing.Scheduler;
 
 public class Hive extends HiveCommon {
@@ -35,10 +37,12 @@ public class Hive extends HiveCommon {
                 System.out.println ( "Command is: " + value.asString("<null>") );
             }});
         registerItem ( cmd );
-		
+        
 		registerItem ( new TimeDataItem("time", _scheduler) );
 		registerItem ( new PlainFileDataItem("hostname",new File("/proc/sys/kernel/hostname"), _scheduler, 1000 * 10 ) );
 		registerItem ( new PlainFileDataItem("file",new File("/tmp/da.txt"), _scheduler, 1000 ) );
+        registerItem ( new WriteDelayItem ( "write-delay" ) );
+        registerItem ( new MemoryCellItem ( this, "memory-cell" ) );
 		
 		_scheduler.addJob ( new LoadAverageJob(this), 1000 );
 	}
---
+++ da/server/sysinfo/src/org/openscada/da/server/sysinfo/items/WriteDelayItem.java
@@ -0,0 +1,48 @@
+package org.openscada.da.server.sysinfo.items;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.openscada.da.core.InvalidOperationException;
+import org.openscada.da.core.common.DataItemOutput;
+import org.openscada.da.core.data.NotConvertableException;
+import org.openscada.da.core.data.NullValueException;
+import org.openscada.da.core.data.Variant;
+
+public class WriteDelayItem extends DataItemOutput
+{
+
+    public WriteDelayItem ( String name )
+    {
+        super ( name );
+    }
+
+    public Map<String, Variant> getAttributes ()
+    {
+        return new HashMap<String, Variant>();
+    }
+
+    public void setAttributes ( Map<String, Variant> attributes )
+    {
+        // no op
+    }
+
+    public void setValue ( Variant value ) throws InvalidOperationException, NullValueException, NotConvertableException
+    {
+       int delay = value.asInteger ();
+       
+       System.out.println ( "Start write: " + delay + "ms" );
+       try
+       {
+           Thread.sleep ( delay );
+           System.out.println ( "End write" );
+       }
+       catch ( InterruptedException e )
+       {
+           System.err.println ( "Write failed" );
+           e.printStackTrace();
+       }
+       
+    }
+
+}
