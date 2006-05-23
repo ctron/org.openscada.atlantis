2f86766776ccdda6d16d780b60600bdc365970ce
Jens Reimann <jens.reimann@th4-systems.com>
2006-05-23 13:42:55 +0000

    make it look good
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis@314 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- da/server/net/src/org/openscada/da/server/net/Application.java
+++ da/server/net/src/org/openscada/da/server/net/Application.java
@@ -14,10 +14,8 @@ public class Application
 	
 	public static void main ( String[] args )
 	{
-		
 		try
         {
-			
             // check if we have a class name
 		    if ( args.length != 1 )
 		    {
