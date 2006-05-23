3aa99b464bfbffd420fd76549784e35fc8603daf
Jens Reimann <jens.reimann@th4-systems.com>
2006-05-23 15:54:15 +0000

    adding string encoder (passed test case)
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis@323 1b50e8ee-deed-0310-b1de-ab401e1c4d63

---
+++ utils/src/org/openscada/utils/lang/StringEncoder.java
@@ -0,0 +1,32 @@
+package org.openscada.utils.lang;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+
+public class StringEncoder
+{
+    public static String encode ( String s )
+    {
+        try
+        {
+            return URLEncoder.encode ( s, "utf-8" );
+        }
+        catch ( UnsupportedEncodingException e )
+        {
+            return s;
+        }    
+    }
+    
+    public static String decode ( String s )
+    {
+        try
+        {
+            return URLDecoder.decode ( s, "utf-8" );
+        }
+        catch ( UnsupportedEncodingException e )
+        {
+            return s;
+        }   
+    }
+}
---
+++ utils/src/org/openscada/utils/lang/test/EncoderTest.java
@@ -0,0 +1,50 @@
+package org.openscada.utils.lang.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.openscada.utils.lang.StringEncoder;
+
+public class EncoderTest
+{
+
+    @Test
+    public void testEncode ()
+    {
+        assertEquals ( "abc", StringEncoder.encode ( "abc" ) );
+        assertEquals ( "123", StringEncoder.encode ( "123" ) );
+        assertEquals ( "", StringEncoder.encode ( "" ) );
+        assertEquals ( "+", StringEncoder.encode ( " " ) );
+        assertEquals ( "+++", StringEncoder.encode ( "   " ) );
+        assertEquals ( "%2B", StringEncoder.encode ( "+" ) );
+    }
+
+    @Test
+    public void testDecode ()
+    {
+        assertEquals ( "abc", StringEncoder.decode ( "abc" ) );
+        assertEquals ( "123", StringEncoder.decode ( "123" ) );
+        assertEquals ( "", StringEncoder.decode ( "" ) );
+        assertEquals ( " ", StringEncoder.decode ( "+" ) );
+        assertEquals ( "   ", StringEncoder.decode ( "+++" ) );
+        assertEquals ( "+", StringEncoder.decode ( "%2B" ) );
+    }
+    
+    private void testEqual ( String string )
+    {
+        assertEquals ( string, StringEncoder.decode ( StringEncoder.encode ( string ) ));
+    }
+    
+    @Test
+    public void testBoth ()
+    {
+        testEqual ( "abc" );
+        testEqual ( "123" );
+        testEqual ( "" );
+        testEqual ( "%" );
+        testEqual ( "%20" );
+        testEqual ( " " );
+        testEqual ( "+" );
+    }
+
+}
