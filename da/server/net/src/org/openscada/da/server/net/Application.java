12743f96fcc1b531a456bbd74a0d0fd2485ba13e
Jens Reimann <jens.reimann@th4-systems.com>
2006-05-23 13:34:56 +0000

    make it look good
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis@310 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- da/server/net/src/org/openscada/da/server/net/Application.java
+++ da/server/net/src/org/openscada/da/server/net/Application.java
@@ -1,7 +1,13 @@
 package org.openscada.da.server.net;
 
 import org.apache.log4j.Logger;
+import java.lang.Class;
 
+/**
+ * Application to export a hive using the OpenSCADA NET protocol
+ * @author jens
+ *
+ */
 public class Application
 {
 	private static Logger _log = Logger.getLogger(Application.class);
@@ -9,22 +15,27 @@ public class Application
 	public static void main ( String[] args )
 	{
 		
-		try {
+		try
+        {
 			
+            // check if we have a class name
 		    if ( args.length != 1 )
 		    {
 		        System.err.println ( "syntax: Application <hiveClassName>" );
                 return;
 		    }
             
+            // create exporter
             Exporter exporter = new Exporter ( args[0] );
             
+            // run the lizzard
             _log.info ( "Running exporter (hive class: " + exporter.getHiveClass ().getCanonicalName () + ")..." );
 			exporter.run ();
             _log.warn ( "Exporter returned!" );
 		}
 		catch ( Exception e )
 		{
+            // ops
 			_log.fatal ( "Error in OpenSCADA DA[NET] Server", e );
 		}
 	}
