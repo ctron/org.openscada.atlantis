192ac9bd1b192d3736a6a40dc7e75ab3f650a034
Jens Reimann <jens.reimann@th4-systems.com>
2006-05-29 13:06:25 +0000

    make it look good
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis@365 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- net/base/src/org/openscada/net/io/ServerConnection.java
+++ net/base/src/org/openscada/net/io/ServerConnection.java
@@ -4,26 +4,29 @@ import org.apache.log4j.Logger;
 import org.openscada.net.base.ConnectionAware;
 import org.openscada.net.base.ConnectionHandler;
 
-public class ServerConnection extends Connection {
+public class ServerConnection extends Connection
+{
 	
-	private static Logger _log = Logger.getLogger(ServerConnection.class);
+	private static Logger _log = Logger.getLogger ( ServerConnection.class );
 	
 	private ConnectionHandler _handler; 
 	
-	public ServerConnection(ConnectionHandler handler, SocketConnection connection) {
-		super(handler, handler, connection);
+	public ServerConnection ( ConnectionHandler handler, SocketConnection connection )
+    {
+		super ( handler, handler, connection );
 		
 		_handler = handler;
 		if ( _handler instanceof ConnectionAware )
-			((ConnectionAware)_handler).setConnection(this);
+			( (ConnectionAware)_handler ).setConnection ( this );
 		
 		connection.setListener ( this );
 		connection.triggerRead ();
 	}
 
 	@Override
-	protected void finalize() throws Throwable {
-		_log.debug("Server connection finalized");
-		super.finalize();
+	protected void finalize() throws Throwable
+    {
+		_log.debug ( "Server connection finalized" );
+		super.finalize ();
 	}
 }
