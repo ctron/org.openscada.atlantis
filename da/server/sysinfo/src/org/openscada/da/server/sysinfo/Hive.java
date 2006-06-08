d49ab8fd4b59c7dc00e5763b77e941f6216b571b
Jens Reimann <jens.reimann@th4-systems.com>
2006-06-08 10:57:05 +0000

    added folder registration
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis@400 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- da/server/sysinfo/src/org/openscada/da/server/sysinfo/Hive.java
+++ da/server/sysinfo/src/org/openscada/da/server/sysinfo/Hive.java
@@ -3,10 +3,14 @@ package org.openscada.da.server.sysinfo;
 
 import java.io.File;
 
+import org.openscada.da.core.common.DataItem;
+import org.openscada.da.core.common.impl.FolderCommon;
 import org.openscada.da.core.common.impl.HiveCommon;
+import org.openscada.da.core.data.Variant;
 import org.openscada.da.server.sysinfo.items.LoadAverageJob;
 import org.openscada.da.server.sysinfo.items.PlainFileDataItem;
 import org.openscada.da.server.sysinfo.items.TimeDataItem;
+import org.openscada.utils.collection.MapBuilder;
 import org.openscada.utils.timing.Scheduler;
 
 public class Hive extends HiveCommon {
@@ -17,9 +21,28 @@ public class Hive extends HiveCommon {
 	{
 		super();
         
-		registerItem ( new TimeDataItem("time", _scheduler) );
-		registerItem ( new PlainFileDataItem("hostname",new File("/proc/sys/kernel/hostname"), _scheduler, 1000 * 10 ) );
+        FolderCommon rootFolder = new FolderCommon ();
+        setRootFolder ( rootFolder );
+        
+        DataItem item;
+		registerItem ( item = new TimeDataItem ( "time", _scheduler ) );
+        rootFolder.add ( "time", item, new MapBuilder<String, Variant> ()
+                .put ( "description", new Variant ( "Time since the epoc in milliseconds!" ) )
+                .getMap ()
+        );
+        
+		registerItem ( item = new PlainFileDataItem ( "hostname", new File ( "/proc/sys/kernel/hostname" ), _scheduler, 1000 * 10 ) );
+        rootFolder.add ( "hostname", item, new MapBuilder<String, Variant> ()
+                .put ( "description", new Variant ( "Hostname of the computer the server is running on." ) )
+                .getMap ()
+        );
 		
-		_scheduler.addJob ( new LoadAverageJob(this), 1000 );
+        FolderCommon loadFolder = new FolderCommon ();
+        rootFolder.add ( "loadavg", loadFolder, new MapBuilder<String, Variant> ()
+                .put ( "description", new Variant ( "Load avarage information" ) )
+                .getMap ()
+        );
+		_scheduler.addJob ( new LoadAverageJob ( this, loadFolder ), 1000 );
+        
 	}
 }
--- da/server/sysinfo/src/org/openscada/da/server/sysinfo/items/LoadAverageJob.java
+++ da/server/sysinfo/src/org/openscada/da/server/sysinfo/items/LoadAverageJob.java
@@ -5,31 +5,50 @@ import java.io.IOException;
 import java.util.StringTokenizer;
 
 import org.openscada.da.core.common.DataItemInputCommon;
+import org.openscada.da.core.common.impl.FolderCommon;
 import org.openscada.da.core.data.Variant;
 import org.openscada.da.server.sysinfo.Hive;
 import org.openscada.da.server.sysinfo.utils.FileUtils;
+import org.openscada.utils.collection.MapBuilder;
 
 public class LoadAverageJob implements Runnable {
 
-	private File _file = new File ("/proc/loadavg");
+	private File _file = new File ( "/proc/loadavg" );
 	
 	private DataItemInputCommon _avg1 = new DataItemInputCommon("loadavg1");
 	private DataItemInputCommon _avg5 = new DataItemInputCommon("loadavg5");
 	private DataItemInputCommon _avg15 = new DataItemInputCommon("loadavg15");
 	
-	private Hive _hive;
+	private Hive _hive = null;
+    private FolderCommon _folder = null;
 	
-	public LoadAverageJob ( Hive hive )
+	public LoadAverageJob ( Hive hive, FolderCommon folder )
 	{
 		_hive = hive;
+        _folder = folder;
 		
-		_hive.registerItem(_avg1);
-		_hive.registerItem(_avg5);
-		_hive.registerItem(_avg15);
+		_hive.registerItem ( _avg1 );
+        _folder.add ( "1", _avg1, new MapBuilder<String, Variant>()
+                .put ( "description", new Variant ( "The 1 minute load avarage" ) )
+                .getMap ()
+        );
+        
+		_hive.registerItem ( _avg5 );
+        _folder.add ( "5", _avg5, new MapBuilder<String, Variant>()
+                .put ( "description", new Variant ( "The 5 minute load avarage" ) )
+                .getMap ()
+        );
+        
+		_hive.registerItem ( _avg15 );
+        _folder.add ( "15", _avg15, new MapBuilder<String, Variant>()
+                .put ( "description", new Variant ( "The 15 minute load avarage" ) )
+                .getMap ()
+        );
 	}
 	
 	public void run() {
-		try {
+		try
+        {
 			read ();
 		}
 		catch ( Exception e )
@@ -44,8 +63,8 @@ public class LoadAverageJob implements Runnable {
 		
 		StringTokenizer tok = new StringTokenizer ( data[0] );
 		
-		_avg1.updateValue ( new Variant ( Double.parseDouble(tok.nextToken())) );
-		_avg5.updateValue ( new Variant ( Double.parseDouble(tok.nextToken())) );
-		_avg15.updateValue ( new Variant ( Double.parseDouble(tok.nextToken())) );
+		_avg1.updateValue ( new Variant ( Double.parseDouble ( tok.nextToken() ) ) );
+		_avg5.updateValue ( new Variant ( Double.parseDouble ( tok.nextToken() ) ) );
+		_avg15.updateValue ( new Variant ( Double.parseDouble ( tok.nextToken() ) ) );
 	}
 }
