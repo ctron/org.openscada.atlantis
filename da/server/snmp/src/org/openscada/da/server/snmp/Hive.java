d1e453f784ac794e573bdd17d05d16c9e9958fe7
Jens Reimann <jens.reimann@th4-systems.com>
2006-06-15 15:12:25 +0000

    cleanup
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis@450 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- da/server/snmp/src/org/openscada/da/server/snmp/Hive.java
+++ da/server/snmp/src/org/openscada/da/server/snmp/Hive.java
@@ -7,14 +7,12 @@ import java.util.Map;
 import org.apache.log4j.Logger;
 import org.openscada.da.core.browser.common.FolderCommon;
 import org.openscada.da.core.common.impl.HiveCommon;
-import org.openscada.utils.timing.Scheduler;
 
 public class Hive extends HiveCommon
 {
+    @SuppressWarnings("unused")
     private static Logger _log = Logger.getLogger ( Hive.class );
     
-	private Scheduler _scheduler = new Scheduler();
-	
     private Map<String, SNMPNode> _nodeMap = new HashMap<String, SNMPNode> ();
     
 	public Hive ()
