e36598101e15ad5667991b69474defd4419d70a2
Jens Reimann <jens.reimann@th4-systems.com>
2006-07-04 13:38:59 +0000

    LGPLified
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis@500 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- net/base/src/org/openscada/net/test/Application.java
+++ net/base/src/org/openscada/net/test/Application.java
@@ -1,3 +1,22 @@
+/*
+ * This file is part of the OpenSCADA project
+ * Copyright (C) 2006 inavare GmbH (http://inavare.com)
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 package org.openscada.net.test;
 
 import java.net.InetAddress;
@@ -14,7 +33,8 @@ import org.openscada.net.io.Server;
 
 public class Application {
 	
-	private static Logger _log = Logger.getLogger(Application.class);
+	@SuppressWarnings("unused")
+    private static Logger _log = Logger.getLogger(Application.class);
 	
 	public static void main(String[] args) {
 		try {
