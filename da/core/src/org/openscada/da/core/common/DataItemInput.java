a37c2b25122178c42f407d0c070c1b0371220f47
Jens Reimann <jens.reimann@th4-systems.com>
2006-08-21 15:15:15 +0000

    clean up
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis@837 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- da/core/src/org/openscada/da/core/common/DataItemInput.java
+++ da/core/src/org/openscada/da/core/common/DataItemInput.java
@@ -27,7 +27,8 @@ import org.openscada.da.core.data.NotConvertableException;
 import org.openscada.da.core.data.NullValueException;
 import org.openscada.da.core.data.Variant;
 
-public abstract class DataItemInput extends DataItemBase {
+public abstract class DataItemInput extends DataItemBase
+{
 
 	public DataItemInput ( String name )
     {
