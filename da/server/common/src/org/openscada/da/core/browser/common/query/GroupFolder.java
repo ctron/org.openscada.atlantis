436cc5c254034f639ab47a3646bd2123818c8ae7
Jens Reimann <jens.reimann@th4-systems.com>
2006-08-28 11:22:13 +0000

    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis@916 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- da/server/common/src/org/openscada/da/core/browser/common/query/GroupFolder.java
+++ da/server/common/src/org/openscada/da/core/browser/common/query/GroupFolder.java
@@ -23,9 +23,9 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Stack;
 
+import org.openscada.da.core.Location;
 import org.openscada.da.core.browser.common.FolderListener;
 import org.openscada.da.core.server.browser.Entry;
-import org.openscada.da.core.server.browser.Location;
 import org.openscada.da.core.server.browser.NoSuchFolderException;
 
 public class GroupFolder implements StorageBasedFolder
--- da/server/common/src/org/openscada/da/core/common/impl/HiveBrowserCommon.java
+++ da/server/common/src/org/openscada/da/core/common/impl/HiveBrowserCommon.java
@@ -25,13 +25,13 @@ import java.util.Map;
 import java.util.Stack;
 
 import org.apache.log4j.Logger;
+import org.openscada.da.core.Location;
 import org.openscada.da.core.browser.common.Folder;
 import org.openscada.da.core.browser.common.FolderListener;
 import org.openscada.da.core.server.InvalidSessionException;
 import org.openscada.da.core.server.Session;
 import org.openscada.da.core.server.browser.Entry;
 import org.openscada.da.core.server.browser.HiveBrowser;
-import org.openscada.da.core.server.browser.Location;
 import org.openscada.da.core.server.browser.NoSuchFolderException;
 
 public abstract class HiveBrowserCommon implements HiveBrowser, FolderListener, SessionListener
