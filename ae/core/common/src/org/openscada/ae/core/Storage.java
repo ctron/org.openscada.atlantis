9ce2e836e94c06bcfdd14d21c22513a7f2b0e8ea
Jens Reimann <jens.reimann@th4-systems.com>
2006-08-29 14:21:27 +0000

    added invalid session exception
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis@946 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- ae/core/common/src/org/openscada/ae/core/Storage.java
+++ ae/core/common/src/org/openscada/ae/core/Storage.java
@@ -2,16 +2,18 @@ package org.openscada.ae.core;
 
 import java.util.Properties;
 
+import org.openscada.core.InvalidSessionException;
+
 public interface Storage
 {
     public Session createSession ( Properties properties );
-    public void closeSession ( Session session );
+    public void closeSession ( Session session ) throws InvalidSessionException;
     
-    void subscribe ( Session session, String queryID, Listener listener );
-    void unsubscribe ( Session session, String queryID );
-    Event[] read ( Session session, String queryID );
+    void subscribe ( Session session, String queryID, Listener listener ) throws InvalidSessionException;
+    void unsubscribe ( Session session, String queryID ) throws InvalidSessionException;
+    Event[] read ( Session session, String queryID ) throws InvalidSessionException;
 
-    QueryDescription[] getQueries ( Session session );
+    QueryDescription[] getQueries ( Session session ) throws InvalidSessionException;
     
     public void submitEvent ( AttributedIdentifier event );
 }
