ae52b1b81299be60d8e6199ff2a9280d653330d9
Jens Reimann <jens.reimann@th4-systems.com>
2006-08-29 14:10:10 +0000

    ignore warnings
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis@943 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- ae/core/common/src/org/openscada/ae/core/QueryDescription.java
+++ ae/core/common/src/org/openscada/ae/core/QueryDescription.java
@@ -1,10 +1,8 @@
 package org.openscada.ae.core;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import org.openscada.core.Variant;
-import org.openscada.core.utils.AttributesHelper;
 
 public class QueryDescription extends AttributedIdentifier
 {
