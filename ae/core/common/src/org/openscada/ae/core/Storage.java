301bf5e544d05881c58383e6eba7cc8987a7a448
Jens Reimann <jens.reimann@th4-systems.com>
2006-08-29 14:11:12 +0000

    keep it clear for pre java 5
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis@944 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- ae/core/common/src/org/openscada/ae/core/Storage.java
+++ ae/core/common/src/org/openscada/ae/core/Storage.java
@@ -1,6 +1,5 @@
 package org.openscada.ae.core;
 
-import java.util.Collection;
 import java.util.Properties;
 
 public interface Storage
@@ -10,9 +9,9 @@ public interface Storage
     
     void subscribe ( Session session, String queryID, Listener listener );
     void unsubscribe ( Session session, String queryID );
-    Collection<Event> read ( Session session, String queryID );
+    Event[] read ( Session session, String queryID );
 
-    Collection<QueryDescription> getQueries ( Session session );
+    QueryDescription[] getQueries ( Session session );
     
     public void submitEvent ( AttributedIdentifier event );
 }
