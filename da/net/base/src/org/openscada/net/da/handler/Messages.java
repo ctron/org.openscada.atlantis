8f45f5c0bf29cddfd21bf6e458806419858442d7
Jens Reimann <jens.reimann@th4-systems.com>
2006-08-30 16:31:40 +0000

    convenience extend
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis@990 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- da/net/base/src/org/openscada/net/da/handler/Messages.java
+++ da/net/base/src/org/openscada/net/da/handler/Messages.java
@@ -39,7 +39,7 @@ import org.openscada.net.base.data.StringValue;
 import org.openscada.net.base.data.Value;
 import org.openscada.net.base.data.VoidValue;
 
-public class Messages
+public class Messages extends org.openscada.core.net.MessageHelper
 {
 	public final static int CC_CREATE_SESSION =                     0x00010001;
 	public final static int CC_CLOSE_SESSION =                      0x00010002;
@@ -101,98 +101,6 @@ public class Messages
         Message msg = new Message ( CC_UNSUBSCRIBE_ITEM );
         msg.getValues().put ( "item-name", new StringValue(itemName) );
         return msg;
-    }
-    
-    public static Variant valueToVariant ( Value value, Variant defaultValue )
-    {
-        if ( value == null )
-            return defaultValue;
-        
-        if ( value instanceof StringValue )
-            return new Variant ( ((StringValue)value).getValue () );
-        else if ( value instanceof BooleanValue )
-            return new Variant ( ((BooleanValue)value).getValue () );
-        else if ( value instanceof DoubleValue )
-            return new Variant ( ((DoubleValue)value).getValue () );
-        else if ( value instanceof LongValue )
-            return new Variant ( ((LongValue)value).getValue () );
-        else if ( value instanceof IntegerValue )
-            return new Variant ( ((IntegerValue)value).getValue () );
-        else if ( value instanceof VoidValue )
-            return new Variant ();
-        
-        return defaultValue;
-    }
-    
-    public static Value variantToValue ( Variant value )
-    {
-        if ( value == null )
-            return null;
-        
-        try {
-            if ( value.isDouble () )
-                return new DoubleValue ( value.asDouble () );
-            else if ( value.isInteger () )
-                return new IntegerValue ( value.asInteger () );
-            else if ( value.isLong () )
-                return new LongValue ( value.asLong () );
-            else if ( value.isBoolean () )
-                return new BooleanValue ( value.asBoolean () );
-            else if ( value.isString () )
-                return new StringValue ( value.asString () );
-            else if ( value.isNull () )
-                return new VoidValue ();
-        }
-        catch ( NullValueException e )
-        {
-            return new VoidValue ();
-        }
-        catch ( NotConvertableException e )
-        {
-        }
-        return null;
-    }
-    
-    /**
-     * Convert a MapValue to a attributes map
-     * @param mapValue the map value to convert
-     * @return the attributes map
-     * @note Only scalar entries in the map are converted. Other values are skipped.
-     */
-    public static Map<String, Variant> mapToAttributes ( MapValue mapValue )
-    {
-        Map<String, Variant> attributes = new HashMap<String, Variant> ();
-        
-        for ( Map.Entry<String, Value> entry : mapValue.getValues ().entrySet () )
-        {
-            Variant value = null;
-            Value entryValue = entry.getValue ();
-            
-            value = valueToVariant ( entryValue, null );
-            
-            if ( value != null )
-            {
-                attributes.put ( new String ( entry.getKey () ) , value );
-            }
-        }
-        
-        return attributes;
-    }
-    
-    public static MapValue attributesToMap ( Map<String, Variant> attributes )
-    {
-        MapValue mapValue = new MapValue ();
-        
-        for ( Map.Entry<String, Variant> entry : attributes.entrySet () )
-        {
-            Value value = variantToValue ( entry.getValue () );
-            if ( value != null )
-            {
-                mapValue.put ( new String ( entry.getKey () ), value );
-            }
-        }
-        
-        return mapValue;
     }
     
     public static Message notifyValue ( String itemName, Variant value, boolean initial )
