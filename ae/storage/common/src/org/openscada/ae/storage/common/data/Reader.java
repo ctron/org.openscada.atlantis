505e67b9acc6b484ad22032bc1cfcead7c21809f
Jens Reimann <jens.reimann@th4-systems.com>
2006-08-30 09:23:00 +0000

    removed unused stuff
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis@953 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- ae/storage/common/src/org/openscada/ae/storage/common/data/Reader.java
+++
@@ -1,8 +0,0 @@
-package org.openscada.ae.storage.common.data;
-
-import org.openscada.ae.core.Event;
-
-public interface Reader
-{
-    Event [] readNext ( int maxCount );
-}
--- ae/storage/common/src/org/openscada/ae/storage/common/data/StorageModule.java
+++
@@ -1,7 +0,0 @@
-package org.openscada.ae.storage.common.data;
-
-public interface StorageModule
-{
-    Reader startRead ( String configuration );
-    
-}
