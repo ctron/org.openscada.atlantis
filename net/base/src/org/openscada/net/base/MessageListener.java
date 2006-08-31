e507e57572c5e2885a75dcb83488c52e8054c54f
Jens Reimann <jens.reimann@th4-systems.com>
2006-08-31 11:43:50 +0000

    allow exceptions
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis@1009 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- net/base/src/org/openscada/net/base/MessageListener.java
+++ net/base/src/org/openscada/net/base/MessageListener.java
@@ -22,6 +22,7 @@ package org.openscada.net.base;
 import org.openscada.net.base.data.Message;
 import org.openscada.net.io.net.Connection;
 
-public interface MessageListener {
-	public void messageReceived ( Connection connection, Message message ); 
+public interface MessageListener
+{
+	public void messageReceived ( Connection connection, Message message ) throws Exception;
 }
