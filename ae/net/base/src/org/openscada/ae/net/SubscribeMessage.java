55beaf53845d87fe5e61ec4845ce0ffeac66aa61
Jens Reimann <jens.reimann@th4-systems.com>
2006-08-31 15:17:02 +0000

    added additional parameters
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis@1034 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- ae/net/base/src/org/openscada/ae/net/SubscribeMessage.java
+++ ae/net/base/src/org/openscada/ae/net/SubscribeMessage.java
@@ -1,11 +1,14 @@
 package org.openscada.ae.net;
 
+import org.openscada.net.base.data.IntegerValue;
 import org.openscada.net.base.data.Message;
 import org.openscada.net.base.data.StringValue;
 
 public class SubscribeMessage
 {
     private String _queryId = null;
+    private int _maxBatchSize = 0;
+    private int _archiveSet = 0;
 
     public String getQueryId ()
     {
@@ -17,17 +20,43 @@ public class SubscribeMessage
         _queryId = queryId;
     }
     
+    public int getArchiveSet ()
+    {
+        return _archiveSet;
+    }
+
+    public void setArchiveSet ( int archiveSet )
+    {
+        _archiveSet = archiveSet;
+    }
+
+    public int getMaxBatchSize ()
+    {
+        return _maxBatchSize;
+    }
+
+    public void setMaxBatchSize ( int maxBatchSize )
+    {
+        _maxBatchSize = maxBatchSize;
+    }
+
     public Message toMessage ()
     {
         Message message = new Message ( Messages.CC_SUBSCRIBE );
         message.getValues ().put ( "query-id", new StringValue ( _queryId ) );
+        message.getValues ().put ( "max-batch-size", new IntegerValue ( _maxBatchSize ) );
+        message.getValues ().put ( "archive-set", new IntegerValue ( _archiveSet ) );
         return message;
     }
     
     public static SubscribeMessage fromMessage ( Message message )
     {
-        SubscribeMessage subscribeMessge = new SubscribeMessage ();
-        subscribeMessge.setQueryId ( message.getValues ().get ( "query-id" ).toString () );
-        return subscribeMessge;
+        SubscribeMessage subscribeMessage = new SubscribeMessage ();
+        subscribeMessage.setQueryId ( message.getValues ().get ( "query-id" ).toString () );
+        if ( message.getValues ().containsKey ( "max-batch-size" ) )
+            subscribeMessage.setMaxBatchSize ( ((IntegerValue)message.getValues ().get ( "max-batch-size" )).getValue () );
+        if ( message.getValues ().containsKey ( "archive-set" ) )
+            subscribeMessage.setArchiveSet ( ((IntegerValue)message.getValues ().get ( "archive-set" )).getValue () );
+        return subscribeMessage;
     }
 }
