678426a6b63023b8fa87f8bef01f93dc7335a9f5
Jens Reimann <jens.reimann@th4-systems.com>
2006-09-01 13:23:00 +0000

    initial import
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis@1071 1b50e8ee-deed-0310-b1de-ab401e1c4d63

---
+++ ae/client/test/src/org/openscada/ae/client/test/PerspectiveFactory.java
@@ -0,0 +1,13 @@
+package org.openscada.ae.client.test;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class PerspectiveFactory implements IPerspectiveFactory
+{
+
+    public void createInitialLayout ( IPageLayout layout )
+    {
+    }
+
+}
