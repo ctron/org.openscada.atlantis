7cbe3f1bbae682816e34c227def78587ee5211a7
Jens Reimann <jens.reimann@th4-systems.com>
2006-09-05 15:41:36 +0000

    aded sample configuration
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis@1097 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- ae/storage/syslog/src/org/openscada/ae/storage/syslog/Storage.java
+++ ae/storage/syslog/src/org/openscada/ae/storage/syslog/Storage.java
@@ -1,18 +1,24 @@
 package org.openscada.ae.storage.syslog;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.SocketException;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Properties;
 
 import org.apache.log4j.Logger;
+import org.apache.xmlbeans.XmlException;
 import org.openscada.ae.core.Event;
 import org.openscada.ae.core.QueryDescription;
 import org.openscada.ae.storage.common.StorageCommon;
 import org.openscada.ae.storage.common.memory.MemoryQuery;
 import org.openscada.ae.storage.syslog.provider.SyslogDaemonProvider;
 import org.openscada.ae.storage.syslog.provider.SyslogFileProvider;
+import org.openscada.ae.syslog.ConfigurationDocument;
+import org.openscada.ae.syslog.FileProviderType;
+import org.openscada.ae.syslog.ProvidersType;
+import org.openscada.ae.syslog.SyslogUdpProviderType;
 import org.openscada.core.Variant;
 import org.openscada.utils.collection.MapBuilder;
 
@@ -24,7 +30,7 @@ public class Storage extends StorageCommon implements DataStore
     
     private List<Object> _providers = new LinkedList<Object> ();
     
-    public Storage () throws SocketException
+    public Storage () throws XmlException, IOException
     {
         super ();
         
@@ -32,6 +38,8 @@ public class Storage extends StorageCommon implements DataStore
                 .put ( "description", new Variant ( "A query containing all items" ) )
                 .getMap () ), _allQuery );
     
+        configure ();
+        
         _providers.add ( new SyslogFileProvider ( this, new File ( "/var/log/syslog" ), "INFO" ) );
         _providers.add ( new SyslogFileProvider ( this, new File ( "/var/log/auth.log" ), "INFO" ) );
         _providers.add ( new SyslogFileProvider ( this, new File ( "/var/log/daemon.log" ), "INFO" ) );
@@ -39,6 +47,26 @@ public class Storage extends StorageCommon implements DataStore
         _providers.add ( new SyslogFileProvider ( this, new File ( "/var/log/debug" ), "DEBUG" ) );
         
         _providers.add ( new SyslogDaemonProvider ( this, 1402 ) );
+    }
+    
+    private void configure () throws XmlException, IOException
+    {
+        String fileName = System.getProperty ( "org.openscada.ae.syslog.configuration", "configuration.xml" );
+        
+        File file = new File ( fileName );
+        if ( file.canRead () )
+        {
+            ConfigurationDocument doc = ConfigurationDocument.Factory.parse ( file );
+            for ( FileProviderType fileProvider : doc.getConfiguration ().getProviders ().getFileProviderList () )
+            {
+                _providers.add ( new SyslogFileProvider ( this, new File ( fileProvider.getFile () ), fileProvider.getDefaultSeverity ().toString () ) );
+            }
+            for ( SyslogUdpProviderType udpProvider : doc.getConfiguration ().getProviders ().getSyslogUdpProviderList () )
+            {
+                _providers.add ( new SyslogDaemonProvider ( this, udpProvider.getPort () ) );
+            }
+
+        }
     }
 
     @Override
