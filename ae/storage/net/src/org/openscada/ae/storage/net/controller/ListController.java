e1ff3a79aba2c3b109f06a2e0611e47e7df029d9
Jens Reimann <jens.reimann@th4-systems.com>
2006-09-15 14:41:25 +0000

    organized imports and added LGPL license header
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis@1200 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- ae/storage/net/src/org/openscada/ae/storage/net/controller/ListController.java
+++ ae/storage/net/src/org/openscada/ae/storage/net/controller/ListController.java
@@ -1,3 +1,22 @@
+/*
+ * This file is part of the OpenSCADA project
+ * Copyright (C) 2006 inavare GmbH (http://inavare.com)
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 package org.openscada.ae.storage.net.controller;
 
 import java.util.Arrays;
@@ -12,12 +31,10 @@ import org.openscada.ae.net.ListReplyMessage;
 import org.openscada.ae.net.Messages;
 import org.openscada.core.InvalidSessionException;
 import org.openscada.core.net.OperationController;
-import org.openscada.net.base.ConnectionHandler;
 import org.openscada.net.base.ConnectionHandlerBase;
 import org.openscada.net.base.data.LongValue;
 import org.openscada.net.base.data.Message;
 import org.openscada.net.base.data.StringValue;
-import org.openscada.net.utils.MessageCreator;
 
 public class ListController extends OperationController implements ListOperationListener
 {
