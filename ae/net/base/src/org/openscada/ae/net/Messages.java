c58b59c36413caaec1036e03bc978051b7742de8
Jens Reimann <jens.reimann@th4-systems.com>
2006-09-15 14:38:49 +0000

    organized imports and added LGPL license header
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis@1198 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- ae/net/base/src/org/openscada/ae/net/Messages.java
+++ ae/net/base/src/org/openscada/ae/net/Messages.java
@@ -1,3 +1,22 @@
+/*
+ * This file is part of the OpenSCADA project
+ * Copyright (C) 2006 inavare GmbH (http://inavare.com)
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 package org.openscada.ae.net;
 
 import java.util.Calendar;
@@ -5,14 +24,11 @@ import java.util.Calendar;
 import org.openscada.ae.core.Event;
 import org.openscada.ae.core.EventInformation;
 import org.openscada.core.net.MessageHelper;
-import org.openscada.core.utils.AttributesHelper;
 import org.openscada.net.base.data.IntegerValue;
 import org.openscada.net.base.data.LongValue;
 import org.openscada.net.base.data.MapValue;
 import org.openscada.net.base.data.StringValue;
 import org.openscada.net.base.data.Value;
-
-import sun.util.calendar.CalendarUtils;
 
 public class Messages
 {
