2732ce9cf15cfaf51b1b2aa6af7765c64fad2cc0
Jens Reimann <jens.reimann@th4-systems.com>
2006-09-15 14:42:38 +0000

    organized imports and added LGPL license header
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis@1201 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- ae/storage/syslog/src/org/openscada/ae/storage/syslog/Storage.java
+++ ae/storage/syslog/src/org/openscada/ae/storage/syslog/Storage.java
@@ -1,9 +1,27 @@
+/*
+ * This file is part of the OpenSCADA project
+ * Copyright (C) 2006 inavare GmbH (http://inavare.com)
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 package org.openscada.ae.storage.syslog;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.net.SocketAddress;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Properties;
@@ -21,8 +39,6 @@ import org.openscada.ae.syslog.FileProviderType;
 import org.openscada.ae.syslog.SyslogUdpProviderType;
 import org.openscada.core.Variant;
 import org.openscada.utils.collection.MapBuilder;
-
-import com.sun.corba.se.spi.servicecontext.SendingContextServiceContext;
 
 public class Storage extends StorageCommon implements DataStore
 {
