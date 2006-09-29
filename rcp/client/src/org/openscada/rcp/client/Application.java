2a37891f541cc6694ab59a6cab28627fa288e3f2
Jens Reimann <jens.reimann@th4-systems.com>
2006-09-29 15:13:38 +0000

    reformat code
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis@1264 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- rcp/client/src/org/openscada/rcp/client/Application.java
+++ rcp/client/src/org/openscada/rcp/client/Application.java
@@ -1,20 +1,15 @@
 /*
- * This file is part of the OpenSCADA project
- * Copyright (C) 2006 inavare GmbH (http://inavare.com)
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
-
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
-
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * This file is part of the OpenSCADA project Copyright (C) 2006 inavare GmbH
+ * (http://inavare.com) This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version. This program is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
+ * the GNU General Public License for more details. You should have received a
+ * copy of the GNU General Public License along with this program; if not, write
+ * to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA.
  */
 
 package org.openscada.rcp.client;
@@ -26,21 +21,29 @@ import org.eclipse.ui.PlatformUI;
 /**
  * This class controls all aspects of the application's execution
  */
-public class Application implements IPlatformRunnable {
+public class Application implements IPlatformRunnable
+{
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.core.runtime.IPlatformRunnable#run(java.lang.Object)
-	 */
-	public Object run(Object args) throws Exception {
-		Display display = PlatformUI.createDisplay();
-		try {
-			int returnCode = PlatformUI.createAndRunWorkbench(display, new ApplicationWorkbenchAdvisor());
-			if (returnCode == PlatformUI.RETURN_RESTART) {
-				return IPlatformRunnable.EXIT_RESTART;
-			}
-			return IPlatformRunnable.EXIT_OK;
-		} finally {
-			display.dispose();
-		}
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.core.runtime.IPlatformRunnable#run(java.lang.Object)
+     */
+    public Object run ( Object args ) throws Exception
+    {
+        Display display = PlatformUI.createDisplay ();
+        try
+        {
+            int returnCode = PlatformUI.createAndRunWorkbench ( display, new ApplicationWorkbenchAdvisor () );
+            if ( returnCode == PlatformUI.RETURN_RESTART )
+            {
+                return IPlatformRunnable.EXIT_RESTART;
+            }
+            return IPlatformRunnable.EXIT_OK;
+        }
+        finally
+        {
+            display.dispose ();
+        }
+    }
 }
