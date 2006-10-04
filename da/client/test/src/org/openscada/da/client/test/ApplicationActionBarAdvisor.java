d09a76c5488d8e59f7f773de0bf84b686e1c395a
Jens Reimann <jens.reimann@th4-systems.com>
2006-10-04 14:52:23 +0000

    reformat code
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis@1273 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- da/client/test/src/org/openscada/da/client/test/ApplicationActionBarAdvisor.java
+++ da/client/test/src/org/openscada/da/client/test/ApplicationActionBarAdvisor.java
@@ -36,70 +36,74 @@ import org.eclipse.ui.application.ActionBarAdvisor;
 import org.eclipse.ui.application.IActionBarConfigurer;
 
 /**
- * An action bar advisor is responsible for creating, adding, and disposing of the
- * actions added to a workbench window. Each window will be populated with
- * new actions.
+ * An action bar advisor is responsible for creating, adding, and disposing of the actions added to a workbench window.
+ * Each window will be populated with new actions.
  */
-public class ApplicationActionBarAdvisor extends ActionBarAdvisor {
+public class ApplicationActionBarAdvisor extends ActionBarAdvisor
+{
 
     // Actions - important to allocate these only in makeActions, and then use them
-    // in the fill methods.  This ensures that the actions aren't recreated
+    // in the fill methods. This ensures that the actions aren't recreated
     // when fillActionBars is called with FILL_PROXY.
     private IWorkbenchAction exitAction;
     private IWorkbenchAction aboutAction;
     private IWorkbenchAction newWindowAction;
-    
 
-    public ApplicationActionBarAdvisor(IActionBarConfigurer configurer) {
-        super(configurer);
+    public ApplicationActionBarAdvisor ( IActionBarConfigurer configurer )
+    {
+        super ( configurer );
     }
-    
-    protected void makeActions(final IWorkbenchWindow window) {
+
+    protected void makeActions ( final IWorkbenchWindow window )
+    {
         // Creates the actions and registers them.
         // Registering is needed to ensure that key bindings work.
         // The corresponding commands keybindings are defined in the plugin.xml file.
         // Registering also provides automatic disposal of the actions when
         // the window is closed.
 
-        exitAction = ActionFactory.QUIT.create(window);
-        register(exitAction);
-        
-        aboutAction = ActionFactory.ABOUT.create(window);
-        register(aboutAction);
-        
-        newWindowAction = ActionFactory.OPEN_NEW_WINDOW.create(window);
-        register(newWindowAction);
-        
-        register(ActionFactory.SHOW_VIEW_MENU.create(window));
-        register(ActionFactory.NEW_WIZARD_DROP_DOWN.create(window));
-        register(ActionFactory.NEW.create(window));
+        exitAction = ActionFactory.QUIT.create ( window );
+        register ( exitAction );
+
+        aboutAction = ActionFactory.ABOUT.create ( window );
+        register ( aboutAction );
+
+        newWindowAction = ActionFactory.OPEN_NEW_WINDOW.create ( window );
+        register ( newWindowAction );
+
+        register ( ActionFactory.NEW_EDITOR.create ( window ) );
+
+        register ( ActionFactory.SHOW_VIEW_MENU.create ( window ) );
+        register ( ActionFactory.NEW_WIZARD_DROP_DOWN.create ( window ) );
+        register ( ActionFactory.NEW.create ( window ) );
     }
-    
-    protected void fillMenuBar(IMenuManager menuBar) {
-        MenuManager fileMenu = new MenuManager("&File", IWorkbenchActionConstants.M_FILE);
-        MenuManager helpMenu = new MenuManager("&Help", IWorkbenchActionConstants.M_HELP);
-        
-        menuBar.add(fileMenu);
+
+    protected void fillMenuBar ( IMenuManager menuBar )
+    {
+        MenuManager fileMenu = new MenuManager ( "&File", IWorkbenchActionConstants.M_FILE );
+        MenuManager helpMenu = new MenuManager ( "&Help", IWorkbenchActionConstants.M_HELP );
+
+        menuBar.add ( fileMenu );
         // Add a group marker indicating where action set menus will appear.
-        menuBar.add(new GroupMarker(IWorkbenchActionConstants.MB_ADDITIONS));
-        menuBar.add(helpMenu);
-        
-       
-               
+        menuBar.add ( new GroupMarker ( IWorkbenchActionConstants.MB_ADDITIONS ) );
+        menuBar.add ( helpMenu );
+
         // File
-        fileMenu.add(newWindowAction);
-        fileMenu.add(new Separator());
-        fileMenu.add(getAction(ActionFactory.NEW.getId()));
-        fileMenu.add(new Separator());
-        fileMenu.add(exitAction);
-        
+        fileMenu.add ( newWindowAction );
+        fileMenu.add ( getAction ( ActionFactory.NEW_EDITOR.getId () ) );
+        fileMenu.add ( new Separator () );
+        fileMenu.add ( getAction ( ActionFactory.NEW.getId () ) );
+        fileMenu.add ( new Separator () );
+        fileMenu.add ( exitAction );
+
         // Help
-        helpMenu.add(aboutAction);
+        helpMenu.add ( aboutAction );
     }
-    
-    protected void fillCoolBar(ICoolBarManager coolBar) {
-        IToolBarManager toolbar = new ToolBarManager(SWT.FLAT | SWT.RIGHT);
-        coolBar.add(new ToolBarContributionItem(toolbar, "main"));
-        toolbar.add(getAction(ActionFactory.NEW_WIZARD_DROP_DOWN.getId()));
+
+    protected void fillCoolBar ( ICoolBarManager coolBar )
+    {
+        IToolBarManager toolbar = new ToolBarManager ( SWT.FLAT | SWT.RIGHT );
+        coolBar.add ( new ToolBarContributionItem ( toolbar, "main" ) );
+        toolbar.add ( getAction ( ActionFactory.NEW_WIZARD_DROP_DOWN.getId () ) );
     }
 }
