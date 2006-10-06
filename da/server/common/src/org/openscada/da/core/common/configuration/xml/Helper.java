8c46a5ebdf478037407fa4141f6644b6c9099b83
Jens Reimann <jens.reimann@th4-systems.com>
2006-10-06 20:43:58 +0000

    re-organized method in order to copy-reuse it ... should have a common xml config base
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis@1277 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- da/server/common/src/org/openscada/da/core/common/configuration/xml/Helper.java
+++ da/server/common/src/org/openscada/da/core/common/configuration/xml/Helper.java
@@ -5,11 +5,44 @@ import java.util.Map;
 
 import org.openscada.common.AttributeType;
 import org.openscada.common.AttributesType;
+import org.openscada.common.VariantType;
 import org.openscada.core.Variant;
 import org.openscada.da.core.common.configuration.ConfigurationError;
 
 public class Helper
 {
+    public static Variant fromXML ( VariantType variantType )
+    {
+        if ( variantType.getBoolean () != null )
+        {
+            return new Variant ( variantType.getBoolean ().getBooleanValue () );
+        }
+        else if ( variantType.getDouble () != null )
+        {
+            return new Variant ( variantType.getDouble ().getDoubleValue () );
+        }
+        else if ( variantType.getInt32 () != null )
+        {
+            return new Variant ( variantType.getInt32 ().getIntValue () );
+        }
+        else if ( variantType.getInt64 () != null )
+        {
+            return new Variant ( variantType.getInt64 ().getLongValue () );
+        }
+        else if ( variantType.getString () != null )
+        {
+            return new Variant ( variantType.getString () );
+        }
+        else if ( variantType.getNull () != null )
+        {
+            return new Variant ();
+        }
+        else
+        {
+            return null;
+        }
+    }
+    
     public static Map<String, Variant> convertAttributes ( AttributesType attributes ) throws ConfigurationError
     {
         Map<String, Variant> attributesMap = new HashMap<String, Variant> ();
@@ -22,36 +55,10 @@ public class Helper
         for ( AttributeType attribute : attributes.getAttributeList () )
         {
             String key = attribute.getName ();
-            Variant value = null;
             
-            if ( attribute.getBoolean () != null )
-            {
-                value = new Variant ( attribute.getBoolean ().getBooleanValue () );
-            }
-            else if ( attribute.getDouble () != null )
-            {
-                value = new Variant ( attribute.getDouble ().getDoubleValue () );
-            }
-            else if ( attribute.getInt32 () != null )
-            {
-                value = new Variant ( attribute.getInt32 ().getIntValue () );
-            }
-            else if ( attribute.getInt64 () != null )
-            {
-                value = new Variant ( attribute.getInt64 ().getLongValue () );
-            }
-            else if ( attribute.getString () != null )
-            {
-                value = new Variant ( attribute.getString () );
-            }
-            else if ( attribute.getNull () != null )
-            {
-                value = new Variant ();
-            }
-            else
-            {
+            Variant value = fromXML ( attribute );
+            if ( value == null )
                 throw new ConfigurationError ( "Invalid variant value configuration" );
-            }
             
             attributesMap.put ( key, value );
         }
