150895c08235c8961ebf39b4d90a930b010621a0
Jens Reimann <jens.reimann@th4-systems.com>
2006-10-10 15:32:22 +0000

    cleanup
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis@1306 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- da/client/viewer/src/org/openscada/da/client/viewer/model/impl/figures/Triangle.java
+++ da/client/viewer/src/org/openscada/da/client/viewer/model/impl/figures/Triangle.java
@@ -1,20 +1,11 @@
 package org.openscada.da.client.viewer.model.impl.figures;
 
-import org.apache.log4j.Logger;
 import org.eclipse.draw2d.IFigure;
-import org.eclipse.draw2d.PositionConstants;
-import org.eclipse.swt.graphics.Color;
-import org.eclipse.swt.graphics.RGB;
-import org.eclipse.swt.widgets.Display;
 import org.openscada.da.client.viewer.model.DynamicUIObject;
-import org.openscada.da.client.viewer.model.impl.AliasedPropertyInput;
-import org.openscada.da.client.viewer.model.impl.BaseDynamicObject;
 import org.openscada.da.client.viewer.model.impl.PropertyInput;
 
 public class Triangle extends BaseFigure implements DynamicUIObject
-{
-    private static Logger _log = Logger.getLogger ( Triangle.class );
-    
+{    
     private org.eclipse.draw2d.Triangle _triangle = null;
     private int _direction = 0;
     
