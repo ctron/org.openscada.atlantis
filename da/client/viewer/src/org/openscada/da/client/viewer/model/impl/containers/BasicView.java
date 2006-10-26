0caac7c194289c1904928da579afd613b0472966
Jens Reimann <jens.reimann@th4-systems.com>
2006-10-26 14:03:00 +0000

    removed dead code
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis@1339 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- da/client/viewer/src/org/openscada/da/client/viewer/model/impl/containers/BasicView.java
+++ da/client/viewer/src/org/openscada/da/client/viewer/model/impl/containers/BasicView.java
@@ -1,15 +1,9 @@
 package org.openscada.da.client.viewer.model.impl.containers;
 
-import java.util.LinkedList;
-import java.util.List;
-
-import org.openscada.da.client.viewer.model.Connector;
 import org.openscada.da.client.viewer.model.View;
 
 public class BasicView extends FigureContainer implements View
 {
-    private List<Connector> _connectors = new LinkedList<Connector> ();
-    
     public BasicView ( String id )
     {
         super ( id );
