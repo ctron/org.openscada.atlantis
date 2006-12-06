127659bf20450a0d33e020c774d48a7438bb5a1a
Jens Reimann <jens.reimann@th4-systems.com>
2006-12-06 09:52:25 +0000

    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis@1534 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- da/client/test/src/org/openscada/da/client/test/views/realtime/Listener.java
+++
@@ -1,8 +0,0 @@
-package org.openscada.da.client.test.views.realtime;
-
-public interface Listener
-{
-    void added ( ListEntry [] entries );
-    void updated ( ListEntry [] entries );
-    void removed ( ListEntry [] entries );
-}
