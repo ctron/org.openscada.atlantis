726530af66fc8f6ef3a1e115d61def02129d8bf6
Jens Reimann <jens.reimann@th4-systems.com>
2006-12-06 10:04:03 +0000

    re-checkin src
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis@1535 1b50e8ee-deed-0310-b1de-ab401e1c4d63

---
+++ da/client/test/src/org/openscada/da/client/test/actions/WatchItemAction.java
@@ -0,0 +1,101 @@
+/*
+ * This file is part of the OpenSCADA project
+ * Copyright (C) 2006 inavare GmbH (http://inavare.com)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package org.openscada.da.client.test.actions;
+
+import org.apache.log4j.Logger;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IViewActionDelegate;
+import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IWorkbenchPartSite;
+import org.eclipse.ui.PartInitException;
+import org.openscada.da.client.test.impl.DataItemEntry;
+import org.openscada.da.client.test.views.DataItemWatchView;
+
+public class WatchItemAction implements IViewActionDelegate, IObjectActionDelegate
+{
+    @SuppressWarnings("unused")
+    private static Logger _log = Logger.getLogger ( WatchItemAction.class );
+    
+    private IWorkbenchPartSite _site = null;
+    
+    private DataItemEntry _item = null;
+    
+    public void run ( IAction action )
+    {
+        if ( _item == null )
+        {
+            _log.info ( "No selection" );
+            return;
+        }
+       
+        try
+        {
+            IViewPart viewer = _site.getPage ().showView ( DataItemWatchView.VIEW_ID, _item.getAsSecondardId(), IWorkbenchPage.VIEW_ACTIVATE );
+            if ( viewer instanceof DataItemWatchView )
+            {
+                ((DataItemWatchView)viewer).setDataItem ( _item );
+            }
+        }
+        catch ( PartInitException e )
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public void selectionChanged ( IAction action, ISelection selection )
+    {
+        action.setEnabled ( false );
+        _item = null;
+        
+        if ( selection == null )
+            return;
+        if ( !(selection instanceof IStructuredSelection) )
+            return;
+        
+        IStructuredSelection sel = (IStructuredSelection)selection;
+        Object obj = sel.getFirstElement ();
+        
+        if ( obj == null )
+            return;
+        if ( !(obj instanceof DataItemEntry) )
+            return;
+        
+        _item = (DataItemEntry)obj;
+        action.setEnabled ( true );
+    }
+
+    public void setActivePart ( IAction action, IWorkbenchPart targetPart )
+    {
+        _site = targetPart.getSite ();
+    }
+
+    public void init ( IViewPart view )
+    {
+        _site = view.getSite();
+    }
+
+
+}
