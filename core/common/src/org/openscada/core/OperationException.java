ae2a020261d9b676d97c0d4a312c2115ed66eab8
Jens Reimann <jens.reimann@th4-systems.com>
2007-01-10 15:02:25 +0000

    moved to "net" free module
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis@1889 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- core/common/src/org/openscada/core/OperationException.java
+++ core/common/src/org/openscada/core/OperationException.java
@@ -17,7 +17,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-package org.openscada.core.client.net.operations;
+package org.openscada.core;
 
 public class OperationException extends Exception
 {
