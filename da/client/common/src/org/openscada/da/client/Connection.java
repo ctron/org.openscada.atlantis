4476dd23e64e20a0b583371be4dd1b9a419e4d21
Jens Reimann <jens.reimann@th4-systems.com>
2007-01-10 15:03:08 +0000

    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis@1891 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- da/client/common/src/org/openscada/da/client/Connection.java
+++ da/client/common/src/org/openscada/da/client/Connection.java
@@ -1,13 +1,24 @@
 package org.openscada.da.client;
 
+import org.openscada.core.OperationException;
 import org.openscada.core.Variant;
 import org.openscada.da.core.browser.Entry;
+import org.openscada.utils.exec.LongRunningOperation;
 import org.openscada.utils.exec.OperationResult;
 import org.openscada.utils.exec.OperationResultHandler;
+import org.openscada.utils.jobqueue.OperationManager.Listener;
 
 public interface Connection extends org.openscada.core.client.Connection
 {
     public Entry[] browse ( String [] path ) throws Exception;
     public OperationResult<Entry[]> startBrowse ( String [] path, Variant value );
     public OperationResult<Entry[]> startBrowse ( String [] path, OperationResultHandler<Entry[]> handler );
+    
+    public void write ( String itemName, Variant value ) throws InterruptedException, OperationException;
+    
+    public void write ( String itemName, Variant value, Listener listener ) throws InterruptedException, OperationException;
+    
+    public LongRunningOperation startWrite ( String itemName, Variant value, Listener listener );
+    
+    public void completeWrite ( LongRunningOperation op ) throws OperationException;
 }
