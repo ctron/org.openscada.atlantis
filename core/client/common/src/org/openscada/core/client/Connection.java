732a6d70b9d83c7e92a77a4a2bac7e94bbf43600
Jens Reimann <jens.reimann@th4-systems.com>
2007-01-10 14:41:29 +0000

    define interface for connections
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis@1883 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- core/client/common/src/org/openscada/core/client/Connection.java
+++ core/client/common/src/org/openscada/core/client/Connection.java
@@ -6,4 +6,9 @@ public interface Connection
     public void disconnect ();
     
     public void waitForConnection () throws Throwable;
+    
+    public void addConnectionStateListener ( ConnectionStateListener connectionStateListener );
+    public void removeConnectionStateListener ( ConnectionStateListener connectionStateListener );
+    
+    public ConnectionState getState ();
 }
