c6956903602037a35d012bcf52eac84e7fa37dc5
Jens Reimann <jens.reimann@th4-systems.com>
2007-03-22 13:39:12 +0000

    added a no connection exception
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis@2331 1b50e8ee-deed-0310-b1de-ab401e1c4d63

---
+++ net/base/src/org/openscada/net/base/NoConnectionException.java
@@ -0,0 +1,11 @@
+package org.openscada.net.base;
+
+public class NoConnectionException extends Exception
+{
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -5722669689966000196L;
+
+}
