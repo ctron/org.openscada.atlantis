7d826203ebee6ebb95683c5b589a737a79a51675
Jens Reimann <jens.reimann@th4-systems.com>
2008-02-08 12:45:59 +0000

    add auto collection
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis@2641 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- da/server/opc/src/org/openscada/da/server/opc/Hive.java
+++ da/server/opc/src/org/openscada/da/server/opc/Hive.java
@@ -21,11 +21,13 @@ package org.openscada.da.server.opc;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.LinkedList;
+import java.util.logging.Level;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.log4j.Logger;
 import org.apache.xmlbeans.XmlException;
+import org.jinterop.dcom.common.JISystem;
 import org.openscada.core.Variant;
 import org.openscada.da.server.browser.common.FolderCommon;
 import org.openscada.da.server.common.DataItemCommand;
@@ -40,37 +42,43 @@ import org.w3c.dom.Node;
 public class Hive extends HiveCommon
 {
     private static Logger _log = Logger.getLogger ( Hive.class );
-    
+
     private Scheduler _scheduler = null;
-    
+
     private FolderCommon _rootFolderCommon = null;
-    
+
     private DataItemCommand _addCommand = null;
-    
+
     private DataItemCommand _gcCommand = null;
 
-    public Hive () throws XmlException, IOException, ConfigurationError 
+    public Hive () throws XmlException, IOException, ConfigurationError
     {
         this ( new XMLConfigurator ( "configuration.xml" ) );
     }
-    
+
     public Hive ( Node node ) throws XmlException, IOException, ConfigurationError
     {
         this ( new XMLConfigurator ( node ) );
     }
-    
+
     public Hive ( XMLConfigurator configurator ) throws XmlException, IOException, ConfigurationError
     {
         super ();
-        
+
         _log.warn ( String.format ( "rpc.socketTimeout = %s", System.getProperty ( "rpc.socketTimeout" ) ) );
+        if ( !Boolean.getBoolean ( "dcom.debug" ) )
+        {
+            java.util.logging.Logger.getLogger ( "org.jinterop" ).setLevel ( Level.WARNING );
+        }
+        JISystem.setJavaCoClassAutoCollection ( !Boolean.getBoolean ( "dcom.disableAutoCollection" ) );
+        _log.info ( "DCOM auto collection: " + JISystem.isJavaCoClassAutoCollectionSet () );
 
         _scheduler = new Scheduler ( true, "OPCHiveScheduler" );
 
         // create root folder
         _rootFolderCommon = new FolderCommon ();
         setRootFolder ( _rootFolderCommon );
-        
+
         // create add command
         _addCommand = new DataItemCommand ( "add" );
         _addCommand.addListener ( new DataItemCommand.Listener () {
@@ -86,25 +94,35 @@ public class Hive extends HiveCommon
                 {
                     _log.warn ( "Failed to add new connection", t );
                 }
-            }} );
+            }
+        } );
         registerItem ( _addCommand );
-        _rootFolderCommon.add ( "add", _addCommand, new MapBuilder<String,Variant> ().put ( "description", new Variant ( "Write a connection string here to temporarily add a new OPC connection." ) ).put ( "sample", new Variant ( "opcda://domain\\user:password@127.0.0.1?clsid=F8582CF2-88FB-11D0-B850-00C0F0104305" ) ).getMap () );
-        
+        _rootFolderCommon.add (
+                "add",
+                _addCommand,
+                new MapBuilder<String, Variant> ().put ( "description",
+                        new Variant ( "Write a connection string here to temporarily add a new OPC connection." ) ).put (
+                        "sample",
+                        new Variant (
+                                "opcda://domain\\user:password@127.0.0.1?clsid=F8582CF2-88FB-11D0-B850-00C0F0104305" ) ).getMap () );
+
         _gcCommand = new DataItemCommand ( "gc" );
         _gcCommand.addListener ( new DataItemCommand.Listener () {
 
             public void command ( Variant value )
             {
                 System.gc ();
-            }} );
+            }
+        } );
         registerItem ( _gcCommand );
-        _rootFolderCommon.add ( "gc", _gcCommand, new MapBuilder<String,Variant> ().put ( "description", new Variant ( "Run the garbage collector once." ) ).getMap () );
-        
+        _rootFolderCommon.add ( "gc", _gcCommand, new MapBuilder<String, Variant> ().put ( "description",
+                new Variant ( "Run the garbage collector once." ) ).getMap () );
+
         setValidatonStrategy ( ValidationStrategy.GRANT_ALL );
-        
+
         configure ( configurator );
     }
-    
+
     public void configure ( XMLConfigurator configurator ) throws ConfigurationError
     {
         if ( configurator != null )
@@ -112,32 +130,32 @@ public class Hive extends HiveCommon
             configurator.configure ( this );
         }
     }
-    
+
     public Scheduler getScheduler ()
     {
         return _scheduler;
     }
-    
+
     public FolderCommon getRootFolderCommon ()
     {
         return _rootFolderCommon;
     }
-    
+
     public void addConnection ( String connectionString )
     {
         Matcher m = Pattern.compile ( "^opcda://(.*?)\\\\(.*?):(.*?)@(.*?)\\?(.*?)=(.*)$" ).matcher ( connectionString );
-        
+
         if ( !m.matches () )
         {
             return;
         }
-        
+
         ConnectionInformation ci = new ConnectionInformation ();
         ci.setDomain ( m.group ( 1 ) );
         ci.setUser ( m.group ( 2 ) );
         ci.setPassword ( m.group ( 3 ) );
         ci.setHost ( m.group ( 4 ) );
-        if ( m.group (5 ).toLowerCase().equals ( "clsid" ) )
+        if ( m.group ( 5 ).toLowerCase ().equals ( "clsid" ) )
             ci.setClsid ( m.group ( 6 ) );
         else if ( m.group ( 5 ).toLowerCase ().equals ( "progid" ) )
             ci.setProgId ( m.group ( 6 ) );
@@ -146,11 +164,11 @@ public class Hive extends HiveCommon
         ConnectionSetup cs = new ConnectionSetup ( ci );
         addConnection ( cs, null, true, new LinkedList<String> () );
     }
-    
+
     public void addConnection ( ConnectionSetup setup, String alias, boolean connect, Collection<String> initialOpcItems )
     {
         OPCConnection connection = new OPCConnection ( this, _scheduler, alias, setup, initialOpcItems );
-        
+
         try
         {
             connection.start ();
@@ -160,11 +178,10 @@ public class Hive extends HiveCommon
                 connection.triggerConnect ();
             }
         }
-        catch (Throwable e )
+        catch ( Throwable e )
         {
             _log.error ( "Failed to add connection", e );
         }
-        
-        
+
     }
 }
