4f80f0cd4e9ce7160595b51a8af1ec5899380e61
Juergen Rose <juergen.rose@th4-systems.com>
2008-11-14 16:07:20 +0000

    browse modified to accept a location
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis@2991 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- da/client/common/src/org/openscada/da/client/Connection.java
+++ da/client/common/src/org/openscada/da/client/Connection.java
@@ -38,41 +38,81 @@ public interface Connection extends org.openscada.core.client.Connection
     /**
      * Browse a server folder for items. The operation will block until the result is available,
      * an error occurred or the connection failed.
+     * @see org.openscada.da.client.Connection#browse(Location)
      * @param path The path to browse
      * @return The entries of the server folder
      * @throws NoConnectionException Raised if there is currently no connect to the server
      * @throws OperationException Raised if the operation failed
      */
+    @Deprecated
     public abstract Entry[] browse ( String[] path ) throws NoConnectionException, OperationException;
-    
+
+    /**
+     * Browse a server folder for items. The operation will block until the result is available,
+     * an error occurred or the connection failed.
+     * @param location The path to browse
+     * @return The entries of the server folder
+     * @throws NoConnectionException Raised if there is currently no connect to the server
+     * @throws OperationException Raised if the operation failed
+     */
+    public abstract Entry[] browse ( Location location ) throws NoConnectionException, OperationException;
+
     /**
      * Browse a server folder for items. The operation will block until the result is available,
      * an error occurred, the timeout expired or the connection failed.
+     * @see org.openscada.da.client.Connection#browse(Location, int))
      * @param path The path to browse
      * @param timeout Timeout of the operation in milliseconds
      * @return The entries of the server folder
      * @throws NoConnectionException Raised if there is currently no connect to the server
      * @throws OperationException Raised if the operation failed
      */
+    @Deprecated
     public abstract Entry[] browse ( String[] path, int timeout ) throws NoConnectionException, OperationException;
-    
+
+    /**
+     * Browse a server folder for items. The operation will block until the result is available,
+     * an error occurred, the timeout expired or the connection failed.
+     * @param location The path to browse
+     * @param timeout Timeout of the operation in milliseconds
+     * @return The entries of the server folder
+     * @throws NoConnectionException Raised if there is currently no connect to the server
+     * @throws OperationException Raised if the operation failed
+     */
+    public abstract Entry[] browse ( Location location, int timeout ) throws NoConnectionException, OperationException;
+
     /**
      * Browse a server folder for items. The operation will return immediately.
+     * @see org.openscada.da.client.Connection#browse(Location, BrowseOperationCallback)
      * @param path The path to browse
      * @param callback The callback that shall receive notifications of the operation
      */
+    @Deprecated
     public abstract void browse ( String[] path, BrowseOperationCallback callback );
 
+    /**
+     * Browse a server folder for items. The operation will return immediately.
+     * @param location The path to browse
+     * @param callback The callback that shall receive notifications of the operation
+     */
+    public abstract void browse ( Location location, BrowseOperationCallback callback );
+
     public abstract void write ( String itemName, Variant value ) throws NoConnectionException, OperationException;
+
     public abstract void write ( String itemName, Variant value, int timeout ) throws NoConnectionException, OperationException;
+
     public abstract void write ( String itemName, Variant value, WriteOperationCallback callback );
 
     public abstract WriteAttributeResults writeAttributes ( String itemId, Map<String, Variant> attributes ) throws NoConnectionException, OperationException;
+
     public abstract WriteAttributeResults writeAttributes ( String itemId, Map<String, Variant> attributes, int timeout ) throws NoConnectionException, OperationException;
+
     public abstract void writeAttributes ( String itemId, Map<String, Variant> attributes, WriteAttributeOperationCallback callback );
 
     public abstract void subscribeFolder ( Location location ) throws NoConnectionException, OperationException;
+
     public abstract void unsubscribeFolder ( Location location ) throws NoConnectionException, OperationException;
+
     /**
      * Set the listener for this location.
      * @param location The location for which to set the listener
@@ -82,7 +122,9 @@ public interface Connection extends org.openscada.core.client.Connection
     public abstract FolderListener setFolderListener ( Location location, FolderListener listener );
 
     public abstract void subscribeItem ( String itemId ) throws NoConnectionException, OperationException;
+
     public abstract void unsubscribeItem ( String itemId ) throws NoConnectionException, OperationException;
+
     /**
      * Set the listener for this item.
      * @param itemId The item for which to set the listener
