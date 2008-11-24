248d09ae2f71a682c05cb96e992f0f91f1ce698c
Jens Reimann <jens.reimann@th4-systems.com>
2008-11-24 11:45:14 +0000

    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis@3007 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- core/client/common/src/org/openscada/core/client/NoConnectionException.java
+++ core/client/common/src/org/openscada/core/client/NoConnectionException.java
@@ -2,9 +2,5 @@ package org.openscada.core.client;
 
 public class NoConnectionException extends Exception
 {
-
-    /**
-     * 
-     */
     private static final long serialVersionUID = 7077006802201924241L;
 }
