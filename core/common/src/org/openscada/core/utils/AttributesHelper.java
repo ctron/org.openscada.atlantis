fc232b6da741aee7437e0f5b23f83248788f8ce9
Jens Reimann <jens.reimann@th4-systems.com>
2008-11-25 15:57:42 +0000

    cleanup
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis@3021 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- core/common/src/org/openscada/core/utils/AttributesHelper.java
+++ core/common/src/org/openscada/core/utils/AttributesHelper.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of the OpenSCADA project
- * Copyright (C) 2006 inavare GmbH (http://inavare.com)
+ * Copyright (C) 2006-2008 inavare GmbH (http://inavare.com)
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -37,7 +37,7 @@ public class AttributesHelper
      * @param change the attributes to change
      * @param diff output of real changes that were made
      */
-    public static void mergeAttributes ( Map<String, Variant> target, Map<String, Variant> change, Map<String, Variant> diff )
+    public static void mergeAttributes ( final Map<String, Variant> target, final Map<String, Variant> change, final Map<String, Variant> diff )
     {
         if ( change == null )
         {
@@ -45,10 +45,12 @@ public class AttributesHelper
             return;
         }
 
-        for ( Map.Entry<String, Variant> entry : change.entrySet () )
+        for ( final Map.Entry<String, Variant> entry : change.entrySet () )
         {
             if ( entry.getKey () == null )
+            {
                 continue;
+            }
 
             if ( entry.getValue () == null )
             {
@@ -56,7 +58,9 @@ public class AttributesHelper
                 {
                     target.remove ( entry.getKey () );
                     if ( diff != null )
+                    {
                         diff.put ( entry.getKey (), null );
+                    }
                 }
             }
             else if ( entry.getValue ().isNull () )
@@ -65,15 +69,21 @@ public class AttributesHelper
                 {
                     target.remove ( entry.getKey () );
                     if ( diff != null )
+                    {
                         diff.put ( entry.getKey (), null );
+                    }
                 }
             }
             else
             {
-                if ( ( diff != null ) && !target.containsKey ( entry.getKey () ) )
+                if ( diff != null && !target.containsKey ( entry.getKey () ) )
+                {
                     diff.put ( entry.getKey (), entry.getValue () );
-                else if ( ( diff != null ) && !target.get ( entry.getKey () ).equals ( entry.getValue () ) )
+                }
+                else if ( diff != null && !target.get ( entry.getKey () ).equals ( entry.getValue () ) )
+                {
                     diff.put ( entry.getKey (), entry.getValue () );
+                }
 
                 target.put ( new String ( entry.getKey () ), new Variant ( entry.getValue () ) );
             }
@@ -85,7 +95,7 @@ public class AttributesHelper
      * @param target the attributes to merge the difference in
      * @param change the attributes to change
      */
-    public static void mergeAttributes ( Map<String, Variant> target, Map<String, Variant> change )
+    public static void mergeAttributes ( final Map<String, Variant> target, final Map<String, Variant> change )
     {
         mergeAttributes ( target, change, null );
     }
@@ -100,10 +110,12 @@ public class AttributesHelper
      * @param diff the difference attributes
      * @param initial initial flag
      */
-    public static void mergeAttributes ( Map<String, Variant> target, Map<String, Variant> diff, boolean initial )
+    public static void mergeAttributes ( final Map<String, Variant> target, final Map<String, Variant> diff, final boolean initial )
     {
         if ( initial )
+        {
             target.clear ();
+        }
 
         mergeAttributes ( target, diff );
     }
@@ -114,20 +126,20 @@ public class AttributesHelper
      * @param attributes the new attributes set
      * @param diff the actual difference
      */
-    public static void set ( Map<String, Variant> target, Map<String, Variant> attributes, Map<String, Variant> diff )
+    public static void set ( final Map<String, Variant> target, Map<String, Variant> attributes, final Map<String, Variant> diff )
     {
         if ( attributes == null )
         {
             attributes = Collections.emptyMap ();
         }
-        
-        Set<String> initialSet = new HashSet<String> ( target.keySet () );
 
-        for ( Map.Entry<String, Variant> entry : attributes.entrySet () )
+        final Set<String> initialSet = new HashSet<String> ( target.keySet () );
+
+        for ( final Map.Entry<String, Variant> entry : attributes.entrySet () )
         {
             if ( target.containsKey ( entry.getKey () ) )
             {
-                if ( ( entry.getValue () == null ) || entry.getValue ().isNull () )
+                if ( entry.getValue () == null || entry.getValue ().isNull () )
                 {
                     // entry removed
                     target.remove ( entry.getKey () );
@@ -146,7 +158,7 @@ public class AttributesHelper
             }
             else
             {
-                if ( ( entry.getValue () != null ) && ( !entry.getValue ().isNull () ) )
+                if ( entry.getValue () != null && !entry.getValue ().isNull () )
                 {
                     // entry is new
                     diff.put ( entry.getKey (), entry.getValue () );
@@ -155,20 +167,20 @@ public class AttributesHelper
             }
         }
 
-        for ( String key : initialSet )
+        for ( final String key : initialSet )
         {
             target.remove ( key );
             diff.put ( key, null );
         }
     }
 
-    public static Map<String, Variant> clone ( Map<String, Variant> attributes )
+    public static Map<String, Variant> clone ( final Map<String, Variant> attributes )
     {
-        Map<String, Variant> theClone = new HashMap<String, Variant> ();
+        final Map<String, Variant> theClone = new HashMap<String, Variant> ();
 
-        for ( Map.Entry<String, Variant> entry : attributes.entrySet () )
+        for ( final Map.Entry<String, Variant> entry : attributes.entrySet () )
         {
-            theClone.put ( entry.getKey (), ( entry.getValue () != null ) ? new Variant ( entry.getValue () ) : null );
+            theClone.put ( entry.getKey (), entry.getValue () != null ? new Variant ( entry.getValue () ) : null );
         }
 
         return theClone;
