b1b2851eb6e946aa6d686eb337649cf9f851a6d0
Jens Reimann <jens.reimann@th4-systems.com>
2008-12-15 10:53:25 +0000

    added connection stuff
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis@3079 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- trunk/org.openscada.da.base/src/org/openscada/da/base/connection/ConnectionManager.java
+++ trunk/org.openscada.da.base/src/org/openscada/da/base/connection/ConnectionManager.java
@@ -1,8 +1,30 @@
+/*
+ * This file is part of the OpenSCADA project
+ * Copyright (C) 2006-2008 inavare GmbH (http://inavare.com)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 package org.openscada.da.base.connection;
 
 import java.net.URI;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 import org.openscada.core.ConnectionInformation;
 import org.openscada.da.base.item.DataItemHolder;
@@ -13,61 +35,57 @@ import org.openscada.rcp.da.client.Activator;
 public class ConnectionManager
 {
 
-    private static class Entry
+    private final Collection<ConnectionManagerListener> listeners = new CopyOnWriteArrayList<ConnectionManagerListener> ();
+
+    private final Map<ConnectionInformation, ConnectionManagerEntry> connections = new HashMap<ConnectionInformation, ConnectionManagerEntry> ();
+
+    public synchronized ConnectionManagerEntry getEntry ( final ConnectionInformation ci, final boolean connect )
     {
-        Connection connection;
-
-        ItemManager itemManager;
-    }
-
-    private final Map<ConnectionInformation, Entry> connections = new HashMap<ConnectionInformation, Entry> ();
-
-    private synchronized Entry getEntry ( final ConnectionInformation ci, final boolean connect )
-    {
-        Entry entry = this.connections.get ( ci );
+        ConnectionManagerEntry entry = this.connections.get ( ci );
         if ( entry == null )
         {
-            entry = new Entry ();
-            entry.connection = (Connection)Activator.createConnection ( ci );
-            entry.itemManager = new ItemManager ( entry.connection );
+            entry = new ConnectionManagerEntry ();
+            entry.setConnection ( (Connection)Activator.createConnection ( ci ) );
+            entry.setItemManager ( new ItemManager ( entry.getConnection () ) );
             if ( connect )
             {
-                entry.connection.connect ();
+                entry.getConnection ().connect ();
             }
-            if ( entry.connection != null )
+            if ( entry.getConnection () != null )
             {
                 this.connections.put ( ci, entry );
             }
+            fireConnectionsAdded ( Arrays.asList ( entry ) );
         }
         return entry;
     }
 
     public synchronized DataItemHolder getDataItemHolder ( final ConnectionInformation ci, final String itemId, final boolean connect )
     {
-        final Entry entry = getEntry ( ci, connect );
+        final ConnectionManagerEntry entry = getEntry ( ci, connect );
         if ( entry != null )
         {
-            return new DataItemHolder ( entry.connection, entry.itemManager, itemId );
+            return new DataItemHolder ( entry.getConnection (), entry.getItemManager (), itemId );
         }
         return null;
     }
 
     public synchronized Connection getConnection ( final ConnectionInformation ci, final boolean connect )
     {
-        final Entry entry = getEntry ( ci, connect );
+        final ConnectionManagerEntry entry = getEntry ( ci, connect );
         if ( entry != null )
         {
-            return entry.connection;
+            return entry.getConnection ();
         }
         return null;
     }
 
     public synchronized Connection getConnection ( final String connectionUri, final boolean connect )
     {
-        final Entry entry = getEntry ( ConnectionInformation.fromURI ( connectionUri ), connect );
+        final ConnectionManagerEntry entry = getEntry ( ConnectionInformation.fromURI ( connectionUri ), connect );
         if ( entry != null )
         {
-            return entry.connection;
+            return entry.getConnection ();
         }
         return null;
     }
@@ -79,10 +97,10 @@ public class ConnectionManager
 
     public synchronized ItemManager getItemManager ( final ConnectionInformation ci, final boolean connect )
     {
-        final Entry entry = getEntry ( ci, connect );
+        final ConnectionManagerEntry entry = getEntry ( ci, connect );
         if ( entry != null )
         {
-            return entry.itemManager;
+            return entry.getItemManager ();
         }
         return null;
     }
@@ -94,9 +112,9 @@ public class ConnectionManager
 
     public synchronized void dispose ()
     {
-        for ( final Map.Entry<ConnectionInformation, Entry> entry : this.connections.entrySet () )
+        for ( final Map.Entry<ConnectionInformation, ConnectionManagerEntry> entry : this.connections.entrySet () )
         {
-            entry.getValue ().connection.disconnect ();
+            entry.getValue ().getConnection ().disconnect ();
         }
         this.connections.clear ();
     }
@@ -104,5 +122,31 @@ public class ConnectionManager
     public static ConnectionManager getDefault ()
     {
         return org.openscada.da.base.Activator.getConnectionManager ();
+    }
+
+    protected void fireConnectionsAdded ( final Collection<ConnectionManagerEntry> connections )
+    {
+        for ( final ConnectionManagerListener listener : this.listeners )
+        {
+            listener.connectionsAdded ( connections );
+        }
+    }
+
+    protected void fireConnectionsRemoved ( final Collection<ConnectionManagerEntry> connections )
+    {
+        for ( final ConnectionManagerListener listener : this.listeners )
+        {
+            listener.connectionsRemoved ( connections );
+        }
+    }
+
+    public void addConnectionManagerListener ( final ConnectionManagerListener listener )
+    {
+        this.listeners.add ( listener );
+    }
+
+    public void removeConnectionManagerListener ( final ConnectionManagerListener listener )
+    {
+        this.listeners.remove ( listener );
     }
 }
