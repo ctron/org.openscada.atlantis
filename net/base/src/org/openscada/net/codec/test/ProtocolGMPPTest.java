f3ab7f976789b736eced12b231dcac32ba421a7c
Jens Reimann <jens.reimann@th4-systems.com>
2008-12-19 11:10:48 +0000

    cleanup
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis@3103 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- net/base/src/org/openscada/net/codec/test/ProtocolGMPPTest.java
+++ net/base/src/org/openscada/net/codec/test/ProtocolGMPPTest.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of the OpenSCADA project
- * Copyright (C) 2006 inavare GmbH (http://inavare.com)
+ * Copyright (C) 2006-2008 inavare GmbH (http://inavare.com)
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -38,32 +38,32 @@ import org.openscada.net.codec.ProtocolGMPP;
 
 public class ProtocolGMPPTest
 {
-    private static Logger _log = Logger.getLogger ( ProtocolGMPPTest.class );
+    private static Logger log = Logger.getLogger ( ProtocolGMPPTest.class );
 
-    private void performTest ( Collection<TestBytePacket> packets, Collection<Message> messages )
+    private void performTest ( final Collection<TestBytePacket> packets, final Collection<Message> messages )
     {
-        TestInputStream stream = new TestInputStream ( packets );
-        TestMessageListener listener = new TestMessageListener ();
+        final TestInputStream stream = new TestInputStream ( packets );
+        final TestMessageListener listener = new TestMessageListener ();
 
-        Protocol protocol = new ProtocolGMPP ( null, listener );
+        final Protocol protocol = new ProtocolGMPP ( null, listener );
 
         stream.run ( protocol );
 
         listener.assertMessages ( messages );
     }
 
-    private void performScatterTest ( Collection<TestBytePacket> packets, Collection<Message> messages, int scatterSize )
+    private void performScatterTest ( final Collection<TestBytePacket> packets, final Collection<Message> messages, final int scatterSize )
     {
-        _log.info ( "Running scatter test with: " + scatterSize );
+        log.info ( "Running scatter test with: " + scatterSize );
 
-        List<TestBytePacket> scatteredPackets = new ArrayList<TestBytePacket> ();
+        final List<TestBytePacket> scatteredPackets = new ArrayList<TestBytePacket> ();
 
-        byte[] data = new byte[scatterSize];
+        final byte[] data = new byte[scatterSize];
         int pos = 0;
 
-        for ( TestBytePacket packet : packets )
+        for ( final TestBytePacket packet : packets )
         {
-            for ( byte b : packet.getBytes () )
+            for ( final byte b : packet.getBytes () )
             {
                 data[pos] = b;
                 pos++;
@@ -77,9 +77,11 @@ public class ProtocolGMPPTest
 
         if ( pos > 0 )
         {
-            byte[] addData = new byte[pos];
+            final byte[] addData = new byte[pos];
             for ( int i = 0; i < pos; i++ )
+            {
                 addData[i] = data[i];
+            }
 
             scatteredPackets.add ( new TestBytePacket ( addData ) );
         }
@@ -87,15 +89,15 @@ public class ProtocolGMPPTest
         performTest ( scatteredPackets, messages );
     }
 
-    private void performScatterTest ( Collection<TestBytePacket> packets, Collection<Message> messages, int... scatterSize )
+    private void performScatterTest ( final Collection<TestBytePacket> packets, final Collection<Message> messages, final int... scatterSize )
     {
-        for ( int i : scatterSize )
+        for ( final int i : scatterSize )
         {
             performScatterTest ( packets, messages, i );
         }
     }
 
-    private void performAllTests ( Collection<TestBytePacket> packets, Collection<Message> messages )
+    private void performAllTests ( final Collection<TestBytePacket> packets, final Collection<Message> messages )
     {
         performTest ( packets, messages );
 
@@ -105,27 +107,27 @@ public class ProtocolGMPPTest
     @Test
     public void testPacket1 ()
     {
-        _log.info ( "Running test1" );
+        log.info ( "Running test1" );
 
-        List<TestBytePacket> packets = new ArrayList<TestBytePacket> ();
+        final List<TestBytePacket> packets = new ArrayList<TestBytePacket> ();
         packets.add ( new TestBytePacket ( " 00 00 00 01" + // command code
-                " 00 00 00 00 00 00 00 00" + // timestamp
-                " 00 00 00 00 00 00 00 02" + // sequence
-                " 00 00 00 00 00 00 00 03" + // reply sequence
-                " 00 00 00 20" + // body size
-                " 00 00 00 07" + // VT_MAP
-                " 00 00 00 18" + // VT_MAP - size
-                " 00 00 00 01" + // VT_MAP - item count
-                " 00 00 00 01" + // VT_STRING
-                " 00 00 00 04" + // string length
-                " 74 65 73 74" + // empty string
-                " 00 00 00 04" + // name length
-                " 74 65 73 74" + // empty name
-                "" ) );
+        " 00 00 00 00 00 00 00 00" + // timestamp
+        " 00 00 00 00 00 00 00 02" + // sequence
+        " 00 00 00 00 00 00 00 03" + // reply sequence
+        " 00 00 00 20" + // body size
+        " 00 00 00 07" + // VT_MAP
+        " 00 00 00 18" + // VT_MAP - size
+        " 00 00 00 01" + // VT_MAP - item count
+        " 00 00 00 01" + // VT_STRING
+        " 00 00 00 04" + // string length
+        " 74 65 73 74" + // empty string
+        " 00 00 00 04" + // name length
+        " 74 65 73 74" + // empty name
+        "" ) );
 
         Message message;
 
-        List<Message> messages = new ArrayList<Message> ();
+        final List<Message> messages = new ArrayList<Message> ();
 
         message = new Message ();
         message.setCommandCode ( 1 );
@@ -140,22 +142,22 @@ public class ProtocolGMPPTest
 
     public void testPacket2 ()
     {
-        _log.info ( "Running test2" );
-        List<TestBytePacket> packets = new ArrayList<TestBytePacket> ();
+        log.info ( "Running test2" );
+        final List<TestBytePacket> packets = new ArrayList<TestBytePacket> ();
         Message message;
-        List<Message> messages = new ArrayList<Message> ();
+        final List<Message> messages = new ArrayList<Message> ();
 
         for ( int i = 0; i < 1000; i++ )
         {
-            int commandCode = i % 255;
-            String strCommandCode = Integer.toHexString ( commandCode );
+            final int commandCode = i % 255;
+            final String strCommandCode = Integer.toHexString ( commandCode );
             packets.add ( new TestBytePacket ( " 00 00 00 " + strCommandCode + // command code
-                    " 00 00 00 00 00 00 00 00" + // timestamp
-                    " 00 00 00 00 00 00 00 02" + // sequence
-                    " 00 00 00 00 00 00 00 03" + // reply sequence
-                    " 00 00 00 00" + // number of values
-                    " 00 00 00 00" + // body size
-                    "" ) );
+            " 00 00 00 00 00 00 00 00" + // timestamp
+            " 00 00 00 00 00 00 00 02" + // sequence
+            " 00 00 00 00 00 00 00 03" + // reply sequence
+            " 00 00 00 00" + // number of values
+            " 00 00 00 00" + // body size
+            "" ) );
 
             message = new Message ();
             message.setCommandCode ( commandCode );
@@ -168,12 +170,12 @@ public class ProtocolGMPPTest
         performAllTests ( packets, messages );
     }
 
-    private void performCode ( Message message, TestBytePacket expectedBuffer ) throws InvalidValueTypeException
+    private void performCode ( final Message message, final TestBytePacket expectedBuffer ) throws InvalidValueTypeException
     {
-        Protocol protocol = new ProtocolGMPP ( null, null );
-        ByteBuffer byteBuffer = protocol.code ( message );
+        final Protocol protocol = new ProtocolGMPP ( null, null );
+        final ByteBuffer byteBuffer = protocol.code ( message );
 
-        int max = Math.max ( byteBuffer.remaining (), expectedBuffer.getBytes ().length );
+        final int max = Math.max ( byteBuffer.remaining (), expectedBuffer.getBytes ().length );
         System.out.println ( "Buffer sizes: " + byteBuffer.remaining () + "/" + expectedBuffer.getBytes ().length );
         for ( int i = 0; i < max; i++ )
         {
@@ -188,19 +190,26 @@ public class ProtocolGMPPTest
                 line += String.format ( "%1$02X - ", b1 );
             }
             else
+            {
                 line += "   - ";
+            }
             if ( i < expectedBuffer.getBytes ().length )
             {
                 b2 = expectedBuffer.getBytes ()[i];
                 line += String.format ( "%1$02X", b2 );
             }
             else
+            {
                 line += "  ";
+            }
 
-            if ( b1 == null && b2 == null )
-                ;
+            if ( b1 == null || b2 == null )
+            {
+            }
             else if ( !b1.equals ( b2 ) )
+            {
                 line += " X";
+            }
 
             System.out.println ( line );
         }
@@ -211,29 +220,29 @@ public class ProtocolGMPPTest
     @Test
     public void testEncode1 () throws InvalidValueTypeException
     {
-        _log.info ( "Running encode1" );
+        log.info ( "Running encode1" );
 
-        Message message = new Message ();
+        final Message message = new Message ();
         message.setTimestamp ( 0 );
         message.setCommandCode ( 1 );
         message.setSequence ( 2 );
         message.setReplySequence ( 3 );
         message.getValues ().put ( "test", new StringValue ( "test" ) );
 
-        TestBytePacket packet = new TestBytePacket ( " 00 00 00 01" + // command code
-                " 00 00 00 00 00 00 00 00" + // timestamp
-                " 00 00 00 00 00 00 00 02" + // sequence
-                " 00 00 00 00 00 00 00 03" + // reply sequence
-                " 00 00 00 20" + // body size
-                " 00 00 00 07" + // VT_MAP
-                " 00 00 00 18" + // VT_MAP - size
-                " 00 00 00 01" + // VT_MAP - item count
-                " 00 00 00 01" + // VT_STRING
-                " 00 00 00 04" + // string length
-                " 74 65 73 74" + // test string
-                " 00 00 00 04" + // name length
-                " 74 65 73 74" + // test name
-                "" );
+        final TestBytePacket packet = new TestBytePacket ( " 00 00 00 01" + // command code
+        " 00 00 00 00 00 00 00 00" + // timestamp
+        " 00 00 00 00 00 00 00 02" + // sequence
+        " 00 00 00 00 00 00 00 03" + // reply sequence
+        " 00 00 00 20" + // body size
+        " 00 00 00 07" + // VT_MAP
+        " 00 00 00 18" + // VT_MAP - size
+        " 00 00 00 01" + // VT_MAP - item count
+        " 00 00 00 01" + // VT_STRING
+        " 00 00 00 04" + // string length
+        " 74 65 73 74" + // test string
+        " 00 00 00 04" + // name length
+        " 74 65 73 74" + // test name
+        "" );
 
         performCode ( message, packet );
     }
@@ -241,9 +250,9 @@ public class ProtocolGMPPTest
     @Test
     public void testEncode2 () throws InvalidValueTypeException
     {
-        _log.info ( "Running encode2" );
+        log.info ( "Running encode2" );
 
-        Message message = new Message ();
+        final Message message = new Message ();
         message.setTimestamp ( 0 );
         message.setCommandCode ( 1 + 0xFF );
         message.setSequence ( 2 );
@@ -251,25 +260,25 @@ public class ProtocolGMPPTest
         message.getValues ().put ( "test", new StringValue ( "test" ) );
         message.getValues ().put ( "tett", new StringValue ( "tett" ) );
 
-        TestBytePacket packet = new TestBytePacket ( " 00 00 01 00" + // command code
-                " 00 00 00 00 00 00 00 00" + // timestamp
-                " 00 00 00 00 00 00 00 02" + // sequence
-                " 00 00 00 00 00 00 01 00" + // reply sequence
-                " 00 00 00 34" + // body size
-                " 00 00 00 07" + // VT_MAP
-                " 00 00 00 2C" + // VT_MAP - size
-                " 00 00 00 02" + // VT_MAP - item count
-                " 00 00 00 01" + // VT_STRING
-                " 00 00 00 04" + // string length
-                " 74 65 74 74" + // empty string
-                " 00 00 00 04" + // name length
-                " 74 65 74 74" + // test name
-                " 00 00 00 01" + // VT_STRING
-                " 00 00 00 04" + // string length
-                " 74 65 73 74" + // test string
-                " 00 00 00 04" + // name length
-                " 74 65 73 74" + // test name
-                "" );
+        final TestBytePacket packet = new TestBytePacket ( " 00 00 01 00" + // command code
+        " 00 00 00 00 00 00 00 00" + // timestamp
+        " 00 00 00 00 00 00 00 02" + // sequence
+        " 00 00 00 00 00 00 01 00" + // reply sequence
+        " 00 00 00 34" + // body size
+        " 00 00 00 07" + // VT_MAP
+        " 00 00 00 2C" + // VT_MAP - size
+        " 00 00 00 02" + // VT_MAP - item count
+        " 00 00 00 01" + // VT_STRING
+        " 00 00 00 04" + // string length
+        " 74 65 74 74" + // empty string
+        " 00 00 00 04" + // name length
+        " 74 65 74 74" + // test name
+        " 00 00 00 01" + // VT_STRING
+        " 00 00 00 04" + // string length
+        " 74 65 73 74" + // test string
+        " 00 00 00 04" + // name length
+        " 74 65 73 74" + // test name
+        "" );
 
         performCode ( message, packet );
     }
@@ -277,45 +286,45 @@ public class ProtocolGMPPTest
     @Test
     public void testEncode3 () throws InvalidValueTypeException
     {
-        _log.info ( "Running encode3" );
+        log.info ( "Running encode3" );
 
-        Message message = new Message ();
+        final Message message = new Message ();
         message.setTimestamp ( 0 );
         message.setCommandCode ( 1 + 0xFF );
         message.setSequence ( 2 );
         message.setReplySequence ( 1 + 0xFF );
-        ListValue list = new ListValue ();
+        final ListValue list = new ListValue ();
         list.getValues ().add ( new StringValue ( "tett" ) );
         list.getValues ().add ( new StringValue ( "teut" ) );
         message.getValues ().put ( "test", list );
 
-        TestBytePacket packet = new TestBytePacket ( " 00 00 01 00" + // command code
-                " 00 00 00 00 00 00 00 00" + // timestamp
-                " 00 00 00 00 00 00 00 02" + // sequence
-                " 00 00 00 00 00 00 01 00" + // reply sequence
-                " 00 00 00 38" + // body size
-                " 00 00 00 07" + // VT_MAP
-                " 00 00 00 30" + // VT_MAP - size
-                " 00 00 00 01" + // VT_MAP - item count
-                " 00 00 00 06" + // VT_LIST
-                " 00 00 00 1C" + // data length
-                " 00 00 00 02" + // number of list items
-                " 00 00 00 01" + // VT_STRING
-                " 00 00 00 04" + // string length
-                " 74 65 74 74" + // test string
-                " 00 00 00 01" + // VT_STRING
-                " 00 00 00 04" + // string length
-                " 74 65 75 74" + // test string
-                " 00 00 00 04" + // name length - of VT_LIST
-                " 74 65 73 74" + // test name
-                "" );
+        final TestBytePacket packet = new TestBytePacket ( " 00 00 01 00" + // command code
+        " 00 00 00 00 00 00 00 00" + // timestamp
+        " 00 00 00 00 00 00 00 02" + // sequence
+        " 00 00 00 00 00 00 01 00" + // reply sequence
+        " 00 00 00 38" + // body size
+        " 00 00 00 07" + // VT_MAP
+        " 00 00 00 30" + // VT_MAP - size
+        " 00 00 00 01" + // VT_MAP - item count
+        " 00 00 00 06" + // VT_LIST
+        " 00 00 00 1C" + // data length
+        " 00 00 00 02" + // number of list items
+        " 00 00 00 01" + // VT_STRING
+        " 00 00 00 04" + // string length
+        " 74 65 74 74" + // test string
+        " 00 00 00 01" + // VT_STRING
+        " 00 00 00 04" + // string length
+        " 74 65 75 74" + // test string
+        " 00 00 00 04" + // name length - of VT_LIST
+        " 74 65 73 74" + // test name
+        "" );
 
         performCode ( message, packet );
     }
 
     public void testPerformance () throws InvalidValueTypeException
     {
-        Message message = new Message ();
+        final Message message = new Message ();
 
         message.getValues ().put ( "string-0", new StringValue ( "test" ) );
         message.getValues ().put ( "string-1", new StringValue ( "test" ) );
@@ -324,18 +333,18 @@ public class ProtocolGMPPTest
         message.getValues ().put ( "double-0", new DoubleValue ( 1.234 ) );
         message.getValues ().put ( "double-1", new DoubleValue ( 1.234 ) );
 
-        Protocol protocol = new ProtocolGMPP ( null, null );
+        final Protocol protocol = new ProtocolGMPP ( null, null );
 
-        _log.info ( "Start performance run" );
+        log.info ( "Start performance run" );
 
-        long start = System.currentTimeMillis ();
+        final long start = System.currentTimeMillis ();
         for ( int i = 0; i < 10 * 1000; i++ )
         {
             protocol.code ( message );
         }
-        long end = System.currentTimeMillis ();
+        final long end = System.currentTimeMillis ();
 
-        _log.info ( "Processing took: " + ( end - start ) + " ms" );
+        log.info ( "Processing took: " + ( end - start ) + " ms" );
 
     }
 }
