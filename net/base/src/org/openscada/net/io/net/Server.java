5868308fe70ea146b73ba287031b8f5789e8a2ab
Jens Reimann <jens.reimann@th4-systems.com>
2008-12-19 11:08:24 +0000

    cleanup
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis@3102 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- net/base/src/org/openscada/net/io/net/Server.java
+++ net/base/src/org/openscada/net/io/net/Server.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of the OpenSCADA project
- * Copyright (C) 2006 inavare GmbH (http://inavare.com)
+ * Copyright (C) 2006-2008 inavare GmbH (http://inavare.com)
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -30,40 +30,41 @@ import org.openscada.net.utils.MessageCreator;
 
 public class Server implements Runnable
 {
-    private ConnectionHandlerFactory _factory = null;
-    private IOProcessor _processor = null;
-    private ServerSocket _serverSocket = null;
+    private ConnectionHandlerFactory factory = null;
 
-    public Server ( ConnectionHandlerFactory factory, int port ) throws IOException
+    private IOProcessor processor = null;
+
+    @SuppressWarnings ( "unused" )
+    private ServerSocket serverSocket = null;
+
+    public Server ( final ConnectionHandlerFactory factory, final int port ) throws IOException
     {
         this ( factory, new IOProcessor (), port );
     }
 
-    public Server ( ConnectionHandlerFactory factory, IOProcessor processor, int port ) throws IOException
+    public Server ( final ConnectionHandlerFactory factory, final IOProcessor processor, final int port ) throws IOException
     {
-        _factory = factory;
-        _processor = processor;
+        this.factory = factory;
+        this.processor = processor;
 
-        _serverSocket = new ServerSocket ( _processor, new InetSocketAddress ( port ),
-                new ServerSocket.ConnectionFactory () {
+        this.serverSocket = new ServerSocket ( this.processor, new InetSocketAddress ( port ), new ServerSocket.ConnectionFactory () {
 
-                    public void accepted ( SocketConnection connection )
-                    {
-                        ServerConnection newConnection = new ServerConnection ( _factory.createConnectionHandler (),
-                                connection );
-                        newConnection.connected ();
-                        newConnection.sendMessage ( MessageCreator.createPing () );
-                    }
-                } );
+            public void accepted ( final SocketConnection connection )
+            {
+                final ServerConnection newConnection = new ServerConnection ( Server.this.factory.createConnectionHandler (), connection );
+                newConnection.connected ();
+                newConnection.sendMessage ( MessageCreator.createPing () );
+            }
+        } );
     }
 
     public void start ()
     {
-        _processor.start ();
+        this.processor.start ();
     }
 
     public void run ()
     {
-        _processor.run ();
+        this.processor.run ();
     }
 }
