c0daa69d13281fa5b3a03d908c39c477c81d00a3
Jens Reimann <jens.reimann@th4-systems.com>
2009-02-02 15:33:41 +0000

    reduce log output
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis@3133 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- da/server/common/src/org/openscada/da/server/common/factory/FactoryHelper.java
+++ da/server/common/src/org/openscada/da/server/common/factory/FactoryHelper.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of the OpenSCADA project
- * Copyright (C) 2006-2008 inavare GmbH (http://inavare.com)
+ * Copyright (C) 2006-2009 inavare GmbH (http://inavare.com)
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -45,7 +45,7 @@ public class FactoryHelper
             }
             catch ( final Throwable e )
             {
-                log.info ( "Failed to load ctor for HiveServiceRegistry" );
+                log.debug ( "Failed to load ctor for HiveServiceRegistry" );
             }
 
             if ( ctor != null )
