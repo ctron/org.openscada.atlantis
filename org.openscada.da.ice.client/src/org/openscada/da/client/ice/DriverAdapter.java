2ad1a7197be76a1ba2e4c4c516adb1ac951ca7a6
Jens Reimann <jens.reimann@th4-systems.com>
2009-02-05 09:36:00 +0000

    relocate to trunk
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis/trunk@3211 1b50e8ee-deed-0310-b1de-ab401e1c4d63

---
+++ org.openscada.da.ice.client/src/org/openscada/da/client/ice/DriverAdapter.java
@@ -0,0 +1,13 @@
+package org.openscada.da.client.ice;
+
+import org.openscada.core.ConnectionInformation;
+import org.openscada.core.client.Connection;
+
+public class DriverAdapter implements org.openscada.rcp.da.client.connector.DriverAdapter
+{
+    public Connection createConnection ( final ConnectionInformation connectionInformation )
+    {
+        return new DriverInformation ().create ( connectionInformation );
+    }
+
+}
