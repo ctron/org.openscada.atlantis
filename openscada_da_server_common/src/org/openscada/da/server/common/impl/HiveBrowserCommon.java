dc70abb454f81b07f9116a7c889ed79c56783220
Jens Reimann <jens.reimann@th4-systems.com>
2009-02-10 14:19:56 +0000

    reduce locking
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis/trunk@3343 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- openscada_da_server_common/src/org/openscada/da/server/common/impl/HiveBrowserCommon.java
+++ openscada_da_server_common/src/org/openscada/da/server/common/impl/HiveBrowserCommon.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of the OpenSCADA project
- * Copyright (C) 2006-2008 inavare GmbH (http://inavare.com)
+ * Copyright (C) 2006-2009 inavare GmbH (http://inavare.com)
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -39,25 +39,25 @@ import org.openscada.utils.jobqueue.OperationManager.Handle;
 
 public abstract class HiveBrowserCommon implements HiveBrowser, FolderListener, SessionListener
 {
-    private static Logger _log = Logger.getLogger ( HiveBrowserCommon.class );
+    private static Logger logger = Logger.getLogger ( HiveBrowserCommon.class );
 
-    private HiveCommon _hive = null;
+    private HiveCommon hive = null;
 
-    private final Map<Object, SessionCommon> _subscriberMap = new HashMap<Object, SessionCommon> ();
+    private final Map<Object, SessionCommon> subscriberMap = new HashMap<Object, SessionCommon> ();
 
     public HiveBrowserCommon ( final HiveCommon hive )
     {
-        this._hive = hive;
-        this._hive.addSessionListener ( this );
+        this.hive = hive;
+        this.hive.addSessionListener ( this );
     }
 
     public long startBrowse ( final Session session, final Location location, final BrowseOperationListener listener ) throws InvalidSessionException
     {
-        _log.debug ( "List request for: " + location.toString () );
+        logger.debug ( "List request for: " + location.toString () );
 
-        final SessionCommon sessionCommon = this._hive.validateSession ( session );
+        final SessionCommon sessionCommon = this.hive.validateSession ( session );
 
-        final Handle handle = this._hive.scheduleOperation ( sessionCommon, new RunnableCancelOperation () {
+        final Handle handle = this.hive.scheduleOperation ( sessionCommon, new RunnableCancelOperation () {
 
             public void run ()
             {
@@ -88,24 +88,24 @@ public abstract class HiveBrowserCommon implements HiveBrowser, FolderListener, 
 
     public void subscribe ( final Session session, final Location location ) throws NoSuchFolderException, InvalidSessionException
     {
-        this._hive.validateSession ( session );
+        this.hive.validateSession ( session );
 
         if ( getRootFolder () == null )
         {
-            _log.warn ( "Having a brower interface without root folder" );
+            logger.warn ( "Having a brower interface without root folder" );
             throw new NoSuchFolderException ();
         }
 
         final Stack<String> pathStack = location.getPathStack ();
 
-        synchronized ( this._subscriberMap )
+        synchronized ( this.subscriberMap )
         {
-            _log.debug ( "Adding path: " + location.toString () );
+            logger.debug ( "Adding path: " + location.toString () );
 
             final SessionCommon sessionCommon = (SessionCommon)session;
             final Object tag = new Object ();
             sessionCommon.getData ().addPath ( tag, new Location ( location ) );
-            this._subscriberMap.put ( tag, sessionCommon );
+            this.subscriberMap.put ( tag, sessionCommon );
 
             boolean success = false;
             try
@@ -118,7 +118,7 @@ public abstract class HiveBrowserCommon implements HiveBrowser, FolderListener, 
                 if ( !success )
                 {
                     sessionCommon.getData ().removePath ( new Location ( location ) );
-                    this._subscriberMap.remove ( tag );
+                    this.subscriberMap.remove ( tag );
                 }
             }
         }
@@ -127,11 +127,11 @@ public abstract class HiveBrowserCommon implements HiveBrowser, FolderListener, 
 
     public void unsubscribe ( final Session session, final Location location ) throws NoSuchFolderException, InvalidSessionException
     {
-        this._hive.validateSession ( session );
+        this.hive.validateSession ( session );
 
         if ( getRootFolder () == null )
         {
-            _log.warn ( "Having a brower interface without root folder" );
+            logger.warn ( "Having a brower interface without root folder" );
             throw new NoSuchFolderException ();
         }
 
@@ -144,9 +144,9 @@ public abstract class HiveBrowserCommon implements HiveBrowser, FolderListener, 
         if ( tag != null )
         {
             session.getData ().removePath ( location );
-            synchronized ( this._subscriberMap )
+            synchronized ( this.subscriberMap )
             {
-                this._subscriberMap.remove ( tag );
+                this.subscriberMap.remove ( tag );
             }
 
             final Stack<String> pathStack = location.getPathStack ();
@@ -157,23 +157,16 @@ public abstract class HiveBrowserCommon implements HiveBrowser, FolderListener, 
 
     public void changed ( final Object tag, final Collection<Entry> added, final Collection<String> removed, final boolean full )
     {
-        synchronized ( this._subscriberMap )
+        final SessionCommon session = this.subscriberMap.get ( tag );
+        if ( session != null )
         {
-            if ( this._subscriberMap.containsKey ( tag ) )
+            final Location location = session.getData ().getPaths ().get ( tag );
+            try
             {
-                final SessionCommon session = this._subscriberMap.get ( tag );
-                final Location location = session.getData ().getPaths ().get ( tag );
-                if ( location != null )
-                {
-                    try
-                    {
-                        session.getFolderListener ().folderChanged ( location, added, removed, full );
-                    }
-                    catch ( final Exception e )
-                    {
-                    }
-                }
-
+                session.getFolderListener ().folderChanged ( location, added, removed, full );
+            }
+            catch ( final Exception e )
+            {
             }
         }
     }
@@ -184,7 +177,7 @@ public abstract class HiveBrowserCommon implements HiveBrowser, FolderListener, 
 
     public void destroy ( final SessionCommon session )
     {
-        _log.debug ( String.format ( "Session destroy: %d entries", session.getData ().getPaths ().size () ) );
+        logger.debug ( String.format ( "Session destroy: %d entries", session.getData ().getPaths ().size () ) );
 
         Map<Object, Location> entries;
 
@@ -197,17 +190,17 @@ public abstract class HiveBrowserCommon implements HiveBrowser, FolderListener, 
         {
             try
             {
-                _log.debug ( "Unsubscribe path: " + entry.getValue ().toString () );
+                logger.debug ( "Unsubscribe path: " + entry.getValue ().toString () );
                 unsubscribePath ( session, entry.getValue () );
             }
             catch ( final NoSuchFolderException e )
             {
-                _log.warn ( "Unable to unsubscribe form path", e );
+                logger.warn ( "Unable to unsubscribe form path", e );
             }
         }
 
         session.getData ().clearPaths ();
-        _log.debug ( "Destruction of session ok" );
+        logger.debug ( "Destruction of session ok" );
     }
 
     public abstract Folder getRootFolder ();
