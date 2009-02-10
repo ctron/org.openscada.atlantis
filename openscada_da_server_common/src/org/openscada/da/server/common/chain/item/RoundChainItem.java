dd1843c27361cb7464bc2347aa1a07c6e1425bc3
Jens Reimann <jens.reimann@th4-systems.com>
2009-02-10 10:36:03 +0000

    add a chain item for rounding
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis/trunk@3333 1b50e8ee-deed-0310-b1de-ab401e1c4d63

---
+++ openscada_da_server_common/src/org/openscada/da/server/common/chain/item/RoundChainItem.java
@@ -0,0 +1,137 @@
+/*
+ * This file is part of the OpenSCADA project
+ * Copyright (C) 2006-2008 inavare GmbH (http://inavare.com)
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package org.openscada.da.server.common.chain.item;
+
+import java.util.Map;
+
+import org.openscada.core.Variant;
+import org.openscada.da.server.common.HiveServiceRegistry;
+import org.openscada.da.server.common.chain.BaseChainItemCommon;
+import org.openscada.da.server.common.chain.StringBinder;
+
+/**
+ * This class is a chain item which performs rounding
+ * @author Jens Reimann
+ *
+ */
+public class RoundChainItem extends BaseChainItemCommon
+{
+    public static final String ROUND_BASE = "org.openscada.da.round";
+
+    public static final String ORIGINAL_VALUE = ROUND_BASE + ".value.original";
+
+    public static final String ROUND_ACTIVE = ROUND_BASE + ".active";
+
+    public static final String ROUND_TYPE = ROUND_BASE + ".type";
+
+    public static final String ROUND_ERROR = ROUND_BASE + ".error";
+
+    private final StringBinder manualType = new StringBinder ();
+
+    private enum RoundType
+    {
+        NONE,
+        UP,
+        DOWN,
+        ROUND
+    }
+
+    public RoundChainItem ( final HiveServiceRegistry serviceRegistry )
+    {
+        super ( serviceRegistry );
+
+        addBinder ( ROUND_TYPE, this.manualType );
+        setReservedAttributes ( ORIGINAL_VALUE, ROUND_ACTIVE, ROUND_ERROR );
+    }
+
+    public void process ( final Variant value, final Map<String, Variant> attributes )
+    {
+        attributes.put ( ROUND_ACTIVE, null );
+        attributes.put ( ORIGINAL_VALUE, null );
+        attributes.put ( ROUND_ERROR, null );
+
+        final Variant originalValue = value;
+
+        // check and get the rounding type
+        final RoundType type = checkRoundType ( attributes );
+
+        Double doubleValue = null;
+
+        try
+        {
+            if ( value.isDouble () )
+            {
+                doubleValue = value.asDouble ();
+            }
+        }
+        catch ( final Throwable e )
+        {
+        }
+
+        // common section
+        if ( type != RoundType.NONE && doubleValue != null )
+        {
+            performRound ( type, doubleValue, value, attributes );
+            attributes.put ( ROUND_ACTIVE, new Variant ( true ) );
+            attributes.put ( ORIGINAL_VALUE, originalValue );
+        }
+
+        // add binder attributes
+        addAttributes ( attributes );
+        attributes.put ( ROUND_TYPE, new Variant ( type.toString () ) );
+    }
+
+    private void performRound ( final RoundType type, final double doubleValue, final Variant value, final Map<String, Variant> attributes )
+    {
+        // round
+        switch ( type )
+        {
+        case NONE:
+            // no-op
+            break;
+        case UP:
+            value.setValue ( Math.ceil ( doubleValue ) );
+            break;
+        case DOWN:
+            value.setValue ( Math.floor ( doubleValue ) );
+            break;
+        case ROUND:
+            value.setValue ( Math.round ( doubleValue ) );
+            break;
+        }
+    }
+
+    private RoundType checkRoundType ( final Map<String, Variant> attributes )
+    {
+        try
+        {
+            final RoundType type = RoundType.valueOf ( this.manualType.getValue () );
+            if ( type != null )
+            {
+                return type;
+            }
+        }
+        catch ( final Throwable e )
+        {
+            attributes.put ( ROUND_ERROR, new Variant ( e.getMessage () ) );
+        }
+        return RoundType.NONE;
+    }
+}
