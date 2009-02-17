66d9eb165425b92831cd3563f6ff42d8d02d342d
Jens Reimann <jens.reimann@th4-systems.com>
2009-02-17 09:42:45 +0000

    Initial checkin
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis/branches/oaw_net_mdd@3386 1b50e8ee-deed-0310-b1de-ab401e1c4d63

---
+++ org.openscada.net.da.model/src-gen/Connection.java
@@ -0,0 +1,19 @@
+public interface Connection {
+
+	public abstract void
+
+	writeAttributes(
+
+	final WriteAttributes argument
+
+	);
+
+	public abstract FolderSubscribe
+
+	writeValue(
+
+	final WriteValue argument
+
+	);
+
+}
---
+++ org.openscada.net.da.model/src-gen/ConnectionClientImpl.java
@@ -0,0 +1,25 @@
+public class ConnectionClientImpl implements Connection {
+
+	public void
+
+	writeAttributes(
+
+	final WriteAttributes argument
+
+	) {
+
+	}
+
+	public FolderSubscribe
+
+	writeValue(
+
+	final WriteValue argument
+
+	) {
+
+		return null;
+
+	}
+
+}
