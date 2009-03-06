9e4f922981f289b17087b51fd50a6497c2bb3166
Jens Reimann <jens.reimann@th4-systems.com>
2009-03-06 13:44:27 +0000

    add two new testcases
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis/trunk@3613 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- openscada_core_common/testsrc/org/openscada/core/ConnectionInformationTest1.java
+++ openscada_core_common/testsrc/org/openscada/core/ConnectionInformationTest1.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of the OpenSCADA project
- * Copyright (C) 2006-2007 inavare GmbH (http://inavare.com)
+ * Copyright (C) 2006-2009 inavare GmbH (http://inavare.com)
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -19,132 +19,133 @@
 
 package org.openscada.core;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import junit.framework.Assert;
 
 import org.junit.Test;
-import org.openscada.core.ConnectionInformation;
 
 public class ConnectionInformationTest1
 {
-    
-    protected void testParserEqual ( String uri, ConnectionInformation expected )
+
+    protected void testParserEqual ( final String uri, final ConnectionInformation expected )
     {
-        ConnectionInformation actual = ConnectionInformation.fromURI ( uri );
+        final ConnectionInformation actual = ConnectionInformation.fromURI ( uri );
         assertNotNull ( "Object is null", actual );
         assertEquals ( expected, actual );
     }
-    
+
     @Test
     public void test1 () throws Throwable
     {
-        ConnectionInformation expected = new ConnectionInformation ();
+        final ConnectionInformation expected = new ConnectionInformation ();
         expected.setInterface ( "da" );
         expected.setDriver ( "net" );
         expected.setTarget ( "localhost" );
         expected.setSecondaryTarget ( 1202 );
-        
+
         testParserEqual ( "da:net://localhost:1202", expected );
     }
-    
+
     @Test
     public void test2 () throws Throwable
     {
-        ConnectionInformation expected = new ConnectionInformation ();
+        final ConnectionInformation expected = new ConnectionInformation ();
         expected.setInterface ( "da" );
         expected.getProperties ().put ( "user", "jens" );
         expected.getProperties ().put ( "password", "test" );
         expected.setDriver ( "net" );
         expected.setTarget ( "localhost" );
         expected.setSecondaryTarget ( 1202 );
-        
+
         testParserEqual ( "da:net://jens:test@localhost:1202", expected );
     }
-    
+
     @Test
     public void test3 () throws Throwable
     {
-        ConnectionInformation expected = new ConnectionInformation ();
+        final ConnectionInformation expected = new ConnectionInformation ();
         expected.setInterface ( "da" );
         expected.setDriver ( "net" );
         expected.setTarget ( "localhost" );
         expected.setSecondaryTarget ( 1202 );
         expected.getSubtargets ().add ( "sub1" );
         expected.getSubtargets ().add ( "sub2" );
-        
+
         testParserEqual ( "da:net://localhost:1202/sub1/sub2", expected );
     }
-    
+
     @Test
     public void test4 () throws Throwable
     {
-        ConnectionInformation expected = new ConnectionInformation ();
+        final ConnectionInformation expected = new ConnectionInformation ();
         expected.setInterface ( "da" );
         expected.setDriver ( "net" );
         expected.setTarget ( "localhost" );
         expected.setSecondaryTarget ( 1202 );
-        
+
         testParserEqual ( "da:net://localhost:1202/", expected );
     }
-    
+
     @Test
     public void test5 () throws Throwable
     {
-        ConnectionInformation expected = new ConnectionInformation ();
+        final ConnectionInformation expected = new ConnectionInformation ();
         expected.setInterface ( "da" );
         expected.setDriver ( "net" );
         expected.setTarget ( "localhost" );
         expected.setSecondaryTarget ( 1202 );
         expected.getProperties ().put ( "key", "value" );
-        
+
         testParserEqual ( "da:net://localhost:1202?key=value", expected );
     }
-    
+
     @Test
     public void test6 () throws Throwable
     {
-        ConnectionInformation expected = new ConnectionInformation ();
+        final ConnectionInformation expected = new ConnectionInformation ();
         expected.setInterface ( "da" );
         expected.setDriver ( "net" );
         expected.setTarget ( "localhost" );
         expected.setSecondaryTarget ( 1202 );
         expected.getProperties ().put ( "key1", "value1" );
         expected.getProperties ().put ( "key2", "value2" );
-        
+
         testParserEqual ( "da:net://localhost:1202?key1=value1&key2=value2", expected );
     }
-    
+
     @Test
     public void test6a () throws Throwable
     {
-        ConnectionInformation expected = new ConnectionInformation ();
+        final ConnectionInformation expected = new ConnectionInformation ();
         expected.setInterface ( "da" );
         expected.setDriver ( "net" );
         expected.setTarget ( "localhost" );
         expected.setSecondaryTarget ( 1202 );
         expected.getProperties ().put ( "key1", "value1" );
         expected.getProperties ().put ( "key2", "value2" );
-        
+
         testParserEqual ( "da:net://localhost:1202?key2=value2&key1=value1", expected );
     }
-    
+
     @Test
     public void test6b () throws Throwable
     {
-        ConnectionInformation expected = new ConnectionInformation ();
+        final ConnectionInformation expected = new ConnectionInformation ();
         expected.setInterface ( "da" );
         expected.setDriver ( "net" );
         expected.setTarget ( "localhost" );
         expected.setSecondaryTarget ( 1202 );
         expected.getProperties ().put ( "key1", "value1" );
         expected.getProperties ().put ( "key2", "value2" );
-        
+
         testParserEqual ( "da:net://localhost:1202?key2=value3&key1=value1&key2=value2", expected );
     }
-    
+
     @Test
     public void test7 () throws Throwable
     {
-        ConnectionInformation expected = new ConnectionInformation ();
+        final ConnectionInformation expected = new ConnectionInformation ();
         expected.setInterface ( "da" );
         expected.setDriver ( "net" );
         expected.getProperties ().put ( "user", "jens" );
@@ -155,14 +156,14 @@ public class ConnectionInformationTest1
         expected.setSecondaryTarget ( 1202 );
         expected.getProperties ().put ( "key1", "value1" );
         expected.getProperties ().put ( "key2", "value2" );
-        
+
         testParserEqual ( "da:net://jens:test@localhost:1202/sub1/sub2?key1=value1&key2=value2", expected );
     }
-    
+
     @Test
     public void test8 () throws Throwable
     {
-        ConnectionInformation expected = new ConnectionInformation ();
+        final ConnectionInformation expected = new ConnectionInformation ();
         expected.setInterface ( "da" );
         expected.setDriver ( "net" );
         expected.getProperties ().put ( "user", ":" );
@@ -172,14 +173,14 @@ public class ConnectionInformationTest1
         expected.getSubtargets ().add ( ":" );
         expected.setSecondaryTarget ( 1202 );
         expected.getProperties ().put ( ":", ":" );
-        
+
         testParserEqual ( "da:net://%3A:%3A@localhost:1202/%3A/%3A?%3A=%3A", expected );
     }
-    
+
     @Test
     public void test9 () throws Throwable
     {
-        ConnectionInformation expected = new ConnectionInformation ();
+        final ConnectionInformation expected = new ConnectionInformation ();
         expected.setInterface ( "da" );
         expected.setDriver ( "net" );
         expected.getProperties ().put ( "user", "&" );
@@ -189,14 +190,14 @@ public class ConnectionInformationTest1
         expected.getSubtargets ().add ( "&" );
         expected.setSecondaryTarget ( 1202 );
         expected.getProperties ().put ( "&", "&" );
-        
+
         testParserEqual ( "da:net://%26:%26@localhost:1202/%26/%26?%26=%26", expected );
     }
-    
+
     @Test
     public void test10 () throws Throwable
     {
-        ConnectionInformation expected = new ConnectionInformation ();
+        final ConnectionInformation expected = new ConnectionInformation ();
         expected.setInterface ( "da" );
         expected.setDriver ( "net" );
         expected.getProperties ().put ( "user", "%" );
@@ -206,14 +207,14 @@ public class ConnectionInformationTest1
         expected.getSubtargets ().add ( "%" );
         expected.setSecondaryTarget ( 1202 );
         expected.getProperties ().put ( "%", "%" );
-        
+
         testParserEqual ( "da:net://%25:%25@localhost:1202/%25/%25?%25=%25", expected );
     }
-    
+
     @Test
     public void test11 () throws Throwable
     {
-        ConnectionInformation expected = new ConnectionInformation ();
+        final ConnectionInformation expected = new ConnectionInformation ();
         expected.setInterface ( "da" );
         expected.setDriver ( "net" );
         expected.getProperties ().put ( "user", "%" );
@@ -223,14 +224,14 @@ public class ConnectionInformationTest1
         expected.getSubtargets ().add ( "%" );
         expected.setSecondaryTarget ( 1202 );
         expected.getProperties ().put ( "%", "%" );
-        
+
         testParserEqual ( "da:net://%25:%25@localhost:1202/%25/%25?%25=%25", expected );
     }
-    
+
     @Test
     public void test12 () throws Throwable
     {
-        ConnectionInformation o = new ConnectionInformation ();
+        final ConnectionInformation o = new ConnectionInformation ();
         o.setInterface ( "da" );
         o.setDriver ( "net" );
         o.getProperties ().put ( "user", "jens" );
@@ -240,6 +241,18 @@ public class ConnectionInformationTest1
         o.getSubtargets ().add ( "sub1" );
         o.getSubtargets ().add ( "sub2" );
         o.getProperties ().put ( "key", "value" );
-        assertEquals ( o.toString (), "da:net://jens:test@localhost:1202/sub1/sub2?key=value" );
+        Assert.assertEquals ( o.toString (), "da:net://jens:test@localhost:1202/sub1/sub2?key=value" );
+    }
+
+    @Test
+    public void testEquals1 () throws Throwable
+    {
+        Assert.assertEquals ( ConnectionInformation.fromURI ( "da:net://localhost:1202" ), ConnectionInformation.fromURI ( "da:net://localhost:1202" ) );
+    }
+
+    @Test
+    public void testEquals2 () throws Throwable
+    {
+        Assert.assertNotSame ( ConnectionInformation.fromURI ( "da:net://localhost:1202" ), ConnectionInformation.fromURI ( "da:net://localhost:1203" ) );
     }
 }
