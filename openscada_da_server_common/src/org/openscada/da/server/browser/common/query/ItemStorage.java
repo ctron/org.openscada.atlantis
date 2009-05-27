4ed83a51636d4aa6993acd9ad593776f715c8047
Jens Reimann <jens.reimann@th4-systems.com>
2009-05-27 09:49:48 +0000

    cleanup
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis/trunk@4120 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- openscada_da_server_common/src/org/openscada/da/server/browser/common/query/ItemStorage.java
+++ openscada_da_server_common/src/org/openscada/da/server/browser/common/query/ItemStorage.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of the OpenSCADA project
- * Copyright (C) 2006 inavare GmbH (http://inavare.com)
+ * Copyright (C) 2006-2009 inavare GmbH (http://inavare.com)
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -21,7 +21,7 @@ package org.openscada.da.server.browser.common.query;
 
 public interface ItemStorage
 {
-    void added ( ItemDescriptor descriptor );
+    public void added ( ItemDescriptor descriptor );
 
-    void removed ( ItemDescriptor descriptor );
+    public void removed ( ItemDescriptor descriptor );
 }
