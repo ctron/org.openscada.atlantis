93a607a62a3e9bdbdc890af6acd9b65f916a61c5
Jens Reimann <jens.reimann@th4-systems.com>
2009-06-03 09:00:34 +0000

    use a common set instead of specific enumset
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis/trunk@4204 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- openscada_da_core_common/src/org/openscada/da/core/DataItemInformation.java
+++ openscada_da_core_common/src/org/openscada/da/core/DataItemInformation.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of the OpenSCADA project
- * Copyright (C) 2006 inavare GmbH (http://inavare.com)
+ * Copyright (C) 2006-2009 inavare GmbH (http://inavare.com)
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -19,21 +19,19 @@
 
 package org.openscada.da.core;
 
-import java.util.EnumSet;
-
-
+import java.util.Set;
 
 /**
  * Data item information
  * <p>
  * Data items information objects must be equal on their name!
- * @author jens
+ * @author Jens Reimann
  *
  */
 public interface DataItemInformation
 {
-    public abstract EnumSet<IODirection> getIODirection ();
-    
+    public abstract Set<IODirection> getIODirection ();
+
     /**
      * Get the ID of the data item
      * <p>
