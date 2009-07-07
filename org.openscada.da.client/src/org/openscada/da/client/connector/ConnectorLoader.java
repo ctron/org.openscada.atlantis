0f8aca859e0e9eea1e7bc1ce71e5fad568272792
Jens Reimann <jens.reimann@th4-systems.com>
2009-07-07 12:35:24 +0000

    rename bundle to end with ".connector"
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis/trunk@4465 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- org.openscada.da.client/src/org/openscada/da/client/connector/ConnectorLoader.java
+++
@@ -1,6 +0,0 @@
-package org.openscada.da.client.connector;
-
-public interface ConnectorLoader
-{
-    public abstract void load ();
-}
--- org.openscada.da.client/src/org/openscada/da/client/connector/DriverAdapter.java
+++
@@ -1,9 +0,0 @@
-package org.openscada.da.client.connector;
-
-import org.openscada.core.ConnectionInformation;
-import org.openscada.core.client.Connection;
-
-public interface DriverAdapter
-{
-    public Connection createConnection ( ConnectionInformation connectionInformation );
-}
