4868f18078432208e8712b269d15d0abe9b21569
Juergen Rose <juergen.rose@th4-systems.com>
2009-07-22 12:02:49 +0000

    handle empty prefixes as well
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis/branches/0.13.x-release@4654 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- openscada_da_proxyserver/src/org/openscada/da/server/proxy/utils/ProxyUtils.java
+++ openscada_da_proxyserver/src/org/openscada/da/server/proxy/utils/ProxyUtils.java
@@ -36,6 +36,9 @@ public class ProxyUtils
      */
     public static String originalItemId ( final String itemId, final String separator, final ProxyPrefixName proxyPrefix, final ProxyPrefixName originalPrefix )
     {
+    	if ( originalPrefix == null || "".equals ( originalPrefix.getName() ) ) {
+    		return itemId.replaceFirst ( Pattern.quote ( proxyPrefix.getName () + separator ), "" );
+    	}
         return itemId.replaceFirst ( Pattern.quote ( proxyPrefix.getName () + separator ), originalPrefix.getName () + separator );
     }
 
@@ -48,6 +51,9 @@ public class ProxyUtils
      */
     public static String proxyItemId ( final String itemId, final String separator, final ProxyPrefixName proxyPrefix, final ProxyPrefixName originalPrefix )
     {
+    	if ( originalPrefix == null || "".equals ( originalPrefix.getName() ) ) {
+    		return proxyPrefix.getName () + separator + itemId;
+    	}
         return itemId.replaceFirst ( Pattern.quote ( originalPrefix.getName () + separator ), proxyPrefix.getName () + separator );
     }
 
@@ -60,6 +66,9 @@ public class ProxyUtils
      */
     public static boolean isOriginalItemForProxyGroup ( final String itemId, final String separator, final ProxyPrefixName originalPrefix )
     {
+    	if ( originalPrefix == null || "".equals ( originalPrefix.getName() ) ) {
+    		return true;
+    	}
         return itemId.startsWith ( originalPrefix.getName () + separator );
     }
 }
