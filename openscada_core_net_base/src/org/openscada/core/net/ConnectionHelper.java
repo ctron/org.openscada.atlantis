2e74ce27ffe8e27feb22234e4619e8c89a48103f
Jens Reimann <jens.reimann@th4-systems.com>
2009-08-04 16:02:19 +0000

    provide basic SSL
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis/trunk@4703 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- openscada_core_net_base/src/org/openscada/core/net/ConnectionHelper.java
+++ openscada_core_net_base/src/org/openscada/core/net/ConnectionHelper.java
@@ -19,9 +19,22 @@
 
 package org.openscada.core.net;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 
+import javax.net.ssl.KeyManager;
+import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import org.apache.log4j.Logger;
 import org.apache.mina.core.filterchain.DefaultIoFilterChainBuilder;
@@ -36,6 +49,25 @@ import org.openscada.net.mina.GMPPProtocolEncoder;
 public class ConnectionHelper
 {
 
+    private static final class X509TrustManagerImplementation implements X509TrustManager
+    {
+        public void checkClientTrusted ( final X509Certificate[] arg0, final String arg1 ) throws CertificateException
+        {
+            System.out.println ( "checkClientTrusted: " + arg0 + "/" + arg1 );
+        }
+
+        public void checkServerTrusted ( final X509Certificate[] arg0, final String arg1 ) throws CertificateException
+        {
+            System.out.println ( "checkServerTrusted: " + arg0 + "/" + arg1 );
+        }
+
+        public X509Certificate[] getAcceptedIssuers ()
+        {
+            System.out.println ( "getAcceptedIssuers" );
+            return new X509Certificate[0];
+        }
+    }
+
     private static Logger logger = Logger.getLogger ( ConnectionHelper.class );
 
     /**
@@ -43,7 +75,7 @@ public class ConnectionHelper
      * @param connectionInformation the connection information to use
      * @param filterChainBuilder the chain builder
      */
-    public static void setupFilterChain ( final ConnectionInformation connectionInformation, final DefaultIoFilterChainBuilder filterChainBuilder )
+    public static void setupFilterChain ( final ConnectionInformation connectionInformation, final DefaultIoFilterChainBuilder filterChainBuilder, final boolean isClient )
     {
         // set up compression
         final String compress = connectionInformation.getProperties ().get ( "compress" );
@@ -56,37 +88,7 @@ public class ConnectionHelper
         final String ssl = connectionInformation.getProperties ().get ( "ssl" );
         if ( ssl != null )
         {
-
-            String sslProtocol = connectionInformation.getProperties ().get ( "sslProtocol" );
-            if ( sslProtocol == null || sslProtocol.length () == 0 )
-            {
-                sslProtocol = "SSLv3";
-            }
-
-            final String sslRandom = connectionInformation.getProperties ().get ( "sslRandom" );
-
-            SSLContext sslContext = null;
-            try
-            {
-                sslContext = SSLContext.getInstance ( sslProtocol );
-
-                SecureRandom random = null;
-                if ( sslRandom != null && sslRandom.length () == 0 )
-                {
-                    random = SecureRandom.getInstance ( sslRandom );
-                }
-
-                sslContext.init ( null, null, random );
-            }
-            catch ( final Throwable e )
-            {
-                logger.warn ( "Failed to enable SSL" );
-            }
-
-            if ( sslContext != null )
-            {
-                filterChainBuilder.addLast ( "ssl", new SslFilter ( sslContext ) );
-            }
+            initSsl ( connectionInformation, filterChainBuilder, isClient );
         }
 
         // set up logging
@@ -98,5 +100,120 @@ public class ConnectionHelper
 
         // add the main codec
         filterChainBuilder.addLast ( "codec", new ProtocolCodecFilter ( new GMPPProtocolEncoder (), new GMPPProtocolDecoder () ) );
+    }
+
+    /**
+     * FIXME: still need to implement correctly
+     * @param connectionInformation
+     * @param filterChainBuilder
+     * @param isClient
+     */
+    protected static void initSsl ( final ConnectionInformation connectionInformation, final DefaultIoFilterChainBuilder filterChainBuilder, final boolean isClient )
+    {
+        SSLContext sslContext = null;
+        try
+        {
+            sslContext = createContext ( connectionInformation );
+            sslContext.init ( getKeyManagers ( connectionInformation, isClient ), getTrustManagers ( connectionInformation ), getRandom ( connectionInformation ) );
+        }
+        catch ( final Throwable e )
+        {
+            logger.warn ( "Failed to enable SSL", e );
+        }
+
+        if ( sslContext != null )
+        {
+            final SslFilter filter = new SslFilter ( sslContext );
+            filter.setUseClientMode ( isClient );
+            filterChainBuilder.addFirst ( "sslFilter", filter );
+        }
+    }
+
+    private static SSLContext createContext ( final ConnectionInformation connectionInformation ) throws NoSuchAlgorithmException
+    {
+        String sslProtocol = connectionInformation.getProperties ().get ( "sslProtocol" );
+        if ( sslProtocol == null || sslProtocol.length () == 0 )
+        {
+            sslProtocol = "SSLv3";
+        }
+
+        return SSLContext.getInstance ( sslProtocol );
+    }
+
+    private static SecureRandom getRandom ( final ConnectionInformation connectionInformation ) throws NoSuchAlgorithmException
+    {
+        final String sslRandom = connectionInformation.getProperties ().get ( "sslRandom" );
+
+        SecureRandom random = null;
+        if ( sslRandom != null && sslRandom.length () > 0 )
+        {
+            random = SecureRandom.getInstance ( sslRandom );
+            return random;
+        }
+        return null;
+    }
+
+    private static TrustManager[] getTrustManagers ( final ConnectionInformation connectionInformation )
+    {
+        return new TrustManager[] { new X509TrustManagerImplementation () };
+    }
+
+    private static KeyManager[] getKeyManagers ( final ConnectionInformation connectionInformation, final boolean isClient ) throws KeyStoreException, NoSuchAlgorithmException, UnrecoverableKeyException, CertificateException, IOException
+    {
+        if ( isClient )
+        {
+            return null;
+        }
+
+        final KeyStore keyStore;
+
+        keyStore = createKeyStore ( connectionInformation );
+
+        final String keyManagerFactory = KeyManagerFactory.getDefaultAlgorithm ();
+        final KeyManagerFactory kmf = KeyManagerFactory.getInstance ( keyManagerFactory );
+
+        kmf.init ( keyStore, getPassword ( connectionInformation, "sslCertPassword" ) );
+
+        return kmf.getKeyManagers ();
+    }
+
+    private static KeyStore createKeyStore ( final ConnectionInformation connectionInformation ) throws KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException
+    {
+        final KeyStore keyStore;
+        final String keyStoreType = connectionInformation.getProperties ().get ( "sslKeyStoreType" );
+        if ( keyStoreType != null )
+        {
+            keyStore = KeyStore.getInstance ( keyStoreType );
+        }
+        else
+        {
+            keyStore = KeyStore.getInstance ( KeyStore.getDefaultType () );
+        }
+
+        keyStore.load ( getKeyStoreStream ( connectionInformation ), getPassword ( connectionInformation, "sslKeyStorePassword" ) );
+
+        return keyStore;
+    }
+
+    private static InputStream getKeyStoreStream ( final ConnectionInformation connectionInformation ) throws IOException
+    {
+        final String uri = connectionInformation.getProperties ().get ( "sslKeyStoreUri" );
+        final URL url = new URL ( uri );
+        return url.openStream ();
+    }
+
+    private static char[] getPassword ( final ConnectionInformation connectionInformation, final String propertyName )
+    {
+        final char[] passwordChars;
+        final String password = connectionInformation.getProperties ().get ( propertyName );
+        if ( password != null )
+        {
+            passwordChars = password.toCharArray ();
+        }
+        else
+        {
+            passwordChars = null;
+        }
+        return passwordChars;
     }
 }
