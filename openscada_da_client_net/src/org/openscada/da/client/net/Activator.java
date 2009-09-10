6db4e976718ca4c1db51ea6265ab7520dcc65b18
Jens Reimann <jens.reimann@th4-systems.com>
2009-09-10 14:50:43 +0000

    de-mavenize
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis/trunk@4824 1b50e8ee-deed-0310-b1de-ab401e1c4d63

---
+++ openscada_da_client_net/src/org/openscada/da/client/net/Activator.java
@@ -0,0 +1,60 @@
+/*
+ * This file is part of the OpenSCADA project
+ * Copyright (C) 2006-2009 inavare GmbH (http://inavare.com)
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package org.openscada.da.client.net;
+
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
+import org.osgi.framework.ServiceRegistration;
+
+/**
+ * An OSGi bundle activator which registers the DriverAdapter with OSGi
+ * @author Jens Reimann
+ * @since 0.14.0
+ *
+ */
+public class Activator implements BundleActivator
+{
+    private org.openscada.core.client.DriverFactory factory;
+
+    private ServiceRegistration handle;
+
+    public void start ( final BundleContext context ) throws Exception
+    {
+        this.factory = new DriverFactory ();
+
+        final Dictionary<String, String> properties = new Hashtable<String, String> ();
+        properties.put ( "interface", "da" );
+        properties.put ( "transport", "net" );
+        properties.put ( Constants.SERVICE_DESCRIPTION, "OpenSCADA DA NET Adapter" );
+        properties.put ( Constants.SERVICE_VENDOR, "inavare GmbH" );
+        this.handle = context.registerService ( org.openscada.core.client.DriverFactory.class.getName (), this.factory, properties );
+    }
+
+    public void stop ( final BundleContext context ) throws Exception
+    {
+        this.handle.unregister ();
+        this.factory = null;
+    }
+
+}
