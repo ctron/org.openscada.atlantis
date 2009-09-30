4821a1df2e77fc01c19214aaa018e5249e6e71fd
Jens Reimann <jens.reimann@th4-systems.com>
2009-09-30 12:10:43 +0000

    register driver adapter
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis/trunk@5159 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- org.openscada.hd.client.net/src/org/openscada/hd/client/net/Activator.java
+++ org.openscada.hd.client.net/src/org/openscada/hd/client/net/Activator.java
@@ -1,22 +1,44 @@
 package org.openscada.hd.client.net;
 
+import java.util.Dictionary;
+import java.util.Hashtable;
+
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
+import org.osgi.framework.ServiceRegistration;
 
-public class Activator implements BundleActivator {
+public class Activator implements BundleActivator
+{
+    private DriverFactoryImpl factory;
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
-	 */
-	public void start(BundleContext context) throws Exception {
-	}
+    private ServiceRegistration handle;
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
-	 */
-	public void stop(BundleContext context) throws Exception {
-	}
+    /*
+     * (non-Javadoc)
+     * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+     */
+    public void start ( final BundleContext context ) throws Exception
+    {
+        this.factory = new DriverFactoryImpl ();
+
+        final Dictionary<String, String> properties = new Hashtable<String, String> ();
+        properties.put ( org.openscada.core.client.DriverFactory.INTERFACE_NAME, "hd" );
+        properties.put ( org.openscada.core.client.DriverFactory.DRIVER_NAME, "net" );
+        properties.put ( Constants.SERVICE_DESCRIPTION, "OpenSCADA HD NET Adapter" );
+        properties.put ( Constants.SERVICE_VENDOR, "inavare GmbH" );
+        this.handle = context.registerService ( org.openscada.core.client.DriverFactory.class.getName (), this.factory, properties );
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+     */
+    public void stop ( final BundleContext context ) throws Exception
+    {
+        this.handle.unregister ();
+        this.handle = null;
+        this.factory = null;
+    }
 
 }
