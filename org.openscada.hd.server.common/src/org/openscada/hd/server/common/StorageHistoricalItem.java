bde11f3d73327d743834fc72a605a83fcc4e0137
Jens Reimann <jens.reimann@th4-systems.com>
2009-10-02 13:01:26 +0000

    make two interfaces
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis/trunk@5187 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- org.openscada.hd.server.common/src/org/openscada/hd/server/common/StorageHistoricalItem.java
+++ org.openscada.hd.server.common/src/org/openscada/hd/server/common/StorageHistoricalItem.java
@@ -1,16 +1,8 @@
 package org.openscada.hd.server.common;
 
 import org.openscada.da.client.DataItemValue;
-import org.openscada.hd.HistoricalItemInformation;
-import org.openscada.hd.Query;
-import org.openscada.hd.QueryListener;
-import org.openscada.hd.QueryParameters;
 
-public interface StorageHistoricalItem
+public interface StorageHistoricalItem extends HistoricalItem
 {
-    public Query createQuery ( QueryParameters parameters, QueryListener listener );
-
-    public HistoricalItemInformation getInformation ();
-
     public void updateData ( DataItemValue value );
 }
