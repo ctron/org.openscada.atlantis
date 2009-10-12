44a2f357b4878d26b0acb8b953465426a5c3fec9
Jens Reimann <jens.reimann@th4-systems.com>
2009-10-12 15:57:12 +0000

    add some security checks
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis/trunk@5323 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- org.openscada.hd.server.common/src/org/openscada/hd/server/common/internal/QueryImpl.java
+++ org.openscada.hd.server.common/src/org/openscada/hd/server/common/internal/QueryImpl.java
@@ -18,6 +18,8 @@ public class QueryImpl implements Query, QueryListener
 
     private final QueryListener listener;
 
+    private QueryParameters queryParameters;
+
     public QueryImpl ( final SessionImpl session, final QueryListener listener )
     {
         this.session = session;
@@ -43,16 +45,64 @@ public class QueryImpl implements Query, QueryListener
 
     public void updateParameters ( final QueryParameters parameters, final Set<String> valueTypes )
     {
+        if ( parameters == null )
+        {
+            throw new IllegalArgumentException ( "'parameters' must not be null" );
+        }
+        if ( valueTypes == null || valueTypes.isEmpty () )
+        {
+            throw new IllegalArgumentException ( "'valueTypes' must not be null or empty" );
+        }
+
+        this.queryParameters = parameters;
         this.listener.updateParameters ( parameters, valueTypes );
     }
 
     public void updateData ( final int index, final Map<String, Value[]> values, final ValueInformation[] valueInformation )
     {
+        if ( values == null )
+        {
+            throw new IllegalArgumentException ( "'values' must not be null" );
+        }
+        if ( values == null )
+        {
+            throw new IllegalArgumentException ( "'valueInformation' must not be null" );
+        }
+        if ( this.queryParameters == null )
+        {
+            throw new IllegalStateException ( "'updateData' must be called after a call to 'updateParameters'" );
+        }
+        if ( index <= 0 || index >= this.queryParameters.getEntries () )
+        {
+            throw new IllegalArgumentException ( "'index' must be greater or equal to zero and lower than the number of reported entries" );
+        }
+        for ( final Map.Entry<String, Value[]> entry : values.entrySet () )
+        {
+            if ( entry.getValue () == null )
+            {
+                throw new IllegalArgumentException ( String.format ( "The values for '%s' are null", entry.getKey () ) );
+            }
+            if ( entry.getValue ().length != valueInformation.length )
+            {
+                throw new IllegalArgumentException ( String.format ( "The number of entries for '%s' is not equal to the rest of the entry count", entry.getKey () ) );
+            }
+        }
+        if ( index + valueInformation.length > this.queryParameters.getEntries () )
+        {
+            throw new IllegalArgumentException ( "The reported data exceeds reported number of entries" );
+        }
+
+        // finally we can pass on the event
         this.listener.updateData ( index, values, valueInformation );
     }
 
     public void updateState ( final QueryState state )
     {
+        if ( state == null )
+        {
+            throw new IllegalArgumentException ( "'state' must not be null" );
+        }
+
         try
         {
             this.listener.updateState ( state );
