01260c0cc2dd17fa9738da183dce6285c1985038
Jens Reimann <jens.reimann@th4-systems.com>
2009-10-12 14:42:02 +0000

    add folder manager
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis/trunk@5312 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- org.openscada.da.connection.provider/src/org/openscada/da/connection/provider/ConnectionService.java
+++ org.openscada.da.connection.provider/src/org/openscada/da/connection/provider/ConnectionService.java
@@ -1,11 +1,14 @@
 package org.openscada.da.connection.provider;
 
 import org.openscada.da.client.Connection;
+import org.openscada.da.client.FolderManager;
 import org.openscada.da.client.ItemManager;
 
 public interface ConnectionService extends org.openscada.core.connection.provider.ConnectionService
 {
     public ItemManager getItemManager ();
+
+    public FolderManager getFolderManager ();
 
     public Connection getConnection ();
 }
--- org.openscada.da.connection.provider/src/org/openscada/da/connection/provider/internal/ConnectionServiceImpl.java
+++ org.openscada.da.connection.provider/src/org/openscada/da/connection/provider/internal/ConnectionServiceImpl.java
@@ -2,12 +2,15 @@ package org.openscada.da.connection.provider.internal;
 
 import org.openscada.core.connection.provider.AbstractConnectionService;
 import org.openscada.da.client.Connection;
+import org.openscada.da.client.FolderManager;
 import org.openscada.da.client.ItemManager;
 import org.openscada.da.connection.provider.ConnectionService;
 
 public class ConnectionServiceImpl extends AbstractConnectionService implements ConnectionService
 {
     private final ItemManager itemManager;
+
+    private final FolderManager folderManager;
 
     private final Connection connection;
 
@@ -16,12 +19,18 @@ public class ConnectionServiceImpl extends AbstractConnectionService implements 
         super ( connection, autoReconnectController );
         this.connection = connection;
         this.itemManager = new ItemManager ( connection );
+        this.folderManager = new FolderManager ( connection );
     }
 
     @Override
     public org.openscada.da.client.Connection getConnection ()
     {
         return this.connection;
+    }
+
+    public FolderManager getFolderManager ()
+    {
+        return this.folderManager;
     }
 
     public ItemManager getItemManager ()
