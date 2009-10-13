90d8ef1e5a6270af500b6b8b8927e517272f1790
Ludwig Sickinger <ludwig.sickinger@inavare.net>
2009-10-13 16:07:47 +0000

    added query implementation
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis/trunk@5426 1b50e8ee-deed-0310-b1de-ab401e1c4d63

---
+++ org.openscada.hd.server.storage/src/org/openscada/hd/server/storage/internal/QueryImpl.java
@@ -0,0 +1,26 @@
+package org.openscada.hd.server.storage.internal;
+
+import org.openscada.hd.Query;
+import org.openscada.hd.QueryParameters;
+
+/**
+ * This is the internal implementation of the HD query interface.
+ * @see org.openscada.hd.Query
+ * @author Ludwig Straub
+ */
+public class QueryImpl implements Query
+{
+    /**
+     * @see org.openscada.hd.Query#changeParameters
+     */
+    public void changeParameters ( QueryParameters parameters )
+    {
+    }
+
+    /**
+     * @see org.openscada.hd.Query#close
+     */
+    public void close ()
+    {
+    }
+}
