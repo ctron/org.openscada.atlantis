59911d44d5a721db4817cf4b32dba51b8c03f69a
Ludwig Sickinger <ludwig.sickinger@inavare.net>
2009-10-13 16:15:39 +0000

    added query implementation
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis/trunk@5427 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- org.openscada.hd.server.storage/src/org/openscada/hd/server/storage/internal/QueryImpl.java
+++ org.openscada.hd.server.storage/src/org/openscada/hd/server/storage/internal/QueryImpl.java
@@ -1,7 +1,9 @@
 package org.openscada.hd.server.storage.internal;
 
 import org.openscada.hd.Query;
+import org.openscada.hd.QueryListener;
 import org.openscada.hd.QueryParameters;
+import org.openscada.hd.server.storage.ShiService;
 
 /**
  * This is the internal implementation of the HD query interface.
@@ -10,11 +12,39 @@ import org.openscada.hd.QueryParameters;
  */
 public class QueryImpl implements Query
 {
+    /** Service that created the query object. */
+    private final ShiService service;
+
+    /** Listener that should receive the data. */
+    private final QueryListener listener;
+
+    /** Input parameters of the query. */
+    private QueryParameters parameters;
+
+    /** Flag indicating whether the result should be periodically updated or not. */
+    private final boolean updateData;
+
+    /**
+     * Constructor.
+     * @param service service that created the query object
+     * @param listener listener that should receive the data
+     * @param parameters input parameters of the query
+     * @param updateData flag indicating whether the result should be periodically updated or not
+     */
+    public QueryImpl ( final ShiService service, final QueryListener listener, final QueryParameters parameters, final boolean updateData )
+    {
+        this.service = service;
+        this.listener = listener;
+        this.parameters = parameters;
+        this.updateData = updateData;
+    }
+
     /**
      * @see org.openscada.hd.Query#changeParameters
      */
     public void changeParameters ( QueryParameters parameters )
     {
+        this.parameters = parameters;
     }
 
     /**
