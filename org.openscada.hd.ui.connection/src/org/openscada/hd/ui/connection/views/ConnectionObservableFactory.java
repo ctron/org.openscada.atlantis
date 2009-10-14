fc4d8888757e751d52639f09bbdb9f225afa3c96
Jens Reimann <jens.reimann@th4-systems.com>
2009-10-14 09:32:21 +0000

    cleanup
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis/trunk@5438 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- org.openscada.hd.ui.connection/src/org/openscada/hd/ui/connection/views/ConnectionObservableFactory.java
+++ org.openscada.hd.ui.connection/src/org/openscada/hd/ui/connection/views/ConnectionObservableFactory.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package org.openscada.hd.ui.connection.views;
 
 import org.eclipse.core.databinding.beans.BeansObservables;
@@ -10,14 +7,9 @@ import org.eclipse.core.databinding.observable.masterdetail.IObservableFactory;
 import org.eclipse.core.databinding.observable.set.IObservableSet;
 import org.openscada.core.ui.connection.data.ConnectionDiscovererBean;
 import org.openscada.core.ui.connection.data.ConnectionHolder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 final class ConnectionObservableFactory implements IObservableFactory
 {
-
-    private final static Logger logger = LoggerFactory.getLogger ( ConnectionObservableFactory.class );
-
     public IObservable createObservable ( final Object target )
     {
         if ( target instanceof ConnectionHolder )
