ee5aa3bebcdfe3798b005b77aea2eb44b4dcd94a
Jens Reimann <jens.reimann@th4-systems.com>
2009-10-15 13:35:06 +0000

    set auto expand
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis/trunk@5512 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- org.openscada.core.ui.connection/src/org/openscada/core/ui/connection/views/ConnectionView.java
+++ org.openscada.core.ui.connection/src/org/openscada/core/ui/connection/views/ConnectionView.java
@@ -1,10 +1,22 @@
 package org.openscada.core.ui.connection.views;
 
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.navigator.CommonNavigator;
+import org.eclipse.ui.navigator.CommonViewer;
 import org.openscada.core.ui.connection.Activator;
 
 public class ConnectionView extends CommonNavigator
 {
+
+    @Override
+    protected CommonViewer createCommonViewerObject ( final Composite aParent )
+    {
+        // quick fix to automatically expand some content
+        final CommonViewer result = super.createCommonViewerObject ( aParent );
+        result.setAutoExpandLevel ( 2 );
+        return result;
+    }
+
     @Override
     protected Object getInitialInput ()
     {
