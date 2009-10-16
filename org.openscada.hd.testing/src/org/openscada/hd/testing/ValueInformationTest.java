90d439b2257f0b8c2c883add782e37b23baaf451
Jens Reimann <jens.reimann@th4-systems.com>
2009-10-16 07:24:52 +0000

    make a testing plugin
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis/trunk@5534 1b50e8ee-deed-0310-b1de-ab401e1c4d63

---
+++ org.openscada.hd.testing/src/org/openscada/hd/testing/ValueInformationTest.java
@@ -0,0 +1,44 @@
+package org.openscada.hd.testing;
+
+import java.util.Calendar;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openscada.hd.ValueInformation;
+
+public class ValueInformationTest
+{
+    @Test
+    public void equals1 ()
+    {
+        final Calendar start = Calendar.getInstance ();
+        start.set ( 2000, 1, 1 );
+        final Calendar end = (Calendar)start.clone ();
+        end.add ( Calendar.MINUTE, 1 );
+
+        final ValueInformation vi1 = new ValueInformation ( start, end, 1.0, 1 );
+        final ValueInformation vi2 = new ValueInformation ( start, end, 1.0, 1 );
+
+        Assert.assertEquals ( "Basic equality", vi1, vi2 );
+    }
+
+    /**
+     * Test if the calendar can be changed without changing the VI instance
+     */
+    @Test
+    public void equals2 ()
+    {
+        final Calendar start = Calendar.getInstance ();
+        start.set ( 2000, 1, 1 );
+        final Calendar end = (Calendar)start.clone ();
+        end.add ( Calendar.MINUTE, 1 );
+
+        final ValueInformation vi1 = new ValueInformation ( start, end, 1.0, 1 );
+
+        start.set ( 2001, 1, 1 );
+
+        final ValueInformation vi2 = new ValueInformation ( start, end, 1.0, 1 );
+
+        Assert.assertFalse ( "Should not be equal", vi1.equals ( vi2 ) );
+    }
+}
