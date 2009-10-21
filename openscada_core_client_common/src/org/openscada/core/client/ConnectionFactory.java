51ccf801e6b199cf897d7bcab2870edf6e160246
Jens Reimann <jens.reimann@th4-systems.com>
2009-10-21 07:20:28 +0000

    clear sun specific stuff
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis/trunk@5647 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- openscada_core_client_common/src/org/openscada/core/client/ConnectionFactory.java
+++ openscada_core_client_common/src/org/openscada/core/client/ConnectionFactory.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of the OpenSCADA project
- * Copyright (C) 2006-2007 inavare GmbH (http://inavare.com)
+ * Copyright (C) 2006-2009 inavare GmbH (http://inavare.com)
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -24,9 +24,6 @@ import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.openscada.core.ConnectionInformation;
-
-// FIXME: isn't exactly legal with a generic java runtime
-// import sun.misc.Service;
 
 public class ConnectionFactory
 {
@@ -60,27 +57,6 @@ public class ConnectionFactory
                 }
             }
         }
-
-        // FIXME: isn't exactly legal with a generic java runtime
-        // and is obviously not used so far
-
-        // now try using the service framework
-        //        try
-        //        {
-        //            final Iterator<?> i = Service.providers ( DriverFactory.class );
-        //            while ( i.hasNext () )
-        //            {
-        //                final DriverFactory factory = (DriverFactory)i.next ();
-        //                final DriverInformation di = factory.getDriverInformation ( connectionInformation );
-        //                if ( di != null )
-        //                {
-        //                    return di;
-        //                }
-        //            }
-        //        }
-        //        catch ( final Throwable e )
-        //        {
-        //        }
 
         return null;
     }
