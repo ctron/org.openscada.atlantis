223eb6950a977cd9871bcbdaa25a72d1f6e8968b
Jens Reimann <jens.reimann@th4-systems.com>
2009-10-21 08:12:00 +0000

    evaluate the "full" flag
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis/trunk@5655 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- org.openscada.da.ui.connection/src/org/openscada/da/ui/connection/views/FolderObserver.java
+++ org.openscada.da.ui.connection/src/org/openscada/da/ui/connection/views/FolderObserver.java
@@ -74,6 +74,7 @@ public abstract class FolderObserver extends WritableSet implements FolderListen
     {
         if ( isDisposed () )
         {
+            logger.debug ( "Folder already disposed" );
             return;
         }
 
@@ -81,6 +82,11 @@ public abstract class FolderObserver extends WritableSet implements FolderListen
 
         try
         {
+            if ( full )
+            {
+                clear ();
+            }
+
             if ( removed != null )
             {
                 for ( final String name : removed )
