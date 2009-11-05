3a67f822ac19469302c6e6c08de1392324bbb964
Jens Reimann <jens.reimann@th4-systems.com>
2009-11-05 14:39:18 +0000

    add master id constant
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis/trunk@5962 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- org.openscada.da.master/src/org/openscada/da/master/MasterItem.java
+++ org.openscada.da.master/src/org/openscada/da/master/MasterItem.java
@@ -2,9 +2,10 @@ package org.openscada.da.master;
 
 import org.openscada.da.datasource.DataSource;
 
-
 public interface MasterItem extends DataSource
 {
+
+    public static final String MASTER_ID = "master.id";
 
     /**
      * remove sub condition
