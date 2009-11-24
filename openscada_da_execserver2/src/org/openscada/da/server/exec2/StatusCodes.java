ba3a01b7b1d594b5863079f47ec29a6abc2342fd
Jens Reimann <jens.reimann@th4-systems.com>
2009-11-24 08:07:37 +0000

    Rename project to new naming scheme
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis/trunk@6175 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- openscada_da_execserver2/src/org/openscada/da/server/exec2/StatusCodes.java
+++
@@ -1,9 +0,0 @@
-package org.openscada.da.server.exec2;
-
-import org.openscada.utils.statuscodes.SeverityLevel;
-import org.openscada.utils.statuscodes.StatusCode;
-
-public class StatusCodes
-{
-    public final static StatusCode TRIGGER_RUNNING = new StatusCode ( "OSEX", "TRIGGER", 0x00001, SeverityLevel.ERROR );
-}
--- openscada_da_execserver2/src/org/openscada/da/server/exec2/command/ProcessListener.java
+++
@@ -1,8 +0,0 @@
-package org.openscada.da.server.exec2.command;
-
-public interface ProcessListener
-{
-    public void processCreated ( Process process );
-
-    public void processCompleted ();
-}
