2f76a1fdba4278131e91ce2b7737f828c8cb753d
Juergen Rose <juergen.rose@th4-systems.com>
2009-12-04 10:47:27 +0000

    finished implementation of EventStorage
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis/trunk@6369 1b50e8ee-deed-0310-b1de-ab401e1c4d63

---
+++ org.openscada.ae.server.storage.jdbc.test/src/org/openscada/ae/server/storage/jdbc/test/JdbcStorageBaseTest.java
@@ -0,0 +1,77 @@
+package org.openscada.ae.server.storage.jdbc.test;
+
+import java.util.GregorianCalendar;
+import java.util.Random;
+import java.util.UUID;
+
+import javax.sql.DataSource;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.openscada.ae.Event;
+import org.openscada.ae.Event.EventBuilder;
+import org.openscada.ae.server.storage.Storage;
+import org.openscada.ae.server.storage.jdbc.internal.Activator;
+import org.osgi.framework.BundleContext;
+import org.osgi.util.tracker.ServiceTracker;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class JdbcStorageBaseTest
+{
+    public static final String TABLE_EVENTS = "org_openscada_ae_server_storage_events";
+
+    public static final String TABLE_EVENT_ATTRIBS = "org_openscada_ae_server_storage_event_attribs";
+
+    public final static long SERVICE_TRACKER_TIMEOUT = 5000l;
+
+    public static BeanFactory appContext;
+
+    @BeforeClass
+    public static void start () throws Exception
+    {
+        BundleContext bundleContext = Activator.getBundleContext ();
+
+        // get spring application context
+        ServiceTracker stSpring = new ServiceTracker ( bundleContext, bundleContext.createFilter ( "(objectClass=org.springframework.context.ApplicationContext)" ), null );
+        stSpring.open ();
+        appContext = (BeanFactory)stSpring.waitForService ( SERVICE_TRACKER_TIMEOUT );
+        stSpring.close ();
+    }
+
+    @Before
+    public void prepareDatabase () throws Exception
+    {
+        getJdbcTemplate ().execute ( "DELETE FROM " + TABLE_EVENT_ATTRIBS );
+        getJdbcTemplate ().execute ( "DELETE FROM " + TABLE_EVENTS );
+    }
+
+    public JdbcTemplate getJdbcTemplate () throws Exception
+    {
+        return new JdbcTemplate ( (DataSource)appContext.getBean ( "dataSource" ) );
+    }
+
+    public Storage getStorage ()
+    {
+        return (Storage)appContext.getBean ( "jdbcStorage" );
+    }
+
+    public UUID makeUUID ( long seed ) throws Exception
+    {
+        Random rnd = new Random ( seed );
+        long l1 = rnd.nextLong ();
+        long l2 = rnd.nextLong ();
+        return UUID.nameUUIDFromBytes ( ( Long.toBinaryString ( l1 ) + Long.toBinaryString ( l2 ) ).getBytes ( "ASCII" ) );
+    }
+
+    public Event makeEvent ( int nr ) throws Exception
+    {
+        EventBuilder eb = Event.create ();
+        eb.sourceTimestamp ( new GregorianCalendar ().getTime () );
+        eb.attribute ( Event.Fields.SOURCE.getName (), "TEST" );
+        eb.attribute ( Event.Fields.PRIORITY.getName (), 5 );
+        eb.attribute ( Event.Fields.TYPE.getName (), "TEST" );
+        eb.attribute ( "nr", nr );
+        return eb.build ();
+    }
+}
--- org.openscada.ae.server.storage.jdbc.test/src/org/openscada/ae/server/storage/jdbc/test/JdbcStorageDAOTest.java
+++ org.openscada.ae.server.storage.jdbc.test/src/org/openscada/ae/server/storage/jdbc/test/JdbcStorageDAOTest.java
@@ -6,15 +6,9 @@ import static org.junit.Assert.assertNotNull;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
 import java.util.UUID;
 
-import javax.sql.DataSource;
-
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.openscada.ae.server.storage.jdbc.internal.Activator;
 import org.openscada.ae.server.storage.jdbc.internal.FilterUtils;
 import org.openscada.ae.server.storage.jdbc.internal.HqlConverter;
 import org.openscada.ae.server.storage.jdbc.internal.JdbcStorageDAO;
@@ -27,49 +21,13 @@ import org.openscada.utils.filter.FilterAssertion;
 import org.openscada.utils.filter.FilterExpression;
 import org.openscada.utils.filter.FilterParser;
 import org.openscada.utils.filter.Operator;
-import org.osgi.framework.BundleContext;
-import org.osgi.util.tracker.ServiceTracker;
-import org.springframework.jdbc.core.JdbcTemplate;
 
-public class JdbcStorageDAOTest
+public class JdbcStorageDAOTest extends JdbcStorageBaseTest
 {
-    private static final String TABLE_EVENTS = "org_openscada_ae_server_storage_events";
 
-    private static final String TABLE_EVENT_ATTRIBS = "org_openscada_ae_server_storage_event_attribs";
-
-    private final static long SERVICE_TRACKER_TIMEOUT = 5000l;
-
-    private static JdbcStorageDAO jdbcStorageDAO;
-
-    private static DataSource dataSource;
-
-    private static JdbcTemplate jdbcTemplate;
-
-    @BeforeClass
-    public static void start () throws Exception
+    public JdbcStorageDAO getJdbcStorageDAO () throws Exception
     {
-        BundleContext bundleContext = Activator.getBundleContext ();
-        ServiceTracker stJdbcStorageDAO = new ServiceTracker ( bundleContext, bundleContext.createFilter ( "(&(objectClass=org.openscada.ae.server.storage.jdbc.internal.JdbcStorageDAO)(type=org.openscada.ae.server.storage.jdbc))" ), null );
-        stJdbcStorageDAO.open ();
-        ServiceTracker stDataSource = new ServiceTracker ( bundleContext, bundleContext.createFilter ( "(&(objectClass=javax.sql.DataSource)(type=org.openscada.ae.server.storage.jdbc))" ), null );
-        stDataSource.open ();
-        jdbcStorageDAO = (JdbcStorageDAO)stJdbcStorageDAO.waitForService ( SERVICE_TRACKER_TIMEOUT );
-        dataSource = (DataSource)stDataSource.waitForService ( SERVICE_TRACKER_TIMEOUT );
-        jdbcTemplate = new JdbcTemplate ( dataSource );
-
-        assertNotNull ( jdbcStorageDAO );
-        assertNotNull ( dataSource );
-        assertNotNull ( jdbcTemplate );
-
-        stJdbcStorageDAO.close ();
-        stDataSource.close ();
-    }
-
-    @Before
-    public void prepareDatabase () throws Exception
-    {
-        jdbcTemplate.execute ( "DELETE FROM " + TABLE_EVENT_ATTRIBS );
-        jdbcTemplate.execute ( "DELETE FROM " + TABLE_EVENTS );
+        return (JdbcStorageDAO)appContext.getBean ( "jdbcStorageDAO" );
     }
 
     @Test
@@ -78,24 +36,24 @@ public class JdbcStorageDAOTest
         MutableEvent event;
 
         // store event without extended attributes
-        event = makeEvent ( 1 );
-        jdbcStorageDAO.storeEvent ( event );
+        event = makeMutableEvent ( 1 );
+        getJdbcStorageDAO ().storeEvent ( event );
         List result;
-        result = jdbcTemplate.queryForList ( "SELECT * FROM " + TABLE_EVENTS );
+        result = getJdbcTemplate ().queryForList ( "SELECT * FROM " + TABLE_EVENTS );
         assertNotNull ( result.get ( 0 ) );
         Map<String, Object> row = (Map<String, Object>)result.get ( 0 );
         UUID retrievedId = UUID.fromString ( (String)row.get ( "ID" ) );
         assertEquals ( makeUUID ( 1 ), retrievedId );
-        int numOfAttribRows = jdbcTemplate.queryForInt ( "SELECT count(*) FROM " + TABLE_EVENT_ATTRIBS );
+        int numOfAttribRows = getJdbcTemplate ().queryForInt ( "SELECT count(*) FROM " + TABLE_EVENT_ATTRIBS );
         assertEquals ( 0, numOfAttribRows );
 
         // store event with additional attributes
-        event = makeEvent ( 2 );
+        event = makeMutableEvent ( 2 );
         event.getAttributes ().put ( "prop", new Variant ( "hello" ) );
-        jdbcStorageDAO.storeEvent ( event );
-        numOfAttribRows = jdbcTemplate.queryForInt ( "SELECT count(*) FROM " + TABLE_EVENTS );
+        getJdbcStorageDAO ().storeEvent ( event );
+        numOfAttribRows = getJdbcTemplate ().queryForInt ( "SELECT count(*) FROM " + TABLE_EVENTS );
         assertEquals ( 2, numOfAttribRows );
-        numOfAttribRows = jdbcTemplate.queryForInt ( "SELECT count(*) FROM " + TABLE_EVENT_ATTRIBS );
+        numOfAttribRows = getJdbcTemplate ().queryForInt ( "SELECT count(*) FROM " + TABLE_EVENT_ATTRIBS );
         assertEquals ( 1, numOfAttribRows );
     }
 
@@ -106,16 +64,16 @@ public class JdbcStorageDAOTest
         MutableEvent resultEvent;
 
         // store event without extended properties
-        event = makeEvent ( 1 );
-        jdbcStorageDAO.storeEvent ( event );
-        resultEvent = jdbcStorageDAO.loadEvent ( makeUUID ( 1 ) );
+        event = makeMutableEvent ( 1 );
+        getJdbcStorageDAO ().storeEvent ( event );
+        resultEvent = getJdbcStorageDAO ().loadEvent ( makeUUID ( 1 ) );
         assertNotNull ( resultEvent );
 
         // store event with extended properties
-        event = makeEvent ( 2 );
+        event = makeMutableEvent ( 2 );
         event.getAttributes ().put ( "foo", new Variant ( "bar" ) );
-        jdbcStorageDAO.storeEvent ( event );
-        resultEvent = jdbcStorageDAO.loadEvent ( makeUUID ( 2 ) );
+        getJdbcStorageDAO ().storeEvent ( event );
+        resultEvent = getJdbcStorageDAO ().loadEvent ( makeUUID ( 2 ) );
         assertNotNull ( resultEvent );
         assertEquals ( new Variant ( "bar" ), resultEvent.getAttributes ().get ( "foo" ) );
     }
@@ -127,10 +85,10 @@ public class JdbcStorageDAOTest
         List<MutableEvent> resultEvents;
 
         // store event with extended properties
-        event = makeEvent ( 1 );
+        event = makeMutableEvent ( 1 );
         event.getAttributes ().put ( "foo", new Variant ( "bar" ) );
-        jdbcStorageDAO.storeEvent ( event );
-        resultEvents = jdbcStorageDAO.queryEvent ( "FROM MutableEvent e WHERE e.id = ?", makeUUID ( 1 ) );
+        getJdbcStorageDAO ().storeEvent ( event );
+        resultEvents = getJdbcStorageDAO ().queryEvent ( "FROM MutableEvent e WHERE e.id = ?", makeUUID ( 1 ) );
         assertNotNull ( resultEvents );
         assertEquals ( 1, resultEvents.size () );
         assertEquals ( makeUUID ( 1 ), resultEvents.get ( 0 ).getId () );
@@ -140,43 +98,43 @@ public class JdbcStorageDAOTest
         filter = new FilterAssertion ( "foo", Assertion.EQUALITY, new Variant ( "bar" ) );
         HqlResult hqlFilter;
         hqlFilter = HqlConverter.toHql ( filter );
-        resultEvents = jdbcStorageDAO.queryEvent ( hqlFilter.getHql (), hqlFilter.getParameters () );
+        resultEvents = getJdbcStorageDAO ().queryEvent ( hqlFilter.getHql (), hqlFilter.getParameters () );
         // System.out.println (hqlFilter.getHql ());
         // String hql = "SELECT M from MutableEvent M left join fetch M.attributes as A WHERE index(A) = 'foo' AND A['foo'] = ?";
-        //resultEvents = jdbcStorageDAO.queryEvent ( hql,  new Variant("bar"));
+        //resultEvents = getJdbcStorageDAO().queryEvent ( hql,  new Variant("bar"));
         assertNotNull ( resultEvents );
         assertEquals ( 1, resultEvents.size () );
         assertEquals ( makeUUID ( 1 ), resultEvents.get ( 0 ).getId () );
         assertEquals ( new Variant ( "bar" ), resultEvents.get ( 0 ).getAttributes ().get ( "foo" ) );
 
-        event = makeEvent ( 2 );
+        event = makeMutableEvent ( 2 );
         event.getAttributes ().put ( "spam", new Variant ( true ) );
-        jdbcStorageDAO.storeEvent ( event );
+        getJdbcStorageDAO ().storeEvent ( event );
         filter = new FilterAssertion ( "spam", Assertion.EQUALITY, new Variant ( true ) );
         hqlFilter = HqlConverter.toHql ( filter );
-        resultEvents = jdbcStorageDAO.queryEvent ( hqlFilter.getHql (), hqlFilter.getParameters () );
+        resultEvents = getJdbcStorageDAO ().queryEvent ( hqlFilter.getHql (), hqlFilter.getParameters () );
         assertNotNull ( resultEvents );
         assertEquals ( 1, resultEvents.size () );
         assertEquals ( makeUUID ( 2 ), resultEvents.get ( 0 ).getId () );
         assertEquals ( new Variant ( true ), resultEvents.get ( 0 ).getAttributes ().get ( "spam" ) );
 
-        event = makeEvent ( 3 );
+        event = makeMutableEvent ( 3 );
         event.getAttributes ().put ( "bacon", new Variant ( 3.141 ) );
-        jdbcStorageDAO.storeEvent ( event );
+        getJdbcStorageDAO ().storeEvent ( event );
         filter = new FilterAssertion ( "bacon", Assertion.EQUALITY, new Variant ( 3.141 ) );
         hqlFilter = HqlConverter.toHql ( filter );
-        resultEvents = jdbcStorageDAO.queryEvent ( hqlFilter.getHql (), hqlFilter.getParameters () );
+        resultEvents = getJdbcStorageDAO ().queryEvent ( hqlFilter.getHql (), hqlFilter.getParameters () );
         assertNotNull ( resultEvents );
         assertEquals ( 1, resultEvents.size () );
         assertEquals ( makeUUID ( 3 ), resultEvents.get ( 0 ).getId () );
         assertEquals ( new Variant ( 3.141 ), resultEvents.get ( 0 ).getAttributes ().get ( "bacon" ) );
 
-        event = makeEvent ( 4 );
+        event = makeMutableEvent ( 4 );
         event.getAttributes ().put ( "eggs", new Variant ( 42L ) );
-        jdbcStorageDAO.storeEvent ( event );
+        getJdbcStorageDAO ().storeEvent ( event );
         filter = new FilterAssertion ( "eggs", Assertion.EQUALITY, new Variant ( 42L ) );
         hqlFilter = HqlConverter.toHql ( filter );
-        resultEvents = jdbcStorageDAO.queryEvent ( hqlFilter.getHql (), hqlFilter.getParameters () );
+        resultEvents = getJdbcStorageDAO ().queryEvent ( hqlFilter.getHql (), hqlFilter.getParameters () );
         assertNotNull ( resultEvents );
         assertEquals ( 1, resultEvents.size () );
         assertEquals ( makeUUID ( 4 ), resultEvents.get ( 0 ).getId () );
@@ -187,47 +145,39 @@ public class JdbcStorageDAOTest
         ( (FilterExpression)filter ).getFilterSet ().add ( new FilterAssertion ( "spam", Assertion.EQUALITY, new Variant ( true ) ) );
         ( (FilterExpression)filter ).getFilterSet ().add ( new FilterAssertion ( "eggs", Assertion.EQUALITY, new Variant ( 42L ) ) );
         hqlFilter = HqlConverter.toHql ( filter );
-        resultEvents = jdbcStorageDAO.queryEvent ( hqlFilter.getHql (), hqlFilter.getParameters () );
+        resultEvents = getJdbcStorageDAO ().queryEvent ( hqlFilter.getHql (), hqlFilter.getParameters () );
         assertNotNull ( resultEvents );
         assertEquals ( 2, resultEvents.size () );
 
-        event = makeEvent ( 5 );
+        event = makeMutableEvent ( 5 );
         event.getAttributes ().put ( "like", new Variant ( "this is a string" ) );
-        jdbcStorageDAO.storeEvent ( event );
+        getJdbcStorageDAO ().storeEvent ( event );
         filter = new FilterParser ( "(like=*is*)" ).getFilter ();
         FilterUtils.toVariant ( filter );
         hqlFilter = HqlConverter.toHql ( filter );
-        resultEvents = jdbcStorageDAO.queryEvent ( hqlFilter.getHql (), hqlFilter.getParameters () );
+        resultEvents = getJdbcStorageDAO ().queryEvent ( hqlFilter.getHql (), hqlFilter.getParameters () );
         assertNotNull ( resultEvents );
         assertEquals ( 1, resultEvents.size () );
 
-        event = makeEvent ( 6 );
+        event = makeMutableEvent ( 6 );
         event.getAttributes ().put ( "like", new Variant ( "john" ) );
-        jdbcStorageDAO.storeEvent ( event );
+        getJdbcStorageDAO ().storeEvent ( event );
         filter = new FilterParser ( "(like~=jon)" ).getFilter ();
         FilterUtils.toVariant ( filter );
         hqlFilter = HqlConverter.toHql ( filter );
-        resultEvents = jdbcStorageDAO.queryEvent ( hqlFilter.getHql (), hqlFilter.getParameters () );
+        resultEvents = getJdbcStorageDAO ().queryEvent ( hqlFilter.getHql (), hqlFilter.getParameters () );
         assertNotNull ( resultEvents );
         assertEquals ( 1, resultEvents.size () );
 
         filter = new FilterParser ( "(like=*)" ).getFilter ();
         FilterUtils.toVariant ( filter );
         hqlFilter = HqlConverter.toHql ( filter );
-        resultEvents = jdbcStorageDAO.queryEvent ( hqlFilter.getHql (), hqlFilter.getParameters () );
+        resultEvents = getJdbcStorageDAO ().queryEvent ( hqlFilter.getHql (), hqlFilter.getParameters () );
         assertNotNull ( resultEvents );
         assertEquals ( 2, resultEvents.size () );
     }
 
-    private UUID makeUUID ( long seed ) throws Exception
-    {
-        Random rnd = new Random ( seed );
-        long l1 = rnd.nextLong ();
-        long l2 = rnd.nextLong ();
-        return UUID.nameUUIDFromBytes ( ( Long.toBinaryString ( l1 ) + Long.toBinaryString ( l2 ) ).getBytes ( "ASCII" ) );
-    }
-
-    private MutableEvent makeEvent ( long id ) throws Exception
+    private MutableEvent makeMutableEvent ( long id ) throws Exception
     {
         Date timestamp = new Date ( System.currentTimeMillis () );
         MutableEvent event = new MutableEvent ();
