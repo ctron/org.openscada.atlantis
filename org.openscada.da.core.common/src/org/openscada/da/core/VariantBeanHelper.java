e7daafd2ab13c041d7e6bf4e31c735bccedf0329
Jens Reimann <jens.reimann@th4-systems.com>
2010-01-26 11:17:17 +0000

    have a boolean converter and fix the variant bean helper
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis/trunk@6626 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- org.openscada.da.core.common/src/org/openscada/da/core/VariantBeanHelper.java
+++ org.openscada.da.core.common/src/org/openscada/da/core/VariantBeanHelper.java
@@ -28,11 +28,52 @@ import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.beanutils.ConvertUtilsBean2;
+import org.apache.commons.beanutils.Converter;
+import org.apache.commons.beanutils.converters.AbstractConverter;
 import org.openscada.core.OperationException;
 import org.openscada.core.Variant;
 
 public class VariantBeanHelper
 {
+
+    private static final ConvertUtilsBean2 converter = new ConvertUtilsBean2 ();
+
+    static
+    {
+        final Converter cvt = new AbstractConverter () {
+            @SuppressWarnings ( "unchecked" )
+            @Override
+            protected Object convertToType ( final Class type, final Object value ) throws Throwable
+            {
+                if ( value == null )
+                {
+                    return null;
+                }
+                if ( value instanceof Boolean )
+                {
+                    return value;
+                }
+
+                if ( value instanceof Number )
+                {
+                    return ( (Number)value ).intValue () != 0;
+                }
+
+                return Boolean.parseBoolean ( value.toString () );
+            }
+
+            @SuppressWarnings ( "unchecked" )
+            @Override
+            protected Class getDefaultType ()
+            {
+                return Boolean.class;
+            }
+        };
+        converter.register ( cvt, boolean.class );
+        converter.register ( cvt, Boolean.class );
+    }
+
     /**
      * Extract the property data as string/variant map
      * @param source the source object
@@ -87,6 +128,7 @@ public class VariantBeanHelper
 
     public static void applyValue ( final Object target, final PropertyDescriptor pd, final Variant value ) throws OperationException, IllegalArgumentException, IllegalAccessException, InvocationTargetException
     {
+        // ensure for the following calls that the PD has a write method
         final Method m = pd.getWriteMethod ();
         if ( m == null )
         {
@@ -98,7 +140,35 @@ public class VariantBeanHelper
         {
             // direct set using variant type
             m.invoke ( target, value );
+            return;
         }
+
+        // now we need to convert
+        if ( !applyValueAsObject ( target, pd, value.getValue () ) )
+        {
+            throw new OperationException ( String.format ( "There is no way to convert '%s' to '%s'", value, targetType ) );
+        }
+    }
+
+    private static boolean applyValueAsObject ( final Object target, final PropertyDescriptor pd, final Object value ) throws IllegalAccessException, InvocationTargetException, OperationException
+    {
+        if ( value == null )
+        {
+            pd.getWriteMethod ().invoke ( target, new Object[] { null } );
+            return true;
+        }
+
+        final Object targetType = converter.convert ( value, pd.getPropertyType () );
+
+        if ( targetType == null )
+        {
+            throw new OperationException ( String.format ( "Unable to convert '%s' to '%s'", value.getClass (), pd.getPropertyType () ) );
+        }
+
+        System.out.println ( "Converting " + value.getClass () + " to " + targetType.getClass () + " for " + pd.getPropertyType () );
+
+        pd.getWriteMethod ().invoke ( target, targetType );
+        return true;
     }
 
     private static PropertyDescriptor findDescriptor ( final BeanInfo bi, final String propertyName )
