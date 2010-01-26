55cf82880588cfb045439567e8e12be5e568a827
Jens Reimann <jens.reimann@th4-systems.com>
2010-01-26 09:29:31 +0000

    use "OK" constant
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis/trunk@6618 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- openscada_da_client_ice/src/org/openscada/da/client/ice/ConnectionHelper.java
+++ openscada_da_client_ice/src/org/openscada/da/client/ice/ConnectionHelper.java
@@ -9,22 +9,22 @@ import OpenSCADA.DA.WriteAttributesResultEntry;
 public class ConnectionHelper
 {
     private static Logger _log = Logger.getLogger ( ConnectionHelper.class );
-    
-    public static WriteAttributeResults fromIce ( WriteAttributesResultEntry [] entries )
+
+    public static WriteAttributeResults fromIce ( final WriteAttributesResultEntry[] entries )
     {
-        WriteAttributeResults ret = new WriteAttributeResults ();
-        
-        for ( WriteAttributesResultEntry entry : entries )
+        final WriteAttributeResults ret = new WriteAttributeResults ();
+
+        for ( final WriteAttributesResultEntry entry : entries )
         {
             _log.debug ( String.format ( "Attribute result '%s': '%s'", entry.item, entry.result ) );
-            
+
             if ( entry.result != null && entry.result.length () > 0 )
             {
                 ret.put ( entry.item, new WriteAttributeResult ( new Exception ( entry.result ) ) );
             }
             else
             {
-                ret.put ( entry.item, new WriteAttributeResult () );
+                ret.put ( entry.item, WriteAttributeResult.OK );
             }
         }
         return ret;
