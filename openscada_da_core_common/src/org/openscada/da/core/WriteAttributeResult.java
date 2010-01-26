a02009a80c70052d11f8e04bb5bab97265e9e1c7
Jens Reimann <jens.reimann@th4-systems.com>
2010-01-26 09:22:02 +0000

    add static "OK"
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis/trunk@6617 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- openscada_da_core_common/src/org/openscada/da/core/WriteAttributeResult.java
+++ openscada_da_core_common/src/org/openscada/da/core/WriteAttributeResult.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of the OpenSCADA project
- * Copyright (C) 2006-2009 inavare GmbH (http://inavare.com)
+ * Copyright (C) 2006-2010 inavare GmbH (http://inavare.com)
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -25,6 +25,8 @@ import org.openscada.utils.lang.Immutable;
 public class WriteAttributeResult
 {
     private Throwable error = null;
+
+    public static final WriteAttributeResult OK = new WriteAttributeResult ();
 
     /**
      * Create an "OK" result
