6024290c3ff99e686c73d1d9d65a331ac453cb10
Jens Reimann <jens.reimann@th4-systems.com>
2010-02-05 10:03:52 +0000

    bump version
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis/trunk@6803 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- openscada_da_client_net/src/org/openscada/da/client/net/Connection.java
+++ openscada_da_client_net/src/org/openscada/da/client/net/Connection.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of the OpenSCADA project
- * Copyright (C) 2006-2009 inavare GmbH (http://inavare.com)
+ * Copyright (C) 2006-2010 inavare GmbH (http://inavare.com)
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
