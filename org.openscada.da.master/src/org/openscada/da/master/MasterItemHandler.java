78315db3e35a1b522235c19c7ae4553a5a525b28
Jens Reimann <jens.reimann@th4-systems.com>
2010-02-08 10:05:04 +0000

    add doc
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis/trunk@6851 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- org.openscada.da.master/src/org/openscada/da/master/MasterItemHandler.java
+++ org.openscada.da.master/src/org/openscada/da/master/MasterItemHandler.java
@@ -6,5 +6,20 @@ public interface MasterItemHandler
 {
     public abstract DataItemValue dataUpdate ( DataItemValue value );
 
+    /**
+     * Handle a write request
+     * <p>
+     * This method is called when a master item received a write request.
+     * It will then pass on the request to all MasterItemHandler in order
+     * to process or alter the write request. Each handler returns a result
+     * and then the next handler will received that altered write request.
+     * </p>
+     * <p>
+     * If null is returned instead of a new write result, original write
+     * request is used and it is considered that the handler has done nothing.
+     * </p>
+     * @param request the write request to handle
+     * @return the resulting write request
+     */
     public abstract WriteRequestResult processWrite ( WriteRequest request );
 }
