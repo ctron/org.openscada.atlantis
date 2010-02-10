7d6cddcf91cee3fb7cbffdf040d33d84a37dad1c
Jens Reimann <jens.reimann@th4-systems.com>
2010-02-10 11:02:23 +0000

    make copy for org.openscada.ca.connection.provider
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis/trunk@6920 1b50e8ee-deed-0310-b1de-ab401e1c4d63

---
+++ org.openscada.ca.connection.provider/src/org/openscada/ae/connection/provider/Activator.java
@@ -0,0 +1,25 @@
+package org.openscada.ae.connection.provider;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator
+{
+
+    /*
+     * (non-Javadoc)
+     * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+     */
+    public void start ( final BundleContext context ) throws Exception
+    {
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+     */
+    public void stop ( final BundleContext context ) throws Exception
+    {
+    }
+
+}
