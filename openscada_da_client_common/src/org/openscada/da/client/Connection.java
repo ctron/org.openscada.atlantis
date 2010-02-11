1f1cbb4c24d4d4ed60278474bef6292ccfa106fb
Jens Reimann <jens.reimann@th4-systems.com>
2010-02-11 16:34:27 +0000

    remove setExecutor
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis/trunk@6945 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- openscada_da_client_common/src/org/openscada/da/client/Connection.java
+++ openscada_da_client_common/src/org/openscada/da/client/Connection.java
@@ -135,15 +135,6 @@ public interface Connection extends org.openscada.core.client.Connection
     public abstract ItemUpdateListener setItemUpdateListener ( String itemId, ItemUpdateListener listener );
 
     /**
-     * Clients may set an executor which the implementation must use for sending any event on registered
-     * listeners.
-     * <p>
-     * The default executor is unspecified.
-     * @param executor the executor to use
-     */
-    public abstract void setExecutor ( Executor executor );
-
-    /**
      * Retrieve the executor that is currently set
      * @return the currently used executor. Implementations must never return <code>null</code>
      */
