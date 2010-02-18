7f3a486987d6bf083463ac5ef1bc492e28600fcf
Jens Reimann <jens.reimann@th4-systems.com>
2010-02-18 17:41:24 +0000

    reduce initial memory footprint
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis/trunk@7054 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- org.openscada.da.server.common/src/org/openscada/da/server/browser/common/query/ItemDescriptor.java
+++ org.openscada.da.server.common/src/org/openscada/da/server/browser/common/query/ItemDescriptor.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of the OpenSCADA project
- * Copyright (C) 2006-2009 inavare GmbH (http://inavare.com)
+ * Copyright (C) 2006-2010 inavare GmbH (http://inavare.com)
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -27,7 +27,7 @@ import org.openscada.da.server.common.DataItem;
 
 public class ItemDescriptor
 {
-    private Map<String, Variant> attributes = new HashMap<String, Variant> ();
+    private Map<String, Variant> attributes = new HashMap<String, Variant> ( 0 );
 
     private DataItem item = null;
 
