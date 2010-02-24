19f8f8da7855c1fec97a1498c57daf153b39c93e
Jens Reimann <jens.reimann@th4-systems.com>
2010-02-24 17:26:47 +0000

    add license stuff
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis/trunk@7135 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- org.openscada.ae/src/org/openscada/ae/ConditionStatusInformation.java
+++ org.openscada.ae/src/org/openscada/ae/ConditionStatusInformation.java
@@ -1,3 +1,22 @@
+/*
+ * This file is part of the OpenSCADA project
+ * Copyright (C) 2008-2010 inavare GmbH (http://inavare.com)
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 package org.openscada.ae;
 
 import java.util.Date;
@@ -66,7 +85,7 @@ public class ConditionStatusInformation
     {
         final int prime = 31;
         int result = 1;
-        result = prime * result + ( ( this.id == null ) ? 0 : this.id.hashCode () );
+        result = prime * result + ( this.id == null ? 0 : this.id.hashCode () );
         return result;
     }
 
@@ -85,7 +104,7 @@ public class ConditionStatusInformation
         {
             return false;
         }
-        ConditionStatusInformation other = (ConditionStatusInformation)obj;
+        final ConditionStatusInformation other = (ConditionStatusInformation)obj;
         if ( this.id == null )
         {
             if ( other.id != null )
@@ -103,7 +122,7 @@ public class ConditionStatusInformation
     @Override
     public String toString ()
     {
-        StringBuilder sb = new StringBuilder ();
+        final StringBuilder sb = new StringBuilder ();
 
         sb.append ( this.id + "(" );
         sb.append ( "status=" + this.status );
