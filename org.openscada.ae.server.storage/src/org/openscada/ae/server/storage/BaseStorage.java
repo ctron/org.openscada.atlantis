2ef358cdcaf0d691210602a4077aeb9337cd59c4
Jens Reimann <jens.reimann@th4-systems.com>
2010-03-04 13:57:52 +0000

    allow proving the entry timestamp
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis/trunk@7296 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- org.openscada.ae.server.storage/src/org/openscada/ae/server/storage/BaseStorage.java
+++ org.openscada.ae.server.storage/src/org/openscada/ae/server/storage/BaseStorage.java
@@ -4,9 +4,12 @@ import java.util.GregorianCalendar;
 import java.util.UUID;
 
 import org.openscada.ae.Event;
+import org.openscada.ae.Event.EventBuilder;
 
 public abstract class BaseStorage implements Storage
 {
+    private static final boolean allowEntryTimestamp = Boolean.getBoolean ( "org.openscada.ae.server.storage.allowExternalEntryTimestamp" );
+
     public Event store ( final Event event )
     {
         return store ( event, null );
@@ -14,7 +17,16 @@ public abstract class BaseStorage implements Storage
 
     protected Event createEvent ( final Event event )
     {
-        return Event.create ().event ( event ).id ( UUID.randomUUID () ).entryTimestamp ( new GregorianCalendar ().getTime () ).build ();
+        final EventBuilder builder = Event.create ().event ( event ).id ( UUID.randomUUID () );
+
+        if ( !allowEntryTimestamp || event.getEntryTimestamp () == null )
+        {
+            // if we are not allowed to have prefilled entryTimestamps
+            // or a missing the timestamp anyway
+            builder.entryTimestamp ( new GregorianCalendar ().getTime () );
+        }
+
+        return builder.build ();
     }
 
     public Event update ( final UUID id, final String comment ) throws Exception
