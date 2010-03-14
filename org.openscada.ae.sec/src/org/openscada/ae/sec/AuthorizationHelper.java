f3ea49390f0d952d3f2dc376c68f736b1d48b5a6
Jens Reimann <jens.reimann@th4-systems.com>
2010-03-14 18:17:09 +0000

    implement
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis/trunk@7425 1b50e8ee-deed-0310-b1de-ab401e1c4d63

---
+++ org.openscada.ae.sec/src/org/openscada/ae/sec/AuthorizationHelper.java
@@ -0,0 +1,73 @@
+package org.openscada.ae.sec;
+
+import java.util.Date;
+import java.util.Map;
+
+import org.openscada.ae.Event;
+import org.openscada.ae.Event.EventBuilder;
+import org.openscada.ae.Event.Fields;
+import org.openscada.ae.event.EventProcessor;
+import org.openscada.sec.AuthorizationResult;
+import org.openscada.sec.UserInformation;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.InvalidSyntaxException;
+
+public class AuthorizationHelper extends org.openscada.sec.osgi.AuthorizationHelper
+{
+
+    private final boolean logAll;
+
+    private final EventProcessor eventProcessor;
+
+    public AuthorizationHelper ( final BundleContext context, final boolean logAll ) throws InvalidSyntaxException
+    {
+        super ( context );
+        this.eventProcessor = new EventProcessor ( context );
+        this.logAll = logAll;
+    }
+
+    public void start ()
+    {
+        this.eventProcessor.open ();
+    }
+
+    public void stop ()
+    {
+        this.eventProcessor.close ();
+    }
+
+    @Override
+    public AuthorizationResult authorize ( final String objectId, final String objectType, final String action, final UserInformation userInformation, final Map<String, Object> context, final AuthorizationResult defaultResult )
+    {
+        final AuthorizationResult result = super.authorize ( objectId, objectType, action, userInformation, context, defaultResult );
+
+        if ( result != null && !result.isGranted () )
+        {
+            this.eventProcessor.publishEvent ( makeEvent ( objectId, objectType, action, userInformation, result ) );
+        }
+
+        return result;
+    }
+
+    private Event makeEvent ( final String objectId, final String objectType, final String action, final UserInformation userInformation, final AuthorizationResult result )
+    {
+        final EventBuilder builder = Event.create ();
+
+        builder.sourceTimestamp ( new Date () );
+        if ( userInformation != null )
+        {
+            builder.attribute ( Fields.ACTOR_NAME, userInformation.getName () );
+            builder.attribute ( Fields.ACTOR_TYPE, "USER" );
+        }
+        builder.attribute ( Fields.EVENT_TYPE, "DENY" );
+        builder.attribute ( Fields.MONITOR_TYPE, "SEC" );
+        builder.attribute ( Fields.ITEM, objectId );
+        builder.attribute ( Fields.MESSAGE, String.format ( "%s: %s", result.getErrorCode (), result.getMessage () ) );
+        builder.attribute ( Fields.SOURCE, objectId );
+        builder.attribute ( "SOURCE_TYPE", objectType );
+        builder.attribute ( Fields.VALUE, action );
+        builder.attribute ( "CODE", result.getErrorCode () );
+
+        return builder.build ();
+    }
+}
