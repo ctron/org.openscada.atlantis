c8edc27f0bd89835e571a90cb5f839034c6c1e76
Jens Reimann <jens.reimann@th4-systems.com>
2010-04-12 09:28:57 +0000

    change license to LGPLv3
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis/trunk@7661 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- org.openscada.da.server.exec/src/org/openscada/da/server/exec/splitter/RegExSplitSplitter.java
+++ org.openscada.da.server.exec/src/org/openscada/da/server/exec/splitter/RegExSplitSplitter.java
@@ -2,19 +2,19 @@
  * This file is part of the OpenSCADA project
  * Copyright (C) 2006-2010 inavare GmbH (http://inavare.com)
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
-
- * This program is distributed in the hope that it will be useful,
+ * OpenSCADA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenSCADA is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
-
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenSCADA. If not, see
+ * <http://opensource.org/licenses/lgpl-3.0.html> for a copy of the LGPLv3 License.
  */
 
 package org.openscada.da.server.exec.splitter;
@@ -35,13 +35,13 @@ public class RegExSplitSplitter implements Splitter
 
     public SplitResult split ( final String inputBuffer )
     {
-        SplitResult result = new SplitResult ();
+        final SplitResult result = new SplitResult ();
 
-        List<String> list = Arrays.asList ( this.pattern.split ( inputBuffer ) );
+        final List<String> list = Arrays.asList ( this.pattern.split ( inputBuffer ) );
 
         if ( list.size () >= 2 )
         {
-            String last = list.remove ( list.size () );
+            final String last = list.remove ( list.size () );
             result.setLines ( list.toArray ( new String[0] ) );
             result.setRemainingBuffer ( last );
         }
