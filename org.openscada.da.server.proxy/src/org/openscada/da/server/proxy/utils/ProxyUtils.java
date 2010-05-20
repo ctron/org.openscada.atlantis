c830bdb4a45b36434c4234955794293830672518
Jens Reimann <jens.reimann@th4-systems.com>
2010-05-20 15:00:39 +0000

    switch to LGPLv3
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis/trunk@8019 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- org.openscada.da.server.proxy/src/org/openscada/da/server/proxy/utils/ProxyUtils.java
+++ org.openscada.da.server.proxy/src/org/openscada/da/server/proxy/utils/ProxyUtils.java
@@ -1,20 +1,20 @@
 /*
  * This file is part of the OpenSCADA project
- * Copyright (C) 2006-2009 inavare GmbH (http://inavare.com)
+ * Copyright (C) 2006-2010 inavare GmbH (http://inavare.com)
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
-
- * This program is distributed in the hope that it will be useful,
+ * OpenSCADA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenSCADA is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
-
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenSCADA. If not, see
+ * <http://opensource.org/licenses/lgpl-3.0.html> for a copy of the LGPLv3 License.
  */
 
 package org.openscada.da.server.proxy.utils;
@@ -36,9 +36,10 @@ public class ProxyUtils
      */
     public static String originalItemId ( final String itemId, final String separator, final ProxyPrefixName proxyPrefix, final ProxyPrefixName originalPrefix )
     {
-    	if ( originalPrefix == null || "".equals ( originalPrefix.getName() ) ) {
-    		return itemId.replaceFirst ( Pattern.quote ( proxyPrefix.getName () + separator ), "" );
-    	}
+        if ( originalPrefix == null || "".equals ( originalPrefix.getName () ) )
+        {
+            return itemId.replaceFirst ( Pattern.quote ( proxyPrefix.getName () + separator ), "" );
+        }
         return itemId.replaceFirst ( Pattern.quote ( proxyPrefix.getName () + separator ), originalPrefix.getName () + separator );
     }
 
@@ -51,9 +52,10 @@ public class ProxyUtils
      */
     public static String proxyItemId ( final String itemId, final String separator, final ProxyPrefixName proxyPrefix, final ProxyPrefixName originalPrefix )
     {
-    	if ( originalPrefix == null || "".equals ( originalPrefix.getName() ) ) {
-    		return proxyPrefix.getName () + separator + itemId;
-    	}
+        if ( originalPrefix == null || "".equals ( originalPrefix.getName () ) )
+        {
+            return proxyPrefix.getName () + separator + itemId;
+        }
         return itemId.replaceFirst ( Pattern.quote ( originalPrefix.getName () + separator ), proxyPrefix.getName () + separator );
     }
 
@@ -66,9 +68,10 @@ public class ProxyUtils
      */
     public static boolean isOriginalItemForProxyGroup ( final String itemId, final String separator, final ProxyPrefixName originalPrefix )
     {
-    	if ( originalPrefix == null || "".equals ( originalPrefix.getName() ) ) {
-    		return true;
-    	}
+        if ( originalPrefix == null || "".equals ( originalPrefix.getName () ) )
+        {
+            return true;
+        }
         return itemId.startsWith ( originalPrefix.getName () + separator );
     }
 }
