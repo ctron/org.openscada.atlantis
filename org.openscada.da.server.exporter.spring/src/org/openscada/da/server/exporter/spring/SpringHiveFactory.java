44b684e6ba710b826c1379f09c5987e9274f4914
Jens Reimann <jens.reimann@th4-systems.com>
2010-05-20 15:04:48 +0000

    switch to LGPLv3
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis/trunk@8022 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- org.openscada.da.server.exporter.spring/src/org/openscada/da/server/exporter/spring/SpringHiveFactory.java
+++ org.openscada.da.server.exporter.spring/src/org/openscada/da/server/exporter/spring/SpringHiveFactory.java
@@ -1,20 +1,20 @@
 /*
  * This file is part of the OpenSCADA project
- * Copyright (C) 2006-2007 inavare GmbH (http://inavare.com)
- * 
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * Copyright (C) 2006-2010 inavare GmbH (http://inavare.com)
  *
- * This library is distributed in the hope that it will be useful,
+ * OpenSCADA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenSCADA is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
-
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenSCADA. If not, see
+ * <http://opensource.org/licenses/lgpl-3.0.html> for a copy of the LGPLv3 License.
  */
 
 package org.openscada.da.server.exporter.spring;
@@ -24,22 +24,22 @@ import java.util.Map;
 
 import org.openscada.da.core.server.Hive;
 import org.openscada.da.server.exporter.ConfigurationException;
-import org.openscada.da.server.exporter.HiveFactory;
 import org.openscada.da.server.exporter.HiveConfigurationType;
+import org.openscada.da.server.exporter.HiveFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class SpringHiveFactory implements HiveFactory
 {
     protected Map<String, ApplicationContext> ctxMap = new HashMap<String, ApplicationContext> ();
-    
-    public Hive createHive ( String reference, HiveConfigurationType configuration ) throws ConfigurationException
+
+    public Hive createHive ( final String reference, final HiveConfigurationType configuration ) throws ConfigurationException
     {
-        String [] tok = reference.split ( "#", 2 );
-        
+        final String[] tok = reference.split ( "#", 2 );
+
         String beanName = "hive";
         String file = "file:applicationContext.xml";
-        
+
         if ( tok.length < 2 )
         {
             file = tok[0];
@@ -49,18 +49,18 @@ public class SpringHiveFactory implements HiveFactory
             file = tok[0];
             beanName = tok[1];
         }
-        
-        Hive hive = (Hive)getApplicationContext ( file ).getBean ( beanName, Hive.class );
+
+        final Hive hive = (Hive)getApplicationContext ( file ).getBean ( beanName, Hive.class );
         return hive;
     }
-    
-    protected ApplicationContext getApplicationContext ( String file )
+
+    protected ApplicationContext getApplicationContext ( final String file )
     {
-        ApplicationContext ctx = ctxMap.get ( file );
+        ApplicationContext ctx = this.ctxMap.get ( file );
         if ( ctx == null )
         {
             ctx = new FileSystemXmlApplicationContext ( file );
-            ctxMap.put ( file, ctx );
+            this.ctxMap.put ( file, ctx );
         }
         return ctx;
     }
