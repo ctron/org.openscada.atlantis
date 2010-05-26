bfa751bc517b71911312a464726306868d595ebc
Jens Reimann <jens.reimann@th4-systems.com>
2010-05-26 15:10:54 +0000

    rename to org.openscada.ae.server.common.monitor
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis/trunk@8083 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- org.openscada.ae.server.common.monitor/src/org/openscada/ae/server/common/monitor/internal/BundleMonitorQuery.java
+++ org.openscada.ae.server.common.monitor/src/org/openscada/ae/server/common/monitor/internal/BundleMonitorQuery.java
@@ -17,7 +17,7 @@
  * <http://opensource.org/licenses/lgpl-3.0.html> for a copy of the LGPLv3 License.
  */
 
-package org.openscada.ae.server.common.condition.internal;
+package org.openscada.ae.server.common.monitor.internal;
 
 import java.util.ArrayList;
 import java.util.Dictionary;
