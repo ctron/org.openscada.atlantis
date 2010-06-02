7ba10989aaf6401c2de5f4b9b2539d7c1a6b1497
Jens Reimann <jens.reimann@th4-systems.com>
2010-06-02 12:28:40 +0000

    not needed to implement that interface twice
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis/trunk@8150 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- org.openscada.da.datasource/src/org/openscada/da/datasource/base/AbstractInputDataSource.java
+++ org.openscada.da.datasource/src/org/openscada/da/datasource/base/AbstractInputDataSource.java
@@ -25,12 +25,11 @@ import org.openscada.core.OperationException;
 import org.openscada.core.Variant;
 import org.openscada.da.core.WriteAttributeResults;
 import org.openscada.da.core.WriteResult;
-import org.openscada.da.datasource.DataSource;
 import org.openscada.da.datasource.WriteInformation;
 import org.openscada.utils.concurrent.InstantErrorFuture;
 import org.openscada.utils.concurrent.NotifyFuture;
 
-public abstract class AbstractInputDataSource extends AbstractDataSource implements DataSource
+public abstract class AbstractInputDataSource extends AbstractDataSource
 {
 
     public NotifyFuture<WriteAttributeResults> startWriteAttributes ( final WriteInformation writeInformation, final Map<String, Variant> attributes )
