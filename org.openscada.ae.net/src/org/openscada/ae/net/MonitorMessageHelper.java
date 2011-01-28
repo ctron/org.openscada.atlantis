1af560361296ec8738d4e9745b8c592829a00311
Jens Reimann <jens.reimann@th4-systems.com>
2011-01-28 16:15:21 +0000

    make void a single instance type
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis/trunk@8601 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- org.openscada.ae.net/src/org/openscada/ae/net/MonitorMessageHelper.java
+++ org.openscada.ae.net/src/org/openscada/ae/net/MonitorMessageHelper.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of the OpenSCADA project
- * Copyright (C) 2006-2010 TH4 SYSTEMS GmbH (http://th4-systems.com)
+ * Copyright (C) 2006-2011 TH4 SYSTEMS GmbH (http://th4-systems.com)
  *
  * OpenSCADA is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License version 3
@@ -174,7 +174,7 @@ public class MonitorMessageHelper
     {
         if ( removed == null )
         {
-            return new VoidValue ();
+            return VoidValue.INSTANCE;
         }
 
         final ListValue result = new ListValue ();
