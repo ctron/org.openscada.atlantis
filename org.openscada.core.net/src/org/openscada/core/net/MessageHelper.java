726687005860f8afd3e0374bf4da403c0f45d5f8
Jens Reimann <jens.reimann@th4-systems.com>
2011-01-28 16:33:50 +0000

    make void a single instance type
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis/trunk@8611 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- org.openscada.core.net/src/org/openscada/core/net/MessageHelper.java
+++ org.openscada.core.net/src/org/openscada/core/net/MessageHelper.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of the OpenSCADA project
- * Copyright (C) 2006-2010 TH4 SYSTEMS GmbH (http://th4-systems.com)
+ * Copyright (C) 2006-2011 TH4 SYSTEMS GmbH (http://th4-systems.com)
  *
  * OpenSCADA is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License version 3
@@ -142,7 +142,7 @@ public class MessageHelper
             }
             else if ( value.isBoolean () )
             {
-                return new BooleanValue ( value.asBoolean () );
+                return BooleanValue.valueOf ( value.asBoolean () );
             }
             else if ( value.isString () )
             {
@@ -150,12 +150,12 @@ public class MessageHelper
             }
             else if ( value.isNull () )
             {
-                return new VoidValue ();
+                return VoidValue.INSTANCE;
             }
         }
         catch ( final NullValueException e )
         {
-            return new VoidValue ();
+            return VoidValue.INSTANCE;
         }
         catch ( final NotConvertableException e )
         {
