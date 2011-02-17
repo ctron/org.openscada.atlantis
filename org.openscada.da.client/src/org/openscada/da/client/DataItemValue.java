e58e801fb2ecfd2f6dfaf396137e795781d9abda
Jens Reimann <jens.reimann@th4-systems.com>
2011-02-17 14:19:22 +0000

    add isBlocked ()
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/atlantis/trunk@8802 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- org.openscada.da.client/src/org/openscada/da/client/DataItemValue.java
+++ org.openscada.da.client/src/org/openscada/da/client/DataItemValue.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of the OpenSCADA project
- * Copyright (C) 2006-2010 TH4 SYSTEMS GmbH (http://th4-systems.com)
+ * Copyright (C) 2006-2011 TH4 SYSTEMS GmbH (http://th4-systems.com)
  *
  * OpenSCADA is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License version 3
@@ -199,6 +199,11 @@ public class DataItemValue
     public boolean isAlarm ()
     {
         return isAttribute ( "alarm", false );
+    }
+
+    public boolean isBlocked ()
+    {
+        return isAttribute ( "blocked", false );
     }
 
     public boolean isConnected ()
