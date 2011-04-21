92cf281e240f40421b8e046bdae4c46255fcbe76
Jens Reimann <jens.reimann@th4-systems.com>
2011-04-21 16:18:24 +0200

    use Variant.valueOf

--- org.openscada.hd.server.storage.hsdb/src/org/openscada/hd/server/storage/hsdb/StorageHistoricalItemService.java
+++ org.openscada.hd.server.storage.hsdb/src/org/openscada/hd/server/storage/hsdb/StorageHistoricalItemService.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of the OpenSCADA project
- * Copyright (C) 2006-2010 TH4 SYSTEMS GmbH (http://th4-systems.com)
+ * Copyright (C) 2006-2011 TH4 SYSTEMS GmbH (http://th4-systems.com)
  *
  * OpenSCADA is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License version 3
@@ -268,7 +268,7 @@ public class StorageHistoricalItemService implements StorageHistoricalItem, Reli
         {
             for ( final Entry<String, String> entry : data.entrySet () )
             {
-                variantData.put ( entry.getKey (), new Variant ( entry.getValue () ) );
+                variantData.put ( entry.getKey (), Variant.valueOf ( entry.getValue () ) );
             }
         }
         return new HistoricalItemInformation ( configuration.getId (), variantData );
