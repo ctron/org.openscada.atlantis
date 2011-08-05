520b674c4af6e9e8513ee42d2c65cb06bc806c69
Jens Reimann <jens.reimann@th4-systems.com>
2011-08-05 16:36:28 +0200

    register configuration factory

--- org.openscada.hd.server.proxy/src/org/openscada/hd/server/proxy/Activator.java
+++ org.openscada.hd.server.proxy/src/org/openscada/hd/server/proxy/Activator.java
@@ -1,7 +1,16 @@
 package org.openscada.hd.server.proxy;
 
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.openscada.ca.ConfigurationAdministrator;
+import org.openscada.ca.ConfigurationFactory;
+import org.openscada.utils.concurrent.NamedThreadFactory;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
 
 public class Activator implements BundleActivator
 {
@@ -13,6 +22,10 @@ public class Activator implements BundleActivator
         return context;
     }
 
+    private ExecutorService executor;
+
+    private ProxyItemFactory service;
+
     /*
      * (non-Javadoc)
      * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
@@ -21,6 +34,14 @@ public class Activator implements BundleActivator
     public void start ( final BundleContext bundleContext ) throws Exception
     {
         Activator.context = bundleContext;
+        this.executor = Executors.newSingleThreadExecutor ( new NamedThreadFactory ( "HistoricalProxyItem" ) );
+        this.service = new ProxyItemFactory ( bundleContext, this.executor );
+
+        final Dictionary<String, Object> properties = new Hashtable<String, Object> ();
+        properties.put ( Constants.SERVICE_VENDOR, "TH4 SYSTEMS GmbH" );
+        properties.put ( Constants.SERVICE_DESCRIPTION, "A historical item that proxies access to a set of other items at the same time, merging the result based on the quality." );
+        properties.put ( ConfigurationAdministrator.FACTORY_ID, "org.openscada.hd.server.proxy.HistoricalProxyItem" );
+        bundleContext.registerService ( ConfigurationFactory.class.getName (), this.service, properties );
     }
 
     /*
@@ -30,6 +51,8 @@ public class Activator implements BundleActivator
     @Override
     public void stop ( final BundleContext bundleContext ) throws Exception
     {
+        this.service.dispose ();
+        this.executor.shutdown ();
         Activator.context = null;
     }
 
