f6c1426bcd962a0d254975e65d4e52960dd53847
Jens Reimann <jens.reimann@th4-systems.com>
2012-01-26 10:31:58 +0100

    using new OSGi tracker

--- org.openscada.hd.server.common.item/src/org/openscada/hd/server/common/item/internal/ConfigurationFactoryImpl.java
+++ org.openscada.hd.server.common.item/src/org/openscada/hd/server/common/item/internal/ConfigurationFactoryImpl.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of the OpenSCADA project
- * Copyright (C) 2006-2011 TH4 SYSTEMS GmbH (http://th4-systems.com)
+ * Copyright (C) 2006-2012 TH4 SYSTEMS GmbH (http://th4-systems.com)
  *
  * OpenSCADA is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License version 3
@@ -158,11 +158,9 @@ public class ConfigurationFactoryImpl implements ConfigurationFactory
         attributes.put ( "master.id", Variant.valueOf ( masterId ) );
 
         final ConfigurationDataHelper cfg = new ConfigurationDataHelper ( properties );
-        final VariantEditor ve = new VariantEditor ();
         for ( final Map.Entry<String, String> entry : cfg.getPrefixed ( "information." ).entrySet () )
         {
-            ve.setAsText ( entry.getValue () );
-            attributes.put ( entry.getKey (), (Variant)ve.getValue () );
+            attributes.put ( entry.getKey (), VariantEditor.toVariant ( entry.getValue () ) );
         }
 
         final HistoricalItemImpl item = new HistoricalItemImpl ( configurationId, attributes, masterId, this.context );
