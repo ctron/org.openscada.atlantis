7947a633854b858d04f21d57625e7c344f790044
Jens Reimann <jens.reimann@th4-systems.com>
2012-01-26 10:40:13 +0100

    cleanup

--- org.openscada.da.server.arduino/src/org/openscada/da/server/arduino/Activator.java
+++ org.openscada.da.server.arduino/src/org/openscada/da/server/arduino/Activator.java
@@ -1,3 +1,22 @@
+/*
+ * This file is part of the OpenSCADA project
+ * Copyright (C) 2006-2012 TH4 SYSTEMS GmbH (http://th4-systems.com)
+ *
+ * OpenSCADA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenSCADA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenSCADA. If not, see
+ * <http://opensource.org/licenses/lgpl-3.0.html> for a copy of the LGPLv3 License.
+ */
+
 package org.openscada.da.server.arduino;
 
 import java.util.Dictionary;
@@ -10,6 +29,7 @@ import org.openscada.ca.ConfigurationFactory;
 import org.openscada.da.server.arduino.factory.ConfigurationFactoryImpl;
 import org.openscada.da.server.common.DataItem;
 import org.openscada.utils.concurrent.NamedThreadFactory;
+import org.openscada.utils.osgi.pool.ObjectPool;
 import org.openscada.utils.osgi.pool.ObjectPoolHelper;
 import org.openscada.utils.osgi.pool.ObjectPoolImpl;
 import org.osgi.framework.BundleActivator;
@@ -24,7 +44,7 @@ public class Activator implements BundleActivator
 
     private ObjectPoolImpl itemPool;
 
-    private ServiceRegistration itemPoolHandle;
+    private ServiceRegistration<ObjectPool> itemPoolHandle;
 
     private ExecutorService executor;
 
--- org.openscada.da.server.arduino/src/org/openscada/da/server/arduino/ArduinoDevice.java
+++ org.openscada.da.server.arduino/src/org/openscada/da/server/arduino/ArduinoDevice.java
@@ -1,3 +1,22 @@
+/*
+ * This file is part of the OpenSCADA project
+ * Copyright (C) 2006-2012 TH4 SYSTEMS GmbH (http://th4-systems.com)
+ *
+ * OpenSCADA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenSCADA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenSCADA. If not, see
+ * <http://opensource.org/licenses/lgpl-3.0.html> for a copy of the LGPLv3 License.
+ */
+
 package org.openscada.da.server.arduino;
 
 import java.net.InetSocketAddress;
@@ -18,10 +37,6 @@ import org.osgi.framework.BundleContext;
 
 public class ArduinoDevice
 {
-    private final BundleContext context;
-
-    private final String configurationId;
-
     private final org.openscada.protocols.arduino.ArduinoDevice device;
 
     private final ObjectPoolImpl itemPool;
@@ -38,8 +53,6 @@ public class ArduinoDevice
 
     public ArduinoDevice ( final BundleContext context, final String configurationId, final Map<String, String> parameters, final ObjectPoolImpl itemPool, final Executor executor )
     {
-        this.context = context;
-        this.configurationId = configurationId;
 
         this.itemPool = itemPool;
         this.executor = executor;
