da8920919c775e30882028904939660633f2b72e
Jens Reimann <jens.reimann@th4-systems.com>
2012-01-26 10:22:19 +0100

    use VariantEditor.toVariant

--- org.openscada.da.datasource.script/src/org/openscada/da/datasource/script/WriterController.java
+++ org.openscada.da.datasource.script/src/org/openscada/da/datasource/script/WriterController.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of the OpenSCADA project
- * Copyright (C) 2006-2011 TH4 SYSTEMS GmbH (http://th4-systems.com)
+ * Copyright (C) 2006-2012 TH4 SYSTEMS GmbH (http://th4-systems.com)
  *
  * OpenSCADA is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License version 3
@@ -97,8 +97,6 @@ public class WriterController
 
     public void writeAsText ( final String itemId, final String value ) throws Exception
     {
-        final VariantEditor ve = new VariantEditor ();
-        ve.setAsText ( value );
-        write ( itemId, ve.getValue () );
+        write ( itemId, VariantEditor.toVariant ( value ) );
     }
 }
