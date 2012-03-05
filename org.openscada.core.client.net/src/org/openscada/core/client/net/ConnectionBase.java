8f3303aba5b8189659ad9696f8b0f18a86b22015
Jens Reimann <jens.reimann@th4-systems.com>
2012-03-05 15:18:46 +0100

    mask out password

--- org.openscada.core.client.net/src/org/openscada/core/client/net/ConnectionBase.java
+++ org.openscada.core.client.net/src/org/openscada/core/client/net/ConnectionBase.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of the OpenSCADA project
- * Copyright (C) 2006-2011 TH4 SYSTEMS GmbH (http://th4-systems.com)
+ * Copyright (C) 2006-2012 TH4 SYSTEMS GmbH (http://th4-systems.com)
  *
  * OpenSCADA is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License version 3
@@ -489,7 +489,7 @@ public abstract class ConnectionBase implements Connection, IoHandler
         String connection = "";
         if ( this.connectionInformation != null )
         {
-            connection += " " + this.connectionInformation.toString ();
+            connection += " " + this.connectionInformation.toMaskedString ();
         }
         logger.info ( "Connection attempt failed" + connection, e );
 
