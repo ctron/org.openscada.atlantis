41c656d68990900c583256b91da1b092aa484747
Jens Reimann <jens.reimann@th4-systems.com>
2012-06-06 11:02:11 +0200

    only record events when manual value state changes, not by write request

--- org.openscada.da.master.common/src/org/openscada/da/master/common/manual/ManualHandlerImpl.java
+++ org.openscada.da.master.common/src/org/openscada/da/master/common/manual/ManualHandlerImpl.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of the OpenSCADA project
- * Copyright (C) 2006-2011 TH4 SYSTEMS GmbH (http://th4-systems.com)
+ * Copyright (C) 2006-2012 TH4 SYSTEMS GmbH (http://th4-systems.com)
  *
  * OpenSCADA is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License version 3
@@ -47,17 +47,147 @@ public class ManualHandlerImpl extends AbstractCommonHandlerImpl
 
     private final static Logger logger = LoggerFactory.getLogger ( ManualHandlerImpl.class );
 
-    private Variant value = Variant.NULL;
-
-    private String user;
-
-    private String reason;
-
-    private Date timestamp;
-
     private final EventProcessor eventProcessor;
 
     private final String id;
+
+    public static class ManualStateData
+    {
+        private Variant value;
+
+        private String user;
+
+        private String reason;
+
+        private Date timestmap;
+
+        public ManualStateData ( final Variant value, final String user, final String reason, final Date timestmap )
+        {
+            super ();
+            this.value = value;
+            this.user = user;
+            this.reason = reason;
+            this.timestmap = timestmap;
+        }
+
+        public Variant getValue ()
+        {
+            return this.value;
+        }
+
+        public void setValue ( final Variant value )
+        {
+            this.value = value;
+        }
+
+        public String getUser ()
+        {
+            return this.user;
+        }
+
+        public void setUser ( final String user )
+        {
+            this.user = user;
+        }
+
+        public String getReason ()
+        {
+            return this.reason;
+        }
+
+        public void setReason ( final String reason )
+        {
+            this.reason = reason;
+        }
+
+        public Date getTimestmap ()
+        {
+            return this.timestmap;
+        }
+
+        public void setTimestmap ( final Date timestmap )
+        {
+            this.timestmap = timestmap;
+        }
+
+        @Override
+        public int hashCode ()
+        {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + ( this.reason == null ? 0 : this.reason.hashCode () );
+            result = prime * result + ( this.timestmap == null ? 0 : this.timestmap.hashCode () );
+            result = prime * result + ( this.user == null ? 0 : this.user.hashCode () );
+            result = prime * result + ( this.value == null ? 0 : this.value.hashCode () );
+            return result;
+        }
+
+        @Override
+        public boolean equals ( final Object obj )
+        {
+            if ( this == obj )
+            {
+                return true;
+            }
+            if ( obj == null )
+            {
+                return false;
+            }
+            if ( getClass () != obj.getClass () )
+            {
+                return false;
+            }
+            final ManualStateData other = (ManualStateData)obj;
+            if ( this.reason == null )
+            {
+                if ( other.reason != null )
+                {
+                    return false;
+                }
+            }
+            else if ( !this.reason.equals ( other.reason ) )
+            {
+                return false;
+            }
+            if ( this.timestmap == null )
+            {
+                if ( other.timestmap != null )
+                {
+                    return false;
+                }
+            }
+            else if ( !this.timestmap.equals ( other.timestmap ) )
+            {
+                return false;
+            }
+            if ( this.user == null )
+            {
+                if ( other.user != null )
+                {
+                    return false;
+                }
+            }
+            else if ( !this.user.equals ( other.user ) )
+            {
+                return false;
+            }
+            if ( this.value == null )
+            {
+                if ( other.value != null )
+                {
+                    return false;
+                }
+            }
+            else if ( !this.value.equals ( other.value ) )
+            {
+                return false;
+            }
+            return true;
+        }
+
+    }
+
+    private ManualStateData state = new ManualStateData ( Variant.NULL, null, null, null );
 
     public ManualHandlerImpl ( final String configurationId, final EventProcessor eventProcessor, final ObjectPoolTracker poolTracker, final int priority, final ServiceTracker<ConfigurationAdministrator, ConfigurationAdministrator> caTracker )
     {
@@ -73,16 +203,14 @@ public class ManualHandlerImpl extends AbstractCommonHandlerImpl
 
         injectAttributes ( builder );
 
-        if ( this.value.isNull () )
+        if ( this.state.getValue ().isNull () )
         {
             return builder.build ();
         }
 
         // apply manual value : manual value is active
 
-        final String user = this.user;
-        final String reason = this.reason;
-        final Date timestamp = this.timestamp;
+        final ManualStateData state = this.state;
 
         final Variant originalError = builder.getAttributes ().remove ( "error" ); //$NON-NLS-1$
         builder.setAttribute ( getPrefixed ( "error.original" ), originalError ); //$NON-NLS-1$
@@ -108,20 +236,20 @@ public class ManualHandlerImpl extends AbstractCommonHandlerImpl
         builder.setAttribute ( getPrefixed ( "active" ), Variant.TRUE ); //$NON-NLS-1$
         builder.setAttribute ( getPrefixed ( null ), Variant.TRUE );
 
-        builder.setValue ( this.value );
+        builder.setValue ( this.state.getValue () );
 
-        if ( user != null )
+        if ( state.getUser () != null )
         {
-            builder.setAttribute ( getPrefixed ( "user" ), Variant.valueOf ( user ) ); //$NON-NLS-1$
+            builder.setAttribute ( getPrefixed ( "user" ), Variant.valueOf ( state.getUser () ) ); //$NON-NLS-1$
         }
-        if ( reason != null )
+        if ( state.getReason () != null )
         {
-            builder.setAttribute ( getPrefixed ( "reason" ), Variant.valueOf ( reason ) ); //$NON-NLS-1$
+            builder.setAttribute ( getPrefixed ( "reason" ), Variant.valueOf ( state.getReason () ) ); //$NON-NLS-1$
         }
-        if ( timestamp != null )
+        if ( state.getTimestmap () != null )
         {
             final Variant originalTimestamp = builder.getAttributes ().get ( "timestamp" ); //$NON-NLS-1$
-            builder.setAttribute ( "timestamp", Variant.valueOf ( timestamp.getTime () ) ); //$NON-NLS-1$
+            builder.setAttribute ( "timestamp", Variant.valueOf ( state.getTimestmap ().getTime () ) ); //$NON-NLS-1$
             if ( originalTimestamp != null )
             {
                 builder.setAttribute ( getPrefixed ( "timestamp.original" ), originalTimestamp ); //$NON-NLS-1$
@@ -141,34 +269,76 @@ public class ManualHandlerImpl extends AbstractCommonHandlerImpl
         final ConfigurationDataHelper cfg = new ConfigurationDataHelper ( parameters );
 
         final String str = cfg.getString ( "value" ); //$NON-NLS-1$
-        logger.debug ( "Request to set manual value '{}'", str );
+        logger.debug ( "Request to set manual value '{}'", str ); //$NON-NLS-1$
+        final Variant newValue;
         if ( str != null )
         {
             ve.setAsText ( str );
-            this.value = (Variant)ve.getValue ();
-            if ( this.value == null )
-            {
-                this.value = Variant.NULL;
-            }
+            newValue = Variant.valueOf ( ve.getValue () );
         }
         else
         {
-            this.value = Variant.NULL;
+            newValue = Variant.NULL;
         }
-        this.user = cfg.getString ( "user" ); //$NON-NLS-1$
-        this.reason = cfg.getString ( "reason" ); //$NON-NLS-1$
-        this.timestamp = new Date ( cfg.getLong ( "timestamp", System.currentTimeMillis () ) ); //$NON-NLS-1$
+
+        final String newUser = cfg.getString ( "user" );//$NON-NLS-1$
+        final String newReason = cfg.getString ( "reason" ); //$NON-NLS-1$;
+        final Date ts = new Date ();
+        final Date newTimestamp = new Date ( cfg.getLong ( "timestamp", ts.getTime () ) ); //$NON-NLS-1$;
+
+        final ManualStateData newState = new ManualStateData ( newValue, newUser, newReason, newTimestamp );
+
+        sendUpdateEvent ( newState, ts );
+
+        this.state = newState;
 
         reprocess ();
     }
 
+    private void sendUpdateEvent ( final ManualStateData newState, final Date eventTimestamp )
+    {
+        final Event.EventBuilder builder = createEventBuilder ();
+
+        builder.entryTimestamp ( eventTimestamp );
+        builder.sourceTimestamp ( eventTimestamp );
+        builder.attribute ( Fields.MONITOR_TYPE, "MAN" ); //$NON-NLS-1$
+        builder.attribute ( Fields.SOURCE, this.id );
+
+        builder.attribute ( Fields.VALUE, newState.getValue () );
+        if ( newState.getValue ().isNull () )
+        {
+            builder.attribute ( Fields.MESSAGE, Messages.getString ( "ManualHandlerImpl.Reset.Message" ) ); //$NON-NLS-1$
+            builder.attribute ( Fields.MESSAGE_CODE, "MAN-RESET" ); //$NON-NLS-1$
+            builder.attribute ( Fields.EVENT_TYPE, Messages.getString ( "ManualHandlerImpl.Reset.Type" ) ); //$NON-NLS-1$
+        }
+        else
+        {
+            builder.attribute ( Fields.MESSAGE, Messages.getString ( "ManualHandlerImpl.Set.Message" ) ); //$NON-NLS-1$
+            builder.attribute ( Fields.MESSAGE_CODE, "MAN-SET" ); //$NON-NLS-1$
+            builder.attribute ( Fields.EVENT_TYPE, Messages.getString ( "ManualHandlerImpl.Set.Type" ) ); //$NON-NLS-1$
+        }
+
+        if ( newState.getUser () != null && !newState.getUser ().isEmpty () )
+        {
+            builder.attribute ( Fields.ACTOR_NAME, newState.getUser () );
+            builder.attribute ( Fields.ACTOR_TYPE, "USER" );
+        }
+
+        if ( newState.getReason () != null && !newState.getReason ().isEmpty () )
+        {
+            builder.attribute ( Fields.COMMENT, newState.getReason () );
+        }
+
+        this.eventProcessor.publishEvent ( builder.build () );
+    }
+
     protected void injectAttributes ( final Builder builder )
     {
-        builder.setAttribute ( getPrefixed ( null ), this.value.isNull () ? Variant.FALSE : Variant.TRUE );
-        builder.setAttribute ( getPrefixed ( "active" ), this.value.isNull () ? Variant.FALSE : Variant.TRUE ); //$NON-NLS-1$
-        builder.setAttribute ( getPrefixed ( "value" ), this.value ); //$NON-NLS-1$
-        builder.setAttribute ( getPrefixed ( "reason" ), Variant.valueOf ( this.reason ) ); //$NON-NLS-1$
-        builder.setAttribute ( getPrefixed ( "user" ), Variant.valueOf ( this.user ) ); //$NON-NLS-1$
+        builder.setAttribute ( getPrefixed ( null ), this.state.getValue ().isNull () ? Variant.FALSE : Variant.TRUE );
+        builder.setAttribute ( getPrefixed ( "active" ), this.state.getValue ().isNull () ? Variant.FALSE : Variant.TRUE ); //$NON-NLS-1$
+        builder.setAttribute ( getPrefixed ( "value" ), this.state.getValue () ); //$NON-NLS-1$
+        builder.setAttribute ( getPrefixed ( "reason" ), Variant.valueOf ( this.state.getReason () ) ); //$NON-NLS-1$
+        builder.setAttribute ( getPrefixed ( "user" ), Variant.valueOf ( this.state.getUser () ) ); //$NON-NLS-1$
     }
 
     @Override
@@ -176,40 +346,15 @@ public class ManualHandlerImpl extends AbstractCommonHandlerImpl
     {
         final Map<String, String> data = new HashMap<String, String> ();
 
-        final Event.EventBuilder builder = createEventBuilder ();
-
-        final Date ts = new Date ();
-        builder.entryTimestamp ( ts );
-        builder.sourceTimestamp ( ts );
-        builder.attribute ( Fields.MONITOR_TYPE, "MAN" ); //$NON-NLS-1$
-        builder.attribute ( Fields.SOURCE, this.id );
-
         final Variant value = attributes.get ( "value" ); //$NON-NLS-1$
         if ( value != null )
         {
-            if ( value == null || value.isNull () )
-            {
-                builder.attribute ( Fields.MESSAGE, Messages.getString ( "ManualHandlerImpl.Reset.Message" ) ); //$NON-NLS-1$
-                builder.attribute ( Fields.MESSAGE_CODE, "MAN-RESET" ); //$NON-NLS-1$
-                builder.attribute ( Fields.EVENT_TYPE, Messages.getString ( "ManualHandlerImpl.Reset.Type" ) ); //$NON-NLS-1$
-            }
-            else
-            {
-                builder.attribute ( Fields.MESSAGE, Messages.getString ( "ManualHandlerImpl.Set.Message" ) ); //$NON-NLS-1$
-                builder.attribute ( Fields.MESSAGE_CODE, "MAN-SET" ); //$NON-NLS-1$
-                builder.attribute ( Fields.EVENT_TYPE, Messages.getString ( "ManualHandlerImpl.Set.Type" ) ); //$NON-NLS-1$
-            }
-
             data.put ( "value", value.toString () ); //$NON-NLS-1$
-            builder.attribute ( Fields.VALUE, value );
         }
 
         if ( operationParameters != null && operationParameters.getUserInformation () != null && operationParameters.getUserInformation ().getName () != null )
         {
-            final String name = operationParameters.getUserInformation ().getName ();
-            data.put ( "user", name ); //$NON-NLS-1$
-            builder.attribute ( Fields.ACTOR_NAME, name );
-            builder.attribute ( Fields.ACTOR_TYPE, "USER" );
+            data.put ( "user", operationParameters.getUserInformation ().getName () ); //$NON-NLS-1$
         }
         else
         {
@@ -221,14 +366,12 @@ public class ManualHandlerImpl extends AbstractCommonHandlerImpl
             // clear user, reason and timestamp if we have a value
 
             data.put ( "reason", "" ); //$NON-NLS-1$ //$NON-NLS-2$
-            data.put ( "timestamp", "" + System.currentTimeMillis () ); //$NON-NLS-1$ //$NON-NLS-2$
         }
 
         final Variant reason = attributes.get ( "reason" ); //$NON-NLS-1$
         if ( reason != null && !reason.isNull () )
         {
             data.put ( "reason", reason.toString () ); //$NON-NLS-1$
-            builder.attribute ( Fields.COMMENT, reason );
         }
 
         final Variant timestamp = attributes.get ( "timestamp" ); //$NON-NLS-1$
@@ -236,8 +379,6 @@ public class ManualHandlerImpl extends AbstractCommonHandlerImpl
         {
             data.put ( "timestamp", "" + timestamp.asLong ( System.currentTimeMillis () ) ); //$NON-NLS-1$ //$NON-NLS-2$
         }
-
-        this.eventProcessor.publishEvent ( builder.build () );
 
         return updateConfiguration ( data, attributes, false, operationParameters );
     }
