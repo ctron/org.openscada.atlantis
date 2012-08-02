63f84b0dc9a2a22377726001e3bec791fdcd003f
Jens Reimann <jens.reimann@th4-systems.com>
2012-08-02 14:48:03 +0200

    adding license header

--- org.openscada.core.server.net/src/org/openscada/core/server/net/ManagedConnection.java
+++ org.openscada.core.server.net/src/org/openscada/core/server/net/ManagedConnection.java
@@ -1,3 +1,22 @@
+/*
+ * This file is part of the OpenSCADA project
+ * Copyright (C) 2006-2012 TH4 SYSTEMS GmbH (http://th4-systems.com)
+ *
+ * OpenSCADA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenSCADA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenSCADA. If not, see
+ * <http://opensource.org/licenses/lgpl-3.0.html> for a copy of the LGPLv3 License.
+ */
+
 package org.openscada.core.server.net;
 
 import java.lang.management.ManagementFactory;
@@ -77,7 +96,7 @@ public abstract class ManagedConnection implements ManagedConnectionMXBean
         try
         {
             final MBeanServer mbs = ManagementFactory.getPlatformMBeanServer ();
-            logger.debug ( "Creatint name for: {}", socketAddress );
+            logger.debug ( "Creating name for: {}", socketAddress );
 
             final String remote = URLEncoder.encode ( socketAddress.toString (), "UTF-8" );
 
