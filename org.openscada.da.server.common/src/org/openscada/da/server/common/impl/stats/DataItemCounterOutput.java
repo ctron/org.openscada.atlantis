285626ed0fe6581fe1179a6e29a59445c0e2b05e
Jens Reimann <jens.reimann@th4-systems.com>
2012-08-24 16:15:45 +0200

    export stats information as JMX

--- org.openscada.da.server.common/src/org/openscada/da/server/common/impl/stats/DataItemCounterOutput.java
+++ org.openscada.da.server.common/src/org/openscada/da/server/common/impl/stats/DataItemCounterOutput.java
@@ -19,26 +19,55 @@
 
 package org.openscada.da.server.common.impl.stats;
 
+import java.lang.management.ManagementFactory;
 import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.Map;
+
+import javax.management.MBeanServer;
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectInstance;
+import javax.management.ObjectName;
 
 import org.openscada.core.Variant;
 import org.openscada.da.server.browser.common.FolderCommon;
 import org.openscada.da.server.common.chain.DataItemInputChained;
 import org.openscada.da.server.common.impl.HiveCommon;
 import org.openscada.utils.concurrent.DirectExecutor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DataItemCounterOutput implements CounterOutput
 {
+
+    private final static Logger logger = LoggerFactory.getLogger ( DataItemCounterOutput.class );
 
     private final DataItemInputChained averageItem;
 
     private final DataItemInputChained totalItem;
 
+    private final MBeanServer mbs;
+
+    private final CounterOutputMXBeanImpl averageExport;
+
+    private final CounterOutputMXBeanImpl totalExport;
+
+    private ObjectInstance averageInstance;
+
+    private ObjectInstance totalInstance;
+
+    private final String itemId;
+
     public DataItemCounterOutput ( final String itemId )
     {
+        this.itemId = itemId;
         this.averageItem = new DataItemInputChained ( itemId + ".average", DirectExecutor.INSTANCE );
         this.totalItem = new DataItemInputChained ( itemId + ".total", DirectExecutor.INSTANCE );
+
+        this.mbs = ManagementFactory.getPlatformMBeanServer ();
+
+        this.averageExport = new CounterOutputMXBeanImpl ();
+        this.totalExport = new CounterOutputMXBeanImpl ();
     }
 
     @Override
@@ -55,6 +84,24 @@ public class DataItemCounterOutput implements CounterOutput
         hive.registerItem ( this.totalItem );
         attributes.put ( "description", Variant.valueOf ( description + " - Total counter" ) );
         rootFolder.add ( this.totalItem.getInformation ().getName (), this.totalItem, attributes );
+
+        try
+        {
+            this.averageInstance = this.mbs.registerMBean ( this.averageExport, makeName ( "org.openscada.da.server.common.stats", this.itemId, "average" ) );
+            this.totalInstance = this.mbs.registerMBean ( this.totalExport, makeName ( "org.openscada.da.server.common.stats", this.itemId, "total" ) );
+        }
+        catch ( final Exception e )
+        {
+            logger.warn ( "Failed to export statistics", e );
+        }
+    }
+
+    private ObjectName makeName ( final String domain, final String description, final String type ) throws MalformedObjectNameException, NullPointerException
+    {
+        final Hashtable<String, String> properties = new Hashtable<String, String> ( 2 );
+        properties.put ( "value", description );
+        properties.put ( "type", type );
+        return new ObjectName ( domain, properties );
     }
 
     @Override
@@ -65,6 +112,31 @@ public class DataItemCounterOutput implements CounterOutput
 
         rootFolder.remove ( this.totalItem );
         hive.unregisterItem ( this.totalItem );
+
+        if ( this.averageInstance != null )
+        {
+            try
+            {
+                this.mbs.unregisterMBean ( this.averageInstance.getObjectName () );
+            }
+            catch ( final Exception e )
+            {
+                logger.warn ( "Failed to unregister average", e );
+            }
+            this.averageInstance = null;
+        }
+        if ( this.totalInstance != null )
+        {
+            try
+            {
+                this.mbs.unregisterMBean ( this.totalInstance.getObjectName () );
+            }
+            catch ( final Exception e )
+            {
+                logger.warn ( "Failed to unregister total", e );
+            }
+            this.totalInstance = null;
+        }
     }
 
     @Override
