e389cd8f427c858e21b4f2b712531f5084c489cd
Jens Reimann <jens.reimann@th4-systems.com>
2012-08-24 16:01:06 +0200

    format

--- org.openscada.da.server.common/src/org/openscada/da/server/common/impl/stats/DataItemCounterOutput.java
+++ org.openscada.da.server.common/src/org/openscada/da/server/common/impl/stats/DataItemCounterOutput.java
@@ -65,7 +65,6 @@ public class DataItemCounterOutput implements CounterOutput
 
         rootFolder.remove ( this.totalItem );
         hive.unregisterItem ( this.totalItem );
-
     }
 
     @Override
