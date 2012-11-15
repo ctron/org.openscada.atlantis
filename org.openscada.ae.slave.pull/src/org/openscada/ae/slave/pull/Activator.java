e3496aeaecd3f4b1df3553a234f9e912980ad9d5
Jens Reimann <jens.reimann@th4-systems.com>
2012-11-15 11:17:44 +0100

    cleanup

--- org.openscada.ae.slave.pull/src/org/openscada/ae/slave/pull/Activator.java
+++ org.openscada.ae.slave.pull/src/org/openscada/ae/slave/pull/Activator.java
@@ -3,28 +3,34 @@ package org.openscada.ae.slave.pull;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
-public class Activator implements BundleActivator {
+public class Activator implements BundleActivator
+{
 
-	private static BundleContext context;
+    private static BundleContext context;
 
-	static BundleContext getContext() {
-		return context;
-	}
+    static BundleContext getContext ()
+    {
+        return context;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
-	 */
-	public void start(BundleContext bundleContext) throws Exception {
-		Activator.context = bundleContext;
-	}
+    /*
+     * (non-Javadoc)
+     * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+     */
+    @Override
+    public void start ( final BundleContext bundleContext ) throws Exception
+    {
+        Activator.context = bundleContext;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
-	 */
-	public void stop(BundleContext bundleContext) throws Exception {
-		Activator.context = null;
-	}
+    /*
+     * (non-Javadoc)
+     * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+     */
+    @Override
+    public void stop ( final BundleContext bundleContext ) throws Exception
+    {
+        Activator.context = null;
+    }
 
 }
