5a265c459594b61c98dbe647f766f3bc1982e878
Jens Reimann <jens.reimann@th4-systems.com>
2012-12-07 15:04:50 +0100

    allow disabling the manual value conversion

--- org.openscada.da.server.opc/src/org/openscada/da/server/opc/connection/OPCItem.java
+++ org.openscada.da.server.opc/src/org/openscada/da/server/opc/connection/OPCItem.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of the OpenSCADA project
- * Copyright (C) 2006-2011 TH4 SYSTEMS GmbH (http://th4-systems.com)
+ * Copyright (C) 2006-2012 TH4 SYSTEMS GmbH (http://th4-systems.com)
  *
  * OpenSCADA is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License version 3
@@ -66,6 +66,8 @@ public class OPCItem extends DataItemInputOutputChained implements SuspendableDa
     private boolean ignoreTimestampOnlyChange = false;
 
     private short qualityErrorIfLessThen = 192;
+
+    private final boolean ignoreManual = Boolean.getBoolean ( "org.openscada.da.server.opc.ignoreManual" );
 
     public OPCItem ( final Hive hive, final OPCController controller, final DataItemInformation di, final String opcItemId )
     {
@@ -166,8 +168,12 @@ public class OPCItem extends DataItemInputOutputChained implements SuspendableDa
             attributes.put ( "opc.quality", Variant.valueOf ( quality ) );
 
             attributes.put ( "opc.quality.error", Variant.valueOf ( quality < this.qualityErrorIfLessThen ) );
-            attributes.put ( "opc.quality.manual", Variant.valueOf ( quality == 216 ) );
-            attributes.put ( "org.openscada.da.manual.active", Variant.valueOf ( quality == 216 ) );
+
+            if ( !this.ignoreManual )
+            {
+                attributes.put ( "opc.quality.manual", Variant.valueOf ( quality == 216 ) );
+                attributes.put ( "org.openscada.da.manual.active", Variant.valueOf ( quality == 216 ) );
+            }
 
             attributes.put ( "opc.value.type", null );
             try
