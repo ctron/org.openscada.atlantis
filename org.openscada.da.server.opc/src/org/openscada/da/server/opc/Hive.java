c6b1abd7005c5c0310433548c6c302f9bf5ab255
Jens Reimann <ctron@dentrassi.de>
2013-01-14 14:20:56 +0100

    allow setting config file for plain starter

--- org.openscada.da.server.opc/src/org/openscada/da/server/opc/Hive.java
+++ org.openscada.da.server.opc/src/org/openscada/da/server/opc/Hive.java
@@ -1,6 +1,8 @@
 /*
  * This file is part of the OpenSCADA project
+ * 
  * Copyright (C) 2006-2011 TH4 SYSTEMS GmbH (http://th4-systems.com)
+ * Copyright (C) 2013 Jens Reimann (ctron@dentrassi.de)
  *
  * OpenSCADA is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License version 3
@@ -50,7 +52,7 @@ public class Hive extends HiveCommon
 
     public Hive () throws XmlException, IOException, ConfigurationError
     {
-        this ( new XMLConfigurator ( "configuration.xml" ) );
+        this ( new XMLConfigurator ( System.getProperty ( "org.openscada.da.server.opc.defaultConfigurationFile", "configuration.xml" ) ) ); //$NON-NLS-1$ //$NON-NLS-2$
     }
 
     public Hive ( final Node node ) throws XmlException, IOException, ConfigurationError
@@ -77,7 +79,7 @@ public class Hive extends HiveCommon
     @Override
     public String getHiveId ()
     {
-        return "org.openscada.da.server.opc";
+        return "org.openscada.da.server.opc"; //$NON-NLS-1$
     }
 
     private void initJInterop ()
