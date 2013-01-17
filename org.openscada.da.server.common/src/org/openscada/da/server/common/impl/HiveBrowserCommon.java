4ee12a209eea76d14f5bde83e5b24a25ae123b8d
Jens Reimann <ctron@dentrassi.de>
2013-01-17 15:57:38 +0100

    minor cleanups

--- org.openscada.da.server.common/src/org/openscada/da/server/common/impl/HiveBrowserCommon.java
+++ org.openscada.da.server.common/src/org/openscada/da/server/common/impl/HiveBrowserCommon.java
@@ -1,6 +1,8 @@
 /*
  * This file is part of the OpenSCADA project
+ * 
  * Copyright (C) 2006-2011 TH4 SYSTEMS GmbH (http://th4-systems.com)
+ * Copyright (C) 2013 Jens Reimann (ctron@dentrassi.de)
  *
  * OpenSCADA is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License version 3
@@ -181,7 +183,7 @@ public abstract class HiveBrowserCommon implements HiveBrowser, FolderListener, 
     {
         logger.debug ( "Session destroy: {} entries", session.getData ().getPaths ().size () );
 
-        Map<Object, Location> entries;
+        final Map<Object, Location> entries;
 
         synchronized ( session.getData ().getPaths () )
         {
