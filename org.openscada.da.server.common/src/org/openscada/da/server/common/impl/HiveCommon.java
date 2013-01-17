b4acb7bc9667c0d988375a5c97818b2dfc7041f4
Jens Reimann <ctron@dentrassi.de>
2013-01-17 16:10:38 +0100

    minor performance fix

--- org.openscada.da.server.common/src/org/openscada/da/server/common/impl/HiveCommon.java
+++ org.openscada.da.server.common/src/org/openscada/da/server/common/impl/HiveCommon.java
@@ -1,6 +1,8 @@
 /*
  * This file is part of the openSCADA project
+ * 
  * Copyright (C) 2006-2012 TH4 SYSTEMS GmbH (http://th4-systems.com)
+ * Copyright (C) 2013 Jens Reimann (ctron@dentrassi.de)
  *
  * openSCADA is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License version 3
@@ -735,7 +737,7 @@ public abstract class HiveCommon extends ServiceCommon<Session> implements Hive,
     {
         final List<Object> topics = this.itemSubscriptionManager.getAllGrantedTopics ();
 
-        final Set<String> items = new HashSet<String> ();
+        final Set<String> items = new HashSet<String> ( topics.size () );
 
         for ( final Object topic : topics )
         {
