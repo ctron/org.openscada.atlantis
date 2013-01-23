a60810ed5e19e4f203784817daada1dc788c598f
Jens Reimann <ctron@dentrassi.de>
2013-01-23 16:43:26 +0100

    fix possible NPE

--- org.openscada.core.server.common/src/org/openscada/core/server/common/session/AbstractSessionImpl.java
+++ org.openscada.core.server.common/src/org/openscada/core/server/common/session/AbstractSessionImpl.java
@@ -1,6 +1,8 @@
 /*
  * This file is part of the OpenSCADA project
+ * 
  * Copyright (C) 2006-2012 TH4 SYSTEMS GmbH (http://th4-systems.com)
+ * Copyright (C) 2013 Jens Reimann (ctron@dentrassi.de)
  *
  * OpenSCADA is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License version 3
@@ -56,7 +58,7 @@ public abstract class AbstractSessionImpl implements Session
     public AbstractSessionImpl ( final UserInformation userInformation, final Map<String, String> properties )
     {
         this.userInformation = userInformation;
-        this.properties = new HashMap<String, String> ( properties );
+        this.properties = properties != null ? new HashMap<String, String> ( properties ) : Collections.<String, String> emptyMap ();
     }
 
     @Override
