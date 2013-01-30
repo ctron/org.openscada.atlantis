2a4e3a84e8f3a6184d00caad15a09d696e7c3576
Jens Reimann <ctron@dentrassi.de>
2013-01-30 17:58:13 +0100

    convert console to new gogo shell console

--- org.openscada.hd.server.storage.master.hds/src/org/openscada/hd/server/storage/master/hds/Activator.java
+++ org.openscada.hd.server.storage.master.hds/src/org/openscada/hd/server/storage/master/hds/Activator.java
@@ -1,10 +1,30 @@
+/*
+ * This file is part of the openSCADA project
+ * 
+ * Copyright (C) 2006-2012 TH4 SYSTEMS GmbH (http://th4-systems.com)
+ * Copyright (C) 2013 Jens Reimann (ctron@dentrassi.de)
+ *
+ * OpenSCADA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenSCADA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenSCADA. If not, see
+ * <http://opensource.org/licenses/lgpl-3.0.html> for a copy of the LGPLv3 License.
+ */
+
 package org.openscada.hd.server.storage.master.hds;
 
 import java.util.Dictionary;
 import java.util.Hashtable;
 
-import org.eclipse.osgi.framework.console.CommandProvider;
-import org.openscada.hd.server.storage.master.hds.console.CommandProviderImpl;
+import org.openscada.hd.server.storage.master.hds.console.Console;
 import org.openscada.hds.DataFilePool;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
@@ -36,7 +56,7 @@ public class Activator implements BundleActivator
     {
         Activator.context = bundleContext;
 
-        this.pool = new DataFilePool ( Integer.getInteger ( "org.openscada.hd.server.storage.master.hds.instanceCountTarget", 10 ) );
+        this.pool = new DataFilePool ( Integer.getInteger ( "org.openscada.hd.server.storage.master.hds.instanceCountTarget", 10 ) ); //$NON-NLS-1$
 
         this.manager = new StorageManager ( bundleContext, this.pool );
         registerConsole ();
@@ -61,13 +81,16 @@ public class Activator implements BundleActivator
     {
         try
         {
-            final CommandProvider provider = new CommandProviderImpl ( this.manager );
+            final Console console = new Console ( this.manager );
             final Dictionary<String, Object> properties = new Hashtable<String, Object> ();
-            context.registerService ( CommandProvider.class.getName (), provider, properties );
+            properties.put ( "osgi.command.scope", "hds" ); //$NON-NLS-1$
+            properties.put ( "osgi.command.function", new String[] { "list", "purgeAll", "remove", "create" } ); //$NON-NLS-1$
+
+            context.registerService ( Console.class, console, properties );
         }
         catch ( final Exception e )
         {
-            logger.warn ( "Failed to register Equinox console", e );
+            logger.warn ( "Failed to register console", e ); //$NON-NLS-1$
         }
     }
 
