5f94eec3f2f55f662f633b03a2287c4bf00220fd
Jens Reimann <ctron@dentrassi.de>
2013-01-30 14:09:16 +0100

    remove synchronized, should be long gone

--- org.openscada.da.datasource/src/org/openscada/da/datasource/base/AbstractDataSource.java
+++ org.openscada.da.datasource/src/org/openscada/da/datasource/base/AbstractDataSource.java
@@ -1,6 +1,8 @@
 /*
  * This file is part of the OpenSCADA project
+ * 
  * Copyright (C) 2006-2012 TH4 SYSTEMS GmbH (http://th4-systems.com)
+ * Copyright (C) 2013 Jens Reimann (ctron@dentrassi.de)
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -105,7 +107,7 @@ public abstract class AbstractDataSource implements DataSource
         }
     }
 
-    protected synchronized void updateData ( DataItemValue value )
+    protected void updateData ( DataItemValue value )
     {
         logger.debug ( "Update data: {} -> {}", new Object[] { value, value == null ? "" : value.getAttributes () } );
 
