3083a419ceb357a7f4525779d3925d84fe3dbe43
Jens Reimann <ctron@dentrassi.de>
2013-02-07 13:04:37 +0100

    fix conversion error which resulted in "null"

--- org.openscada.da.server.ngp/src/org/openscada/da/server/ngp/AttributesTransport.java
+++ org.openscada.da.server.ngp/src/org/openscada/da/server/ngp/AttributesTransport.java
@@ -1,6 +1,8 @@
 /*
  * This file is part of the openSCADA project
+ * 
  * Copyright (C) 2011-2012 TH4 SYSTEMS GmbH (http://th4-systems.com)
+ * Copyright (C) 2013 Jens Reimann (ctron@dentrassi.de)
  *
  * openSCADA is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License version 3
@@ -38,7 +40,7 @@ public class AttributesTransport
 
         for ( final Map.Entry<String, Variant> entry : attributes.entrySet () )
         {
-            if ( entry.getKey () == null )
+            if ( entry.getValue () == null )
             {
                 this.removed.add ( entry.getKey () );
             }
