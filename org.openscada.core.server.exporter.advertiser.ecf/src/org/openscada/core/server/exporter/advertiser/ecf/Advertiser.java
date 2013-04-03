197f5c352cbdf7cc3bac44c8eb65989d085de1fb
Jens Reimann <ctron@dentrassi.de>
2013-04-03 13:31:39 +0200

    don't run without instance

--- org.openscada.core.server.exporter.advertiser.ecf/src/org/openscada/core/server/exporter/advertiser/ecf/Advertiser.java
+++ org.openscada.core.server.exporter.advertiser.ecf/src/org/openscada/core/server/exporter/advertiser/ecf/Advertiser.java
@@ -1,6 +1,8 @@
 /*
  * This file is part of the OpenSCADA project
+ * 
  * Copyright (C) 2006-2012 TH4 SYSTEMS GmbH (http://th4-systems.com)
+ * Copyright (C) 2013 Jens Reimann (ctron@dentrassi.de)
  *
  * OpenSCADA is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License version 3
@@ -70,6 +72,7 @@ public class Advertiser
         catch ( final Exception e )
         {
             logger.warn ( "Failed to start advertiser", e );
+            return;
         }
 
         this.tracker = new ServiceTracker<ExporterInformation, ExporterInformation> ( context, ExporterInformation.class, new ServiceTrackerCustomizer<ExporterInformation, ExporterInformation> () {
