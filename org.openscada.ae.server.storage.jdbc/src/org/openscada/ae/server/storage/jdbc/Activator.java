d5b16a7c48935d3453b95e1a2fe623d943f22411
Jens Reimann <ctron@dentrassi.de>
2013-08-23 12:46:50 +0200

    log

--- org.openscada.ae.server.storage.jdbc/src/org/openscada/ae/server/storage/jdbc/Activator.java
+++ org.openscada.ae.server.storage.jdbc/src/org/openscada/ae/server/storage/jdbc/Activator.java
@@ -75,7 +75,7 @@ public class Activator implements BundleActivator
 
         final String driver = DataSourceHelper.getDriver ( "org.openscada.ae.server.storage.jdbc.driver", DataSourceHelper.DEFAULT_PREFIX );
 
-        logger.debug ( "Using driver: {}", driver );
+        logger.info ( "Using driver: {}", driver );
 
         final Filter filter = context.createFilter ( "(&(objectClass=" + DataSourceFactory.class.getName () + ")(" + DataSourceFactory.OSGI_JDBC_DRIVER_CLASS + "=" + driver + "))" );
         this.dataSouceFactoryTracker = new SingleServiceTracker<DataSourceFactory> ( bundleContext, filter, new SingleServiceListener<DataSourceFactory> () {
