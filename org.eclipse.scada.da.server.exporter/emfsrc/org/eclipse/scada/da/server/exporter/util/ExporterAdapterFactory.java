2399eacd6957f7259460690dda45e8aff5368b38
Jens Reimann <jens.reimann@ibh-systems.com>
2013-09-27 11:55:41 +0200

    remove emf generated copyright fields

--- org.eclipse.scada.da.server.exporter/emfsrc/org/eclipse/scada/da/server/exporter/util/ExporterAdapterFactory.java
+++ org.eclipse.scada.da.server.exporter/emfsrc/org/eclipse/scada/da/server/exporter/util/ExporterAdapterFactory.java
@@ -35,12 +35,7 @@ import org.eclipse.scada.da.server.exporter.*;
  */
 public class ExporterAdapterFactory extends AdapterFactoryImpl
 {
-    /**
-     * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
-     * @generated
-     */
-    public static final String copyright = "This file is part of the openSCADA project\n\nCopyright (C) 2013 Jens Reimann (ctron@dentrassi.de)\n\nopenSCADA is free software: you can redistribute it and/or modify\nit under the terms of the GNU Lesser General Public License version 3\nonly, as published by the Free Software Foundation.\n\nopenSCADA is distributed in the hope that it will be useful,\nbut WITHOUT ANY WARRANTY; without even the implied warranty of\nMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\nGNU Lesser General Public License version 3 for more details\n(a copy is included in the LICENSE file that accompanied this code).\n\nYou should have received a copy of the GNU Lesser General Public License\nversion 3 along with openSCADA. If not, see\n<http://opensource.org/licenses/lgpl-3.0.html> for a copy of the LGPLv3 License."; //$NON-NLS-1$
+    //$NON-NLS-1$
 
     /**
      * The cached model package.
--- org.openscada.da.server.snmp/emfsrc/org/openscada/da/snmp/configuration/SnmpVersion.java
+++ org.openscada.da.server.snmp/emfsrc/org/openscada/da/snmp/configuration/SnmpVersion.java
@@ -66,12 +66,7 @@ public enum SnmpVersion implements Enumerator
      */
     _3 ( 2, "_3", "3" ); //$NON-NLS-1$ //$NON-NLS-2$
 
-    /**
-     * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
-     * @generated
-     */
-    public static final String copyright = "This file is part of the openSCADA project\n\nCopyright (C) 2013 Jens Reimann (ctron@dentrassi.de)\n\nopenSCADA is free software: you can redistribute it and/or modify\nit under the terms of the GNU Lesser General Public License version 3\nonly, as published by the Free Software Foundation.\n\nopenSCADA is distributed in the hope that it will be useful,\nbut WITHOUT ANY WARRANTY; without even the implied warranty of\nMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\nGNU Lesser General Public License version 3 for more details\n(a copy is included in the LICENSE file that accompanied this code).\n\nYou should have received a copy of the GNU Lesser General Public License\nversion 3 along with openSCADA. If not, see\n<http://opensource.org/licenses/lgpl-3.0.html> for a copy of the LGPLv3 License."; //$NON-NLS-1$
+    //$NON-NLS-1$
 
     /**
      * The '<em><b>1</b></em>' literal value.
