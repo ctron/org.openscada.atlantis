84a5dd837ffaed02bf7b9cb970f0e6c25d811bf9
Jens Reimann <jens.reimann@ibh-systems.com>
2014-11-05 09:43:36 +0100

    add IEC60870-5-104

---
+++ org.openscada.protocol.iec60870/src/org/openscada/protocol/iec60870/asdu/message/Encodeable.java
@@ -0,0 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 2014 IBH SYSTEMS GmbH and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBH SYSTEMS GmbH - initial API and implementation
+ *******************************************************************************/
+package org.openscada.protocol.iec60870.asdu.message;
+
+import io.netty.buffer.ByteBuf;
+
+import org.openscada.protocol.iec60870.ProtocolOptions;
+
+public interface Encodeable
+{
+    public void encode ( ProtocolOptions options, ByteBuf out );
+}
