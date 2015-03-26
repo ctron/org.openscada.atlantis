ca14147402f9944a384c488d3804b14254aa64ff
Jens Reimann <jens.reimann@ibh-systems.com>
2015-03-26 12:27:17 +0100

    fix a typo

--- org.openscada.protocol.iec60870/src/org/openscada/protocol/iec60870/CauseOfTransmissionType.java
+++ org.openscada.protocol.iec60870/src/org/openscada/protocol/iec60870/CauseOfTransmissionType.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014 IBH SYSTEMS GmbH and others.
+ * Copyright (c) 2014, 2015 IBH SYSTEMS GmbH and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -60,5 +60,5 @@ public enum CauseOfTransmissionType
 
     public abstract byte readOriginatorAddress ( ByteBuf data );
 
-    public abstract void write ( final byte casue, byte originatorAddress, final ByteBuf out );
+    public abstract void write ( final byte cause, byte originatorAddress, final ByteBuf out );
 }
