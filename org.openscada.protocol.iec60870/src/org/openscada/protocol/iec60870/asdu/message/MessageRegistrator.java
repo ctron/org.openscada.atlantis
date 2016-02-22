d30bf74ecd5a5e1316e03cd83ced4b29439da127
Jens Reimann <jens.reimann@ibh-systems.com>
2016-02-22 13:31:10 +0100

    register EndOfInitialization

--- org.openscada.protocol.iec60870/src/org/openscada/protocol/iec60870/asdu/message/MessageRegistrator.java
+++ org.openscada.protocol.iec60870/src/org/openscada/protocol/iec60870/asdu/message/MessageRegistrator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014 IBH SYSTEMS GmbH and others.
+ * Copyright (c) 2014, 2016 IBH SYSTEMS GmbH and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -38,5 +38,7 @@ public class MessageRegistrator
         manager.registerClass ( DoubleCommand.class );
         manager.registerClass ( SetPointCommandShortFloatingPoint.class );
         manager.registerClass ( SetPointCommandScaledValue.class );
+
+        manager.registerClass ( EndOfInitialization.class );
     }
 }
